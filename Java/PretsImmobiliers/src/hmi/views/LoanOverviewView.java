51f4ce16b4f782726b600dea2788c677d2b84b4c
yanndanielou <yanndanielou@users.noreply.github.com>
2016-01-22 20:10:35 +0100

    Create LoanOverviewView.java

---
+++ Java/PretsImmobiliers/src/hmi/views/LoanOverviewView.java
@@ -0,0 +1,42 @@
+package hmi.views;
+
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
+
+public class LoanOverviewView extends ProjetImmobilierBaseView {
+  private static final long serialVersionUID = -3015965288824537079L;
+
+  private static final LoanOverviewView INSTANCE = new LoanOverviewView();
+
+  private JLabel montantTotalInteretsLabel;
+  private JLabel montantTotalInteretsValue;
+
+  private LoanOverviewView() {
+    loanViewsMediator.setLoanOverviewView(this);
+  }
+
+  public static LoanOverviewView getInstance() {
+    return INSTANCE;
+  }
+
+  @Override
+  protected void placeWidgets() {
+    int columns = 2;
+    int rows = 1;
+    setLayout(new GridLayout(rows, columns));
+    add(montantTotalInteretsLabel);
+    add(montantTotalInteretsValue);
+  }
+
+  @Override
+  protected void createWidgets() {
+    montantTotalInteretsLabel = new JLabel("Montant total interets");
+    montantTotalInteretsValue = new JLabel();
+  }
+
+  public void afterEmpruntCreated() {
+    String text = String.valueOf(projetImmobilier.getMontantTotalInterets());
+    montantTotalInteretsValue.setText(text);
+  }
+}
