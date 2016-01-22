3fd81bfd055a42685be362213d7be5506db20fdf
yanndanielou <yanndanielou@users.noreply.github.com>
2016-01-22 19:58:45 +0100

    Create PretsImmobiliersApplication.java
    First version

---
+++ Java/PretsImmobiliers/src/main/PretsImmobiliersApplication.java
@@ -0,0 +1,14 @@
+/*
+ * Controlguide
+ * Copyright (c) Siemens AG 2016, All Rights Reserved, Confidential
+ */
+package main;
+
+import ihm.views.MainView;
+
+public class PretsImmobiliersApplication {
+
+  public static void main(String[] args) {
+    MainView.getInstance();
+  }
+}
