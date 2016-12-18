e1588893e1af5459397c3cfe096e0e8ef642db7c
Yann Danielou <yanndu91@msn.com>
2016-12-18 21:02:15 +0100

    add minus and plus operation

--- Java/Numbers/src/main/numbers/InfiniteNaturalNumber.java
+++ Java/Numbers/src/main/numbers/InfiniteNaturalNumber.java
@@ -3,12 +3,38 @@ package main.numbers;
 import java.util.ArrayList;
 import java.util.List;
 
-public class InfiniteNaturalNumber {
+public class InfiniteNaturalNumber implements Cloneable {
 
+	public static final InfiniteNaturalNumber ZERO = new InfiniteNaturalNumber("0");
+
+	public static final InfiniteNaturalNumber ONE = new InfiniteNaturalNumber("1");
+	public static final InfiniteNaturalNumber TWO = new InfiniteNaturalNumber("2");
+	public static final InfiniteNaturalNumber THREE = new InfiniteNaturalNumber("3");
+	public static final InfiniteNaturalNumber FOUR = new InfiniteNaturalNumber("4");
+	public static final InfiniteNaturalNumber FIVE = new InfiniteNaturalNumber("5");
+	public static final InfiniteNaturalNumber SIX = new InfiniteNaturalNumber("6");
+	public static final InfiniteNaturalNumber SEVEN = new InfiniteNaturalNumber("7");
+	public static final InfiniteNaturalNumber HEIGHT = new InfiniteNaturalNumber("8");
+	public static final InfiniteNaturalNumber NINE = new InfiniteNaturalNumber("9");
+
+	public static final InfiniteNaturalNumber TEN = new InfiniteNaturalNumber("10");
+	public static final InfiniteNaturalNumber ELEVEN = new InfiniteNaturalNumber("11");
+
+	public static final InfiniteNaturalNumber TWENTY = new InfiniteNaturalNumber("20");
+	public static final InfiniteNaturalNumber TWENTY_ONE = new InfiniteNaturalNumber("21");
+
+	// ordered from the bigger digit to the unit
 	private List<Byte> digits = new ArrayList<Byte>();
 
-	public InfiniteNaturalNumber() {
+	private InfiniteNaturalNumber() {
+		// this("0");
+	}
 
+	public InfiniteNaturalNumber(InfiniteNaturalNumber toClone) {
+		ArrayList<Byte> arrayList = (ArrayList<Byte>) (toClone.digits);
+		Object cloneAsObject = arrayList.clone();
+		List<Byte> clone = (List<Byte>) cloneAsObject;
+		digits = clone;
 	}
 
 	public InfiniteNaturalNumber(String asString) {
@@ -21,6 +47,154 @@ public class InfiniteNaturalNumber {
 				return;
 			}
 		}
+	}
+
+	//
+
+	public InfiniteNaturalNumber asAtLeastAsManuDigitsThan(InfiniteNaturalNumber rhs) {
+		if (getNumberOfDigits() >= rhs.getNumberOfDigits()) {
+			return this;
+		} else {
+			InfiniteNaturalNumber clone = new InfiniteNaturalNumber(this);
+			while (clone.getNumberOfDigits() < rhs.getNumberOfDigits()) {
+				clone.digits.add(0, (byte) 0);
+			}
+			return clone;
+		}
+	}
+
+	// Operations
+
+	public InfiniteNaturalNumber plus(InfiniteNaturalNumber rhs) {
+		InfiniteNaturalNumber cloneLhs = this.asAtLeastAsManuDigitsThan(rhs);
+		InfiniteNaturalNumber cloneRhs = rhs.asAtLeastAsManuDigitsThan(this);
+
+		InfiniteNaturalNumber ret = new InfiniteNaturalNumber();
+
+		boolean carry = false;
+
+		// lhs and rhs have same digit numbers
+		for (int i = 0; i < cloneLhs.getNumberOfDigits(); i++) {
+			byte sumDigits = (byte) (cloneLhs.digits.get(i) + cloneRhs.digits.get(i));
+			if (carry) {
+				sumDigits++;
+			}
+
+			if (sumDigits >= 10) {
+				sumDigits -= 10;
+				carry = true;
+			} else
+				carry = false;
+
+			ret.digits.add(sumDigits);
+		}
+
+		if (carry)
+			ret.digits.add((byte) 1);
+
+		return ret;
+	}
+
+	public InfiniteNaturalNumber minus(InfiniteNaturalNumber rhs) {
+
+		/*
+		 * if (equals(rhs)) { return ZERO; }
+		 */
+		if (this.isStrictlySmallerThan(rhs)) {
+			throw new IllegalStateException(getClass().getName() + " cannot be negative");
+		}
+
+		InfiniteNaturalNumber cloneLhs = this.asAtLeastAsManuDigitsThan(rhs);
+		InfiniteNaturalNumber cloneRhs = rhs.asAtLeastAsManuDigitsThan(this);
+
+		InfiniteNaturalNumber ret = new InfiniteNaturalNumber();
+		boolean carry = false;
+
+		for (int i = cloneLhs.getNumberOfDigits() - 1; i >= 0; i--) {
+
+			byte lhsDigit = cloneLhs.digits.get(i);
+			byte rhsDigit = cloneRhs.digits.get(i);
+
+			byte difference = (byte) (lhsDigit - rhsDigit);
+
+			if (carry)
+				difference--;
+
+			if (rhsDigit > lhsDigit) {
+				carry = true;
+				difference += 10;
+			} else {
+				carry = false;
+			}
+
+			ret.digits.add(0, difference);
+		}
+
+		if (carry)
+			ret.digits.add((byte) 1);
+
+		ret.removeUselessZeroDigits();
+		
+		return ret;
+	}
+
+	// Comparisons
+
+	public boolean isSmallerOrEqualsTo(InfiniteNaturalNumber rhs) {
+		return equals(rhs) || isStrictlySmallerThan(rhs);
+	}
+
+	public boolean isStrictlySmallerThan(InfiniteNaturalNumber rhs) {
+		if (equals(rhs))
+			return false;
+
+		if (getNumberOfDigits() < rhs.getNumberOfDigits())
+			return true;
+
+		if (getNumberOfDigits() > rhs.getNumberOfDigits())
+			return false;
+
+		return !isStrictlyGreaterThan(rhs);
+	}
+
+	public boolean isGreaterOrEqualsTo(InfiniteNaturalNumber rhs) {
+		return equals(rhs) || isStrictlyGreaterThan(rhs);
+	}
+
+	public boolean isStrictlyGreaterThan(InfiniteNaturalNumber rhs) {
+		if (equals(rhs))
+			return false;
+
+		if (getNumberOfDigits() > rhs.getNumberOfDigits())
+			return true;
+
+		if (getNumberOfDigits() < rhs.getNumberOfDigits())
+			return false;
+
+		if (getNumberOfDigits() == rhs.getNumberOfDigits()) {
+			for (int i = 0; i < getNumberOfDigits(); i++) {
+				Byte digit_lhs = digits.get(i);
+				Byte digit_rhs = rhs.digits.get(i);
+				if (digit_lhs > digit_rhs) {
+					return true;
+				}
+				if (digit_lhs < digit_rhs) {
+					return true;
+				}
+			}
+
+		}
+
+		// error
+		return false;
+	}
+
+	private void removeUselessZeroDigits() {
+
+		while (digits.size() > 1 && digits.get(0) == 0) {
+			digits.remove(0);
+		}
+
 	}
 
 	private byte digitAsByte(char digit) {
@@ -43,7 +217,7 @@ public class InfiniteNaturalNumber {
 		return false;
 	}
 
-	public int numberOfDigit() {
+	public int getNumberOfDigits() {
 		return digits.size();
 	}
 
--- Java/Numbers/src/test/numbers/InfiniteNaturalNumberTest.java
+++ Java/Numbers/src/test/numbers/InfiniteNaturalNumberTest.java
@@ -1,33 +1,147 @@
 
-
 package test.numbers;
 
-import static org.junit.Assert.*;
+import static main.numbers.InfiniteNaturalNumber.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import de.bechte.junit.runners.context.HierarchicalContextRunner;
 import main.numbers.InfiniteNaturalNumber;
 
+@RunWith(HierarchicalContextRunner.class)
 public class InfiniteNaturalNumberTest {
 
-	  @Test
-	  public void sameNumberAreEquals() {
-		  InfiniteNaturalNumber first_ten = new InfiniteNaturalNumber("10");
-		  InfiniteNaturalNumber second_ten = new InfiniteNaturalNumber("10");
-		  assertTrue(first_ten.equals(second_ten));
-	  }
-	  
-	  @Test
-	  public void tenAndTwentyAreNotEqual() {
-		  InfiniteNaturalNumber ten = new InfiniteNaturalNumber("10");
-		  InfiniteNaturalNumber twenty = new InfiniteNaturalNumber("20");
-		  assertFalse(ten.equals(twenty));
-	  }
-	  @Test
-	  public void oneAndTenAreNotEqual() {
-		  InfiniteNaturalNumber ten = new InfiniteNaturalNumber("10");
-		  InfiniteNaturalNumber one = new InfiniteNaturalNumber("1");
-		  assertFalse(ten.equals(one));
-	  }
-	
+	@Ignore
+	public class Constructor {
+
+	}
+
+	public class Addition {
+
+		@Test
+		public void onePlusOneEqualsTwo() {
+			InfiniteNaturalNumber additionResult = ONE.plus(ONE);
+			assertTrue(additionResult.equals(TWO));
+		}
+
+		@Test
+		public void onePlusTwoEqualsThree() {
+			InfiniteNaturalNumber additionResult = ONE.plus(TWO);
+			assertTrue(additionResult.equals(THREE));
+		}
+
+		@Test
+		public void onePlusTenEqualsEleven() {
+			InfiniteNaturalNumber additionResult = ONE.plus(TEN);
+			assertTrue(additionResult.equals(ELEVEN));
+		}
+
+		@Test
+		public void onePlusTwentyEqualsTwentyOne() {
+			InfiniteNaturalNumber additionResult = ONE.plus(TWENTY);
+			assertTrue(additionResult.equals(TWENTY_ONE));
+		}
+
+		@Test
+		public void withCarry_5Plus6Equals11() {
+			InfiniteNaturalNumber additionResult = FIVE.plus(SIX);
+			assertTrue(additionResult.equals(ELEVEN));
+		}
+
+		@Test
+		public void afterAdditionInitialNumbersAreUnchanged() {
+			InfiniteNaturalNumber unused_additionResult = ONE.plus(TEN);
+
+			assertTrue(ONE.getNumberOfDigits() == 1);
+			assertTrue(TEN.getNumberOfDigits() == 2);
+
+			// to avoid warning
+			assertTrue(unused_additionResult.equals(ELEVEN));
+		}
+
+	}
+
+	public class Substraction {
+
+		@Test
+		public void oneMinusOneIsZero() {
+			InfiniteNaturalNumber substractionResult = ONE.minus(ONE);
+			assertTrue(substractionResult.equals(ZERO));
+		}
+
+		@Test
+		public void tenMinusTwoIsHeight() {
+			InfiniteNaturalNumber substractionResult = TEN.minus(TWO);
+			assertTrue(substractionResult.equals(HEIGHT));
+		}
+
+	}
+
+	public class Comparison {
+		public class Equal {
+
+			@Test
+			public void sameNumberAreEquals() {
+				InfiniteNaturalNumber first_ten = new InfiniteNaturalNumber("10");
+				InfiniteNaturalNumber second_ten = new InfiniteNaturalNumber("10");
+				assertTrue(first_ten.equals(second_ten));
+			}
+
+			@Test
+			public void tenAndTwentyAreNotEqual() {
+				InfiniteNaturalNumber ten = new InfiniteNaturalNumber("10");
+				InfiniteNaturalNumber twenty = new InfiniteNaturalNumber("20");
+				assertFalse(ten.equals(twenty));
+			}
+
+			@Test
+			public void oneAndTenAreNotEqual() {
+				InfiniteNaturalNumber ten = new InfiniteNaturalNumber("10");
+				InfiniteNaturalNumber one = new InfiniteNaturalNumber("1");
+				assertFalse(ten.equals(one));
+			}
+
+		}
+
+		public class StrictlyGreater {
+
+			@Test
+			public void twoIsStricltlyGreaterThanOne() {
+				InfiniteNaturalNumber one = new InfiniteNaturalNumber("1");
+				InfiniteNaturalNumber two = new InfiniteNaturalNumber("2");
+				assertTrue(two.isStrictlyGreaterThan(one));
+			}
+
+			@Test
+			public void tenIsStricltlyGreaterThanOne() {
+				InfiniteNaturalNumber one = new InfiniteNaturalNumber("1");
+				InfiniteNaturalNumber ten = new InfiniteNaturalNumber("10");
+				assertTrue(ten.isStrictlyGreaterThan(one));
+			}
+
+			@Test
+			public void twentyTwoIsStricltlyGreaterThanEleven() {
+				InfiniteNaturalNumber twentyOne = new InfiniteNaturalNumber("21");
+				InfiniteNaturalNumber eleven = new InfiniteNaturalNumber("11");
+				assertTrue(twentyOne.isStrictlyGreaterThan(eleven));
+			}
+
+			@Test
+			public void tenIsNotStricltlyGreaterThanTen_sameInstance() {
+				InfiniteNaturalNumber ten = new InfiniteNaturalNumber("10");
+				assertFalse(ten.isStrictlyGreaterThan(ten));
+			}
+
+			@Test
+			public void tenIsNotStricltlyGreaterThanTen_differentInstances() {
+				InfiniteNaturalNumber ten_instance1 = new InfiniteNaturalNumber("10");
+				InfiniteNaturalNumber ten_instance2 = new InfiniteNaturalNumber("10");
+				assertFalse(ten_instance1.isStrictlyGreaterThan(ten_instance2));
+			}
+		}
+	}
 }
