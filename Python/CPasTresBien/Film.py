2db0dd9132be6fffe9f26939df9086d6e17501bc
yanndanielou <yanndu91@msn.com>
2018-06-03 12:37:40 +0200

    Traitement titre

--- Python/CPasTresBien/Film.py
+++ Python/CPasTresBien/Film.py
@@ -18,7 +18,38 @@ class Film:
 	def __init__(self, title, title2, lien, poids, seed_ok_up, down):
 		"""Constructeur de notre classe"""
 				
-		self.title_in_windows_format = title.replace("(","").replace(")","").replace("'"," ").replace("?","").replace("/","").replace("\\xc3","e").replace("\\exa8","e").replace("\\","")
+		self.title_in_windows_format = title.replace("(","")
+		self.title_in_windows_format = self.title_in_windows_format.replace(")","")
+		self.title_in_windows_format = self.title_in_windows_format.replace("\\xc3\\xb4","o")
+		self.title_in_windows_format = self.title_in_windows_format.replace("\\xc3\\xa2","a")
+		self.title_in_windows_format = self.title_in_windows_format.replace("\\xc3\\xaf","i")
+		self.title_in_windows_format = self.title_in_windows_format.replace("\\xc3\\xbb","u")
+		self.title_in_windows_format = self.title_in_windows_format.replace("\\'"," ")
+		self.title_in_windows_format = self.title_in_windows_format.replace("'"," ")
+		self.title_in_windows_format = self.title_in_windows_format.replace("?","")
+		self.title_in_windows_format = self.title_in_windows_format.replace("/","")
+		self.title_in_windows_format = self.title_in_windows_format.replace("\\xc3\\xa8","e")
+		self.title_in_windows_format = self.title_in_windows_format.replace("\\xc3\\xa0","a")
+		self.title_in_windows_format = self.title_in_windows_format.replace("\\xc3\\xa9","e")
+		self.title_in_windows_format = self.title_in_windows_format.replace("\\xc3\\xa9","e")
+		self.title_in_windows_format = self.title_in_windows_format.replace("\\xc3\\x8a","E")
+		self.title_in_windows_format = self.title_in_windows_format.replace("\xc3\xa9","e")
+		self.title_in_windows_format = self.title_in_windows_format.replace("\\xc3\\x94","O")
+		self.title_in_windows_format = self.title_in_windows_format.replace("\\xc5\\x93","o")
+		self.title_in_windows_format = self.title_in_windows_format.replace("\\xc3\\x80","A")
+		self.title_in_windows_format = self.title_in_windows_format.replace("\\xc3\\x87","C")
+		self.title_in_windows_format = self.title_in_windows_format.replace("\\xc3\\x89","E")
+		self.title_in_windows_format = self.title_in_windows_format.replace("\\xb0","°")
+		self.title_in_windows_format = self.title_in_windows_format.replace("\\xc2","A")
+		self.title_in_windows_format = self.title_in_windows_format.replace("\\xc3","e")
+		self.title_in_windows_format = self.title_in_windows_format.replace("\\exa8","e")
+
+
+
+
+
+
+
 		self.title = title
 		self.title2 = title2
 		self.lien = lien
