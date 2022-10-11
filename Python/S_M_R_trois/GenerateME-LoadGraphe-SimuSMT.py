18a24281435448c10be98b826542024dbd3f6e28
yanndanielou <yanndanielou@users.noreply.github.com>
2022-10-11 22:04:16 +0200

    Update S_M_R_trois

--- Python/S_M_R_trois/GenerateME-LoadGraphe-SimuSMT.py
+++ Python/S_M_R_trois/GenerateME-LoadGraphe-SimuSMT.py
@@ -1,4 +1,9 @@
-﻿import os
+﻿#For logs
+import logging
+import time
+import param
+
+import os
 import sys
 sys.path.append("..")
 
@@ -7,6 +12,36 @@ from GenerateMEDataModel import SimulationResultsSingleton
 from GenerateMEDataModel import Graphe
 from GenerateMEDataModel import PT2ADir
 from GenerateMEDataModel import remove_proxies
+
+def printOnly(toPrint):
+    log_timestamp = time.asctime( time.localtime(time.time()))
+    print(log_timestamp + '\t' + toPrint)
+
+def printAndLog(toPrintAndLog):
+    printOnly(toPrintAndLog)
+    logging.info(toPrintAndLog)
+
+def configureLogger():
+    logger_directory = "logs"
+    
+    if not os.path.exists(logger_directory):
+        os.makedirs(logger_directory)
+    
+    logger_level = param.logger_level
+    
+    print("Logger level:" +str(logger_level))
+    
+    logging.basicConfig(level=logger_level,
+                        format='%(asctime)s %(levelname)-8s %(message)s',
+                        datefmt='%a, %d %b %Y %H:%M:%S',
+                        filename=logger_directory+'\SMT_trois.log',
+                        filemode='w')
+    #logging.debug
+    #logging.info
+    #logging.warning
+    #logging.error
+    #logging.critical
+    
 
 def testAdonfImport():
     pointsOptimisationsCBTC = ['HAUSSMANN_SL_V31', 'HAUSSMANN_SL_V32', 'HAUSSMANN_SL_V33', 'HAUSSMANN_SL_V34', 'MAGENTA_V51', 'MAGENTA_V52', 'MAGENTA_V53', 'MAGENTA_V54', 'PTA_SDP_T3', 'ROSA_PARKS_V1', 'ROSA_PARKS_V2', 'PANTIN_V1EOLE', 'PANTIN_V2EOLE']
@@ -32,33 +67,40 @@ def testAdonfImport():
 
     __graphe = GrapheSingleton.Load(GrapheSingleton,"D:\\SMT3_generation\\SMT3\\Graphe-NG_ReadyForSimu.gme")
 
-    print("Nombre de missions élémentaires de régulation : " + str(len(__graphe.missionsElementairesRegulation)))
-    print("Nombre de missions élémentaires : " + str(len(__graphe.missionsElementaires)))
+    printAndLog("Nombre de missions élémentaires de régulation : " + str(len(__graphe.missionsElementairesRegulation)))
+    printAndLog("Nombre de missions élémentaires : " + str(len(__graphe.missionsElementaires)))
 
-    print("Load empty Simulation.sme") 
-    simuResults = SimulationResultsSingleton.Load(SimulationResultsSingleton, "D:\\SMT3_generation\\SMT3\\Simulation.sme")
+    printAndLog("Load empty Simulation.sme") 
+    simuResults = SimulationResultsSingleton.Load(SimulationResultsSingleton, "D:\\SMT3_generation\\SMT3\\Simulation_empty.sme")
+
+
+#      simuResults = SimulationResultsSingleton.Load(SimulationResultsSingleton, "D:\\SMT3_generation\\SMT3\\Simulation.sme")
 
     ignoredMER = ['TRAMTRAIN_P27_1TER_INOUT|NOISY_K_P27_1RN_INOUT']
-    print("ignoredMER : " + str(ignoredMER)) 
+    printAndLog("ignoredMER : " + str(ignoredMER)) 
 
-    print("ProduireSimplesRuns") 
-    __graphe.ProduireSimplesRuns("http://127.0.0.1:8080", 0.4, 30.0, "D:\\SMT3_generation\\SMT3\\Simulation.sme",750,1.1,ignoredMER)
+    printAndLog("ProduireSimplesRuns") 
+    pas_sauvegarde = 10
+    __graphe.ProduireSimplesRuns("http://127.0.0.1:8080", 0.4, 30.0, "D:\\SMT3_generation\\SMT3\\Simulation_output.sme",pas_sauvegarde,1.1,ignoredMER)
   
-    print("ExporterSimplesRunsSimulations") 
+    printAndLog("ExporterSimplesRunsSimulations") 
     simuResults.ExporterSimplesRunsSimulations("D:\\SMT3_generation\\SMT3\\SimplesRunsSimulationsResults.csv")
  
     #__graphe.EstimerNombreDeSimulation()
 
-    print("Liste des points de contrôle")
+    printAndLog("Liste des points de contrôle")
     for i in sorted (__graphe.pointsDeControle.keys()) :
-        print(i)
+        printAndLog(i)
 
-    print("Liste des transitions")
+    printAndLog("Liste des transitions")
     for i in __graphe.transitions.values():
         i.print()
 
 def main():
+    configureLogger()
+    printAndLog('Start application')
     testAdonfImport()
+    printAndLog('End application')
 
 if __name__ == '__main__':
     main()
