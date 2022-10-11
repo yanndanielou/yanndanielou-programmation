cfcd6225266e7afc0f751ed578ccf92aef88624a
yanndanielou <yanndanielou@users.noreply.github.com>
2022-10-11 22:03:33 +0200

    Update ArreterSimulationSmtTroisRienNeBouge.py

--- Python/ArreterSimulationSmtTroisRienNeBouge/ArreterSimulationSmtTroisRienNeBouge.py
+++ Python/ArreterSimulationSmtTroisRienNeBouge/ArreterSimulationSmtTroisRienNeBouge.py
@@ -25,21 +25,31 @@ def window_SMT3_blocked_detected(window_SMT3_blocked):
     time.sleep(5) 
     LoggerConfig.printAndLogInfo("Press enter to close window")
     pyautogui.hotkey('enter') 
-                
+
+def infinite_loop_detect_SMT3_blocked_popup():
+    title_fenetre_blocante_smt3 = "Rien ne bouge.."
+    list_of_windows_SMT3_blocked=pyautogui.getWindowsWithTitle(title_fenetre_blocante_smt3)
+
+    while True:
+        logging.debug("There are " + str(len(list_of_windows_SMT3_blocked)) + " windows with title " + title_fenetre_blocante_smt3)
+        if len(list_of_windows_SMT3_blocked) > 0:
+            LoggerConfig.printAndLogInfo("There are " + str(len(list_of_windows_SMT3_blocked)) + " windows with title " + title_fenetre_blocante_smt3)
+            for window_SMT3_blocked in list_of_windows_SMT3_blocked:
+                window_SMT3_blocked_detected(window_SMT3_blocked)
+
+        logging.debug("Begin to wait")
+        
+        time.sleep(30)
+
 def main(argv):
     
     log_file_name = 'ArreterSimulationSmtTroisRienNeBouge' + "." +  str(random.randrange(100)) + ".log"
     LoggerConfig.configureLogger(log_file_name)    
-    
-    title_fenetre_blocante_smt3 = "Rien ne bouge.."
-    list_of_windows_SMT3_blocked=pyautogui.getWindowsWithTitle(title_fenetre_blocante_smt3)
+    LoggerConfig.printAndLogInfo("Application start")
 
-    logging.debug("There are " + str(len(list_of_windows_SMT3_blocked)) + " windows with title " + title_fenetre_blocante_smt3)
-    if len(list_of_windows_SMT3_blocked) > 0:
-        LoggerConfig.printAndLogInfo("There are " + str(len(list_of_windows_SMT3_blocked)) + " windows with title " + title_fenetre_blocante_smt3)
-        for window_SMT3_blocked in list_of_windows_SMT3_blocked:
-            window_SMT3_blocked_detected(window_SMT3_blocked)
+    infinite_loop_detect_SMT3_blocked_popup()
 
+    LoggerConfig.printAndLogInfo("Application ended")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
