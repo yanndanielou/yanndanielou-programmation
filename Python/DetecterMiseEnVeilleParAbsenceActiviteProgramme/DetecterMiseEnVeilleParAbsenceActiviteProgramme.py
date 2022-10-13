80fa8b45bc7d1df3128cd498b431c811fef83bd1
yanndanielou <yanndanielou@users.noreply.github.com>
2022-10-13 21:57:12 +0200

    Update DetecterMiseEnVeilleParAbsenceActiviteProgramme.py

--- Python/DetecterMiseEnVeilleParAbsenceActiviteProgramme/DetecterMiseEnVeilleParAbsenceActiviteProgramme.py
+++ Python/DetecterMiseEnVeilleParAbsenceActiviteProgramme/DetecterMiseEnVeilleParAbsenceActiviteProgramme.py
@@ -24,7 +24,7 @@ def infinite_loop_detect_mise_en_veille_par_absence_activite():
         time_before_wait = time.time() # repris juste apres la precedente mesure pour detecter une veille n'importe où dans le programme
         if duree_wait_realisee > 2*duree_attente_theorique_en_secondes:
             #LoggerConfig.printAndLogInfo("Mise en veille du PC detectee, aux environs de :" + time.localtime(time_before_wait).strftime("%m/%d/%Y, %H:%M:%S") + " avec reprise aux environs de : " + time.localtime(time_before_wait).strftime("%m/%d/%Y, %H:%M:%S") + " . Duree de la veille estimee: " + str(timedelta(seconds=duree_wait_realisee)))
-            LoggerConfig.printAndLogInfo("Mise en veille du PC detectee, aux environs de :" + str(time.localtime(time_before_wait)) + " avec reprise aux environs de : " + str(time.localtime(time_before_wait)) + " . Duree de la veille estimee: " + str(timedelta(seconds=duree_wait_realisee)))
+            LoggerConfig.printAndLogInfo("Mise en veille du PC detectee, aux environs de :" + time.strftime('%Y-%m-%dT%H:%M:%SZ', time.localtime(time_before_wait)) + " avec reprise aux environs de : " + time.strftime('%Y-%m-%dT%H:%M:%SZ', time.localtime(time_after_wait)) + " . Duree de la veille estimee: " + str(timedelta(seconds=duree_wait_realisee)))
         #else:
         #    logging.debug("Pas de mise en veille du PC detectee, time_before_wait: " + time.localtime(time_before_wait).strftime("%m/%d/%Y, %H:%M:%S") + " time_before_wait: " + time.localtime(time_before_wait).strftime("%m/%d/%Y, %H:%M:%S") + " . Duree de la non veille estimee: " + str(timedelta(seconds=duree_wait_realisee)))
  
