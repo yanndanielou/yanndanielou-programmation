93e43f7ace2cc07febb208c4bf1d952242bb5f0d
yanndanielou <yanndanielou@users.noreply.github.com>
2022-10-13 22:49:32 +0200

    Create ProduireSimplesRunsAndExporterSimplesRunsSimulations-8080.py

---
+++ Python/S_M_R_trois/ProduireSimplesRunsAndExporterSimplesRunsSimulations-8080.py
@@ -0,0 +1,11 @@
+﻿import ProduireSimplesRunsAndExporterSimplesRunsSimulations
+
+smt3_port=8080
+numero_premiere_mission_elementaire_a_traiter = 1
+numero_derniere_mission_elementaire_a_traiter = 1000
+
+def main():
+    ProduireSimplesRunsAndExporterSimplesRunsSimulations.launch(smt3_port, numero_premiere_mission_elementaire_a_traiter, numero_derniere_mission_elementaire_a_traiter)
+
+if __name__ == '__main__':
+    main()
