92667e1ec7fb9d6cb5a30298ec38a9a1692dcae9
yanndanielou <yanndanielou@users.noreply.github.com>
2022-10-19 14:24:08 +0200

    Update launch_DetecterMiseEnVeilleParAbsenceActiviteProgramme.bat

--- Python/DetecterMiseEnVeilleParAbsenceActiviteProgramme/launch_DetecterMiseEnVeilleParAbsenceActiviteProgramme.bat
+++ Python/DetecterMiseEnVeilleParAbsenceActiviteProgramme/launch_DetecterMiseEnVeilleParAbsenceActiviteProgramme.bat
@@ -1,2 +1,3 @@
+Title Launch launch_DetecterMiseEnVeilleParAbsenceActiviteProgramme %DATE% %TIME%
 call C:\Users\Yann\AppData\Local\Programs\Python\Python39\python.exe DetecterMiseEnVeilleParAbsenceActiviteProgramme.py
 timeout /t 30
