f009f475d74a88185caea679bba669981286a229
yanndanielou <yanndanielou@users.noreply.github.com>
2022-10-19 14:24:57 +0200

    Update launch_ArreterSimulationSmtTroisRienNeBouge.bat

--- Python/ArreterSimulationSmtTroisRienNeBouge/launch_ArreterSimulationSmtTroisRienNeBouge.bat
+++ Python/ArreterSimulationSmtTroisRienNeBouge/launch_ArreterSimulationSmtTroisRienNeBouge.bat
@@ -1,7 +1,7 @@
 
-rem cd C:\Users\Yann\AppData\Local\Programs\Python\Python39
 :INFINITE_LOOP
 ECHO %DATE% %TIME% Start ArreterSimulationSmtTroisRienNeBouge.py >> logs\launch_ArreterSimulationSmtTroisRienNeBouge.log
+Title Launch ArreterSimulationSmtTroisRienNeBouge %DATE% %TIME%
 call C:\Users\Yann\AppData\Local\Programs\Python\Python39\python.exe ArreterSimulationSmtTroisRienNeBouge.py >> logs\launch_ArreterSimulationSmtTroisRienNeBouge.log 2>launch_ArreterSimulationSmtTroisRienNeBouge.error_log
 timeout /t 30
 GOTO :INFINITE_LOOP
