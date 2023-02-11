041b18051ce8e1431d8169c56c062058639db75f
yanndanielou <yanndanielou@users.noreply.github.com>
2023-02-11 22:14:09 +0100

    Update load_all_data.m

--- Matlab/SMT2_Data_high_performances/Sources/load_all_data.m
+++ Matlab/SMT2_Data_high_performances/Sources/load_all_data.m
@@ -2,24 +2,109 @@ function load_all_data(varargin)
 
 disp(string(datetime) + " load_all_data begin");
 
-affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_projet.m");
-affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Seg_Aig.m");
-affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Cv_Lvisi.m");
-affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_CdV.m");
-affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Quai_Paf.m");
-affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE.m");
-affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mB.m");
-affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Pente.m");
-affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Courbure.m");
-affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Aerien.m");
-affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Balise.m");
-affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Vitesses.m");
-affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Vitesses_mB.m");
-affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Regul.m");
-affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Modele.m");
-affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_TSAZ.m");
-affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Zinh.m");
-affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_PC_ATS.m");
+%affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_projet.m");
+%affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Seg_Aig.m");
+%affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Cv_Lvisi.m");
+%affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_CdV.m");
+%affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Quai_Paf.m");
+
+%affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE.m");
+
+%affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_001_initialisation_structures.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_002.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_003.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_004.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_005.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_006.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_007.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_008.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_009.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_010.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_011.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_012.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_013.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_014.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_015.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_016.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_017.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_018.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_019.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_020.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_021.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_022.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_023.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_024.m");
+%execute_file_as_matlab_content("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_025.m");
+%affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mE_part_100_fill_SMT_mE_feu_BAL.m");
+%
+%
+%
+%affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_mB.m");
+%affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Pente.m");
+%affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Courbure.m");
+%affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Aerien.m");
+%affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Balise.m");
+%affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Vitesses.m");
+%affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Vitesses_mB.m");
+%affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Regul.m");
+%affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Modele.m");
+%affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_TSAZ.m");
+%affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_Zinh.m");
+%affectation_variables_globales_to_test_load_data("D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits\SMT2_Data_PC_ATS.m");
+
+
+disp(string(datetime) + " SMT2_Data_mE_part_001_initialisation_structures");
+SMT2_Data_mE_part_001_initialisation_structures;
+disp(string(datetime) + " SMT2_Data_mE_part_002");
+SMT2_Data_mE_part_002;
+disp(string(datetime) + " SMT2_Data_mE_part_003");
+SMT2_Data_mE_part_003;
+disp(string(datetime) + " SMT2_Data_mE_part_004");
+SMT2_Data_mE_part_004;
+disp(string(datetime) + " SMT2_Data_mE_part_005");
+SMT2_Data_mE_part_005;
+disp(string(datetime) + " SMT2_Data_mE_part_006");
+SMT2_Data_mE_part_006;
+disp(string(datetime) + " SMT2_Data_mE_part_007");
+SMT2_Data_mE_part_007;
+disp(string(datetime) + " SMT2_Data_mE_part_008");
+SMT2_Data_mE_part_008;
+disp(string(datetime) + " SMT2_Data_mE_part_009");
+SMT2_Data_mE_part_009;
+disp(string(datetime) + " SMT2_Data_mE_part_010");
+SMT2_Data_mE_part_010;
+disp(string(datetime) + " SMT2_Data_mE_part_011");
+SMT2_Data_mE_part_011;
+disp(string(datetime) + " SMT2_Data_mE_part_012");
+SMT2_Data_mE_part_012;
+disp(string(datetime) + " SMT2_Data_mE_part_013");
+SMT2_Data_mE_part_013;
+disp(string(datetime) + " SMT2_Data_mE_part_014");
+SMT2_Data_mE_part_014;
+disp(string(datetime) + " SMT2_Data_mE_part_015");
+SMT2_Data_mE_part_015;
+disp(string(datetime) + " SMT2_Data_mE_part_016");
+SMT2_Data_mE_part_016;
+disp(string(datetime) + " SMT2_Data_mE_part_017");
+SMT2_Data_mE_part_017;
+disp(string(datetime) + " SMT2_Data_mE_part_018");
+SMT2_Data_mE_part_018;
+disp(string(datetime) + " SMT2_Data_mE_part_019");
+SMT2_Data_mE_part_019;
+disp(string(datetime) + " SMT2_Data_mE_part_020");
+SMT2_Data_mE_part_020;
+disp(string(datetime) + " SMT2_Data_mE_part_021");
+SMT2_Data_mE_part_021;
+disp(string(datetime) + " SMT2_Data_mE_part_022");
+SMT2_Data_mE_part_022;
+disp(string(datetime) + " SMT2_Data_mE_part_023");
+SMT2_Data_mE_part_023;
+disp(string(datetime) + " SMT2_Data_mE_part_024");
+SMT2_Data_mE_part_024;
+disp(string(datetime) + " SMT2_Data_mE_part_025");
+SMT2_Data_mE_part_025;
+disp(string(datetime) + " SMT2_Data_mE_part_100_fill_SMT_mE_feu_BAL");
+SMT2_Data_mE_part_100_fill_SMT_mE_feu_BAL;
 
 
 disp(string(datetime) + " load_all_data end");
