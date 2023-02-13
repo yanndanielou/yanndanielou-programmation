be80bfb55f43ebd174d41324c5945f2aa8d35ad9
yanndanielou <yanndanielou@users.noreply.github.com>
2023-02-13 13:53:58 +0100

    Create launch.bat

---
+++ Python/S_M_R_trois_launcher/Jeu_de_donnees_de_tests/2022-10/launch.bat
@@ -0,0 +1,2 @@
+CD Exe
+Smt3_ATSPlus_D5_2_11_8080.exe "D:\Pour_Yann_Melnotte\SMT3_Package\Fichiers .m extraits" -spdef smt_server.base_uri.port=8080 -Dlog4j.configurationFile=./log4j2-smtserver2.xml -Dvicos.cbtc.logger.folder.path=./logs/  1>logs/app_8080.log
