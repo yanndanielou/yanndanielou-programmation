68cb6a382cd812e54935e3aa29bd5430bf97c6b2
yanndanielou <yanndanielou@users.noreply.github.com>
2023-02-13 22:10:36 +0100

    Create SMT2_Data_mE_fichier_m_affectation_variables_globales.m

---
+++ Matlab/SMT2_Data_mE_fichier_texte/Sources/SMT2_Data_mE_fichier_m_affectation_variables_globales.m
@@ -0,0 +1,11 @@
+function SMT2_Data_mE_fichier_texte(varargin)
+
+disp(string(datetime) + " SMT2_Data_mE_fichier_texte begin");
+
+affectation_variables_globales_to_test_load_data("D:\GitHub\yanndanielou-programmation\Matlab\SMT2_Data_mE_fichier_texte\Sources\fichiers_m_generes\SMT2_Data_mE_part_001_initialisation_structures.m");
+affectation_variables_globales_to_test_load_data("D:\GitHub\yanndanielou-programmation\Matlab\SMT2_Data_mE_fichier_texte\Sources\fichiers_txt_generes\SMT2_Data_mE_part_002.m");
+
+disp(string(datetime) + " SMT2_Data_mE_fichier_texte end");
+
+return
+end
