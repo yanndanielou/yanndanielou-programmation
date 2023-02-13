812e1e77968afab30fc373a8619d422677c64a8e
yanndanielou <yanndanielou@users.noreply.github.com>
2023-02-13 22:22:16 +0100

    Update SMT2_Data_mE_fichier_m_affectation_variables_globales.m

--- Matlab/SMT2_Data_mE_fichier_texte/Sources/SMT2_Data_mE_fichier_m_affectation_variables_globales.m
+++ Matlab/SMT2_Data_mE_fichier_texte/Sources/SMT2_Data_mE_fichier_m_affectation_variables_globales.m
@@ -1,11 +1,11 @@
-function SMT2_Data_mE_fichier_texte(varargin)
+function SMT2_Data_mE_fichier_m_affectation_variables_globales(varargin)
 
-disp(string(datetime) + " SMT2_Data_mE_fichier_texte begin");
+disp(string(datetime) + " SMT2_Data_mE_fichier_m_affectation_variables_globales begin");
 
 affectation_variables_globales_to_test_load_data("D:\GitHub\yanndanielou-programmation\Matlab\SMT2_Data_mE_fichier_texte\Sources\fichiers_m_generes\SMT2_Data_mE_part_001_initialisation_structures.m");
-affectation_variables_globales_to_test_load_data("D:\GitHub\yanndanielou-programmation\Matlab\SMT2_Data_mE_fichier_texte\Sources\fichiers_txt_generes\SMT2_Data_mE_part_002.m");
+affectation_variables_globales_to_test_load_data("D:\GitHub\yanndanielou-programmation\Matlab\SMT2_Data_mE_fichier_texte\Sources\fichiers_m_generes\SMT2_Data_mE_part_002.m");
 
-disp(string(datetime) + " SMT2_Data_mE_fichier_texte end");
+disp(string(datetime) + " SMT2_Data_mE_fichier_m_affectation_variables_globales end");
 
 return
 end
