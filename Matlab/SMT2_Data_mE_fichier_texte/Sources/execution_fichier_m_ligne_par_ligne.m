6eb774ec5aca18c433db76ecec186c2240f9751a
yanndanielou <yanndanielou@users.noreply.github.com>
2023-02-13 22:16:36 +0100

    Create execution_fichier_m_ligne_par_ligne.m

---
+++ Matlab/SMT2_Data_mE_fichier_texte/Sources/execution_fichier_m_ligne_par_ligne.m
@@ -0,0 +1,17 @@
+function execution_fichier_m_ligne_par_ligne(nom_fichier)
+
+disp(string(datetime) + " execution_fichier_m_ligne_par_ligne of file " + nom_fichier + " begin");
+execution_fichier_m_ligne_par_ligne_begin_time = datetime;
+
+
+fid = fopen(nom_fichier);
+list_var_global{:,1} = [];
+ligne_list_var_global = 1;
+tline = fgetl(fid);
+while ischar(tline)
+    tline = fgetl(fid);
+    evalc(tline);
+end
+
+enddisp(string(datetime) + " execution_fichier_m_ligne_par_ligne of file " + nom_fichier + " end."  + " Time elapsed in fonction:" + string(datetime - execution_fichier_m_ligne_par_ligne_begin_time));
+end
