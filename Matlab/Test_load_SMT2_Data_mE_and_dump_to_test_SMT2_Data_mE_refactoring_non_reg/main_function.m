855bc46e1cbfb335fc87938c0737cd1592a96139
yanndanielou <yanndanielou@users.noreply.github.com>
2023-02-15 23:57:37 +0100

    Update main_function.m

--- Matlab/Test_load_SMT2_Data_mE_and_dump_to_test_SMT2_Data_mE_refactoring_non_reg/main_function.m
+++ Matlab/Test_load_SMT2_Data_mE_and_dump_to_test_SMT2_Data_mE_refactoring_non_reg/main_function.m
@@ -5,14 +5,15 @@ application_launch_time = datetime;
 
 global SMT_mE_aig
 
+disp("Load initial structure");
+
 SMT2_Data_mE_only_create_initial_SMT_mE_aig;
 
-output_text_file_with_structure_content = ""
 
-disp("Initial structure");
-print_SMT_mE_aig_content(SMT_mE_aig, output_text_file_with_structure_content);
+disp("Print initial structure:");
+%print_SMT_mE_aig_content(SMT_mE_aig);
 
-nom_fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne = "fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne.m"
+nom_fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne = "fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne.m";
 if isfile(nom_fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne)
 	disp(string(datetime) + " " + "optionnal nom_fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne file " + nom_fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne + " exists: will be parsed by execution_fichier_m_ligne_par_ligne function." + " (Time since application startup:" + (string(datetime - application_launch_time)) + ")");
 	execution_fichier_m_ligne_par_ligne(nom_fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne);
@@ -20,7 +21,7 @@ else
 	disp(string(datetime) + " " + "optionnal nom_fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne file " + nom_fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne + " does not exist: step skipped." + " (Time since application startup:" + (string(datetime - application_launch_time)) + ")");
 end
 
-nom_fichier_a_executer_dans_affectation_variables_globales = "fichier_a_executer_dans_affectation_variables_globales.m"
+nom_fichier_a_executer_dans_affectation_variables_globales = "fichier_a_executer_dans_affectation_variables_globales.m";
 if isfile(nom_fichier_a_executer_dans_affectation_variables_globales)
 	disp(string(datetime) + " " + "optionnal nom_fichier_a_executer_dans_affectation_variables_globales file " + nom_fichier_a_executer_dans_affectation_variables_globales + " exists: will be parsed by affectation_variables_globales function." + " (Time since application startup:" + (string(datetime - application_launch_time)) + ")");
 	affectation_variables_globales(nom_fichier_a_executer_dans_affectation_variables_globales);
@@ -29,7 +30,9 @@ else
 end
 
 
-disp("Initial structure");
+
+disp("Print final structure after changes:");
+%print_SMT_mE_aig_content(SMT_mE_aig);
 
 
 return
