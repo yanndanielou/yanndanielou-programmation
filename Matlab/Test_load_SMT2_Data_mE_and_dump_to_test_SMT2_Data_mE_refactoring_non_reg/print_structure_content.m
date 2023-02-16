c4a1a351127091d90ab84cb259d730fc50627c48
yanndanielou <yanndanielou@users.noreply.github.com>
2023-02-16 07:29:36 +0100

    Create print_structure_content.m

---
+++ Matlab/Test_load_SMT2_Data_mE_and_dump_to_test_SMT2_Data_mE_refactoring_non_reg/print_structure_content.m
@@ -0,0 +1,19 @@
+function print_structure_content(structure_to_print, structure_name)
+
+disp(string(datetime) + " print_structure_content " + structure_name + " begin");
+
+disp(string(datetime) + " structure " + structure_name + " fields:");
+
+disp(string(datetime) + " structure " + structure_name + " size:" + length(structure_to_print));
+disp(structure_to_print);
+
+
+for structure_it = 1 : length(structure_to_print)
+
+	disp(structure_name + " at " +  structure_it + " : ");
+	disp(structure_to_print(structure_it));
+
+end
+
+disp(string(datetime) + " print_structure_content " + structure_name + " end")
+end
