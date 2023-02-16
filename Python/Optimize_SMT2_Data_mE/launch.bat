1f84d3fb79cffec438e76141657b6e0503ae97de
yanndanielou <yanndanielou@users.noreply.github.com>
2023-02-16 15:14:36 +0100

    Update launch.bat

--- Python/Optimize_SMT2_Data_mE/launch.bat
+++ Python/Optimize_SMT2_Data_mE/launch.bat
@@ -1,4 +1,8 @@
 SET python_full_path="%LOCALAPPDATA%\Programs\Python\Python310\python.exe"
+IF NOT EXIST %python_full_path% SET python_full_path="%LOCALAPPDATA%\Programs\Python\Python39\python.exe"
+
+echo python_full_path : %python_full_path%
+
 %python_full_path% Optimize_SMT2_Data_mE.py
 
 timeout /t 30
