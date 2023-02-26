1cc79bcaa5938d28c59a19acdd9544bcac8e52fe
yanndanielou <yanndanielou@users.noreply.github.com>
2023-02-26 17:44:04 +0100

    Update Test_load_SMT2_Data_mE_contents.m

--- Matlab/Modify_SMT2_Data_mE_and_print_content/Test_load_SMT2_Data_mE_contents.m
+++ Matlab/Modify_SMT2_Data_mE_and_print_content/Test_load_SMT2_Data_mE_contents.m
@@ -20,7 +20,7 @@ end
 %print_structure_content(SMT_mE_aig,"SMT_mE_aig");
 %print_structure_content(SMT_mE_feu_BAL,"SMT_mE_feu_BAL");
 
-nom_fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne = "input_files\fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne.m";
+nom_fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne = "input_files\\fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne.m";
 if isfile(nom_fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne)
 	disp(string(datetime) + " " + "optionnal nom_fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne file " + nom_fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne + " exists: will be parsed by execution_fichier_m_ligne_par_ligne function." + " (Time since application startup:" + (string(datetime - application_launch_time)) + ")");
 	execution_fichier_m_ligne_par_ligne(nom_fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne);
@@ -28,7 +28,7 @@ else
 	disp(string(datetime) + " " + "optionnal nom_fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne file " + nom_fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne + " does not exist: step skipped." + " (Time since application startup:" + (string(datetime - application_launch_time)) + ")");
 end
 
-nom_fichier_a_executer_dans_affectation_variables_globales = "input_files\fichier_a_executer_dans_affectation_variables_globales.m";
+nom_fichier_a_executer_dans_affectation_variables_globales = "input_files\\fichier_a_executer_dans_affectation_variables_globales.m";
 if isfile(nom_fichier_a_executer_dans_affectation_variables_globales)
 	disp(string(datetime) + " " + "optionnal nom_fichier_a_executer_dans_affectation_variables_globales file " + nom_fichier_a_executer_dans_affectation_variables_globales + " exists: will be parsed by affectation_variables_globales function." + " (Time since application startup:" + (string(datetime - application_launch_time)) + ")");
 	affectation_variables_globales(nom_fichier_a_executer_dans_affectation_variables_globales);
