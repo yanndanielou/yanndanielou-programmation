781265da3af59cbf96bf180b6dcf1f5e0538fd18
yanndanielou <yanndanielou@users.noreply.github.com>
2023-02-26 14:40:41 +0100

    Update Optimize_SMT2_Data_mE.py

--- Python/Optimize_SMT2_Data_mE/Optimize_SMT2_Data_mE.py
+++ Python/Optimize_SMT2_Data_mE/Optimize_SMT2_Data_mE.py
@@ -228,7 +228,7 @@ class StructureModificationInstruction:
         #Rubular
         #([0-9A-Za-z]*)[(](\d+)[)][.]([0-9A-Za-z_\"]*)[(](\d+)[,](\d+)[)]\s[=]\s(\d+)[;]
 
-        structure_modification_instruction_line_regex_as_string = "(?P<structure_name>[0-9A-Za-z_]*)[(](?P<structure_index>\\d+)[)][.](?P<field_name>[0-9A-Za-z_]*)[(](?P<field_index_1>\\d+)[,](?P<field_index_2>\\d+)[)]\s[=]\s(?P<new_value>\\d+)[;]"
+        structure_modification_instruction_line_regex_as_string = "(?P<structure_name>[0-9A-Za-z_]*)[(](?P<structure_index>\\d+)[)][.](?P<field_name>[0-9A-Za-z_]*)[(](?P<field_index_1>\\d+)[,](?P<field_index_2>\\d+)[)]\s[=]\s(?P<new_value>-?\\d+)[;]"
         structure_modification_instruction_line_regex_compiled = re.compile(structure_modification_instruction_line_regex_as_string)
         match_result = structure_modification_instruction_line_regex_compiled.match(full_content_as_string)
         #self.value_as_string = full_content_as_string.split("=")[1].strip()
