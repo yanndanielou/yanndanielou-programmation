2c17815b851732c6a6bdffd7cd0b3b213f87f59f
yanndanielou <yanndanielou@users.noreply.github.com>
2023-02-26 18:38:24 +0100

    Update fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne.m

--- Matlab/Modify_SMT2_Data_mE_and_print_content/input_files/fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne.m
+++ Matlab/Modify_SMT2_Data_mE_and_print_content/input_files/fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne.m
@@ -1,6 +1,11 @@
+global SMT_mE_aig
+
 SMT_mE_aig(5).aig_asso(3).no(1) = 142;
 SMT_mE_aig(5).aig_asso(3).pos(1) = -1;
 SMT_mE_aig(5).aig_asso(3).no(2) = 157;
 SMT_mE_aig(5).aig_asso(3).pos(2) = 1;
 SMT_mE_aig(5).aig_asso(3).no(3) = 141;
 SMT_mE_aig(5).aig_asso(3).pos(3) = -1;
+
+%fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne("SMT2_Data_mE_fill_aig.txt")
+%fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne("SMT2_Data_mE_fill_feu_bal.txt")
