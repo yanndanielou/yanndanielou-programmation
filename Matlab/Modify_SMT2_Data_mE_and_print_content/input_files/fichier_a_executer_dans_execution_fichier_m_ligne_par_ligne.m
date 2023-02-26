1af378b4ae185f529a2e98a9cdef66975810377d
yanndanielou <yanndanielou@users.noreply.github.com>
2023-02-26 18:14:43 +0100

    Update fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne.m

--- Matlab/Modify_SMT2_Data_mE_and_print_content/input_files/fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne.m
+++ Matlab/Modify_SMT2_Data_mE_and_print_content/input_files/fichier_a_executer_dans_execution_fichier_m_ligne_par_ligne.m
@@ -1,2 +1,6 @@
-execution_fichier_m_ligne_par_ligne("D:\GitHub\yanndanielou-programmation\Matlab\Test_load_SMT2_Data_mE_and_dump_to_test_SMT2_Data_mE_refactoring_non_reg\input_files\SMT2_Data_mE_fill_aig.txt");
-execution_fichier_m_ligne_par_ligne("D:\GitHub\yanndanielou-programmation\Matlab\Test_load_SMT2_Data_mE_and_dump_to_test_SMT2_Data_mE_refactoring_non_reg\input_files\SMT2_Data_mE_fill_feu_bal.txt");
+SMT_mE_aig(5).aig_asso(3).no(1) = 142;
+SMT_mE_aig(5).aig_asso(3).pos(1) = -1;
+SMT_mE_aig(5).aig_asso(3).no(2) = 157;
+SMT_mE_aig(5).aig_asso(3).pos(2) = 1;
+SMT_mE_aig(5).aig_asso(3).no(3) = 141;
+SMT_mE_aig(5).aig_asso(3).pos(3) = -1;
