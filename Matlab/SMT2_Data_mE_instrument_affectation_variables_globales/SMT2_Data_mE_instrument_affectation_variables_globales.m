e5d6a1b735cd92b3732943f17db643cc520a0728
yanndanielou <yanndanielou@users.noreply.github.com>
2023-02-28 16:37:54 +0100

    Update SMT2_Data_mE_instrument_affectation_variables_globales.m

--- Matlab/SMT2_Data_mE_instrument_affectation_variables_globales/SMT2_Data_mE_instrument_affectation_variables_globales.m
+++ Matlab/SMT2_Data_mE_instrument_affectation_variables_globales/SMT2_Data_mE_instrument_affectation_variables_globales.m
@@ -2,7 +2,7 @@ function SMT2_Data_mE_instrument_affectation_variables_globales(varargin)
 
 disp(string(datetime) + " SMT2_Data_mE_instrument_affectation_variables_globales begin");
 
-affectation_variables_globales_to_test_load_data("D:\GitHub\yanndanielou-programmation\Matlab\SMT2_Data_mE_fichier_texte\Sources\fichiers_m_generes\SMT2_Data_mE_part_001_initialisation_structures.m");
+affectation_variables_globales("Input\\SMT2_Data_mE.m");
 
 disp(string(datetime) + " SMT2_Data_mE_instrument_affectation_variables_globales end");
 
