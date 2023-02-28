a6c0f068665fe90721e3b0ba5a6075d9db4da7d4
yanndanielou <yanndanielou@users.noreply.github.com>
2023-02-28 22:29:27 +0100

    Update SMT2_Data_mE_instrument_affectation_variables_globales.m

--- Matlab/SMT2_Data_mE_instrument_affectation_variables_globales/SMT2_Data_mE_instrument_affectation_variables_globales.m
+++ Matlab/SMT2_Data_mE_instrument_affectation_variables_globales/SMT2_Data_mE_instrument_affectation_variables_globales.m
@@ -1,10 +1,18 @@
 function SMT2_Data_mE_instrument_affectation_variables_globales(varargin)
 
+if ~isdeployed
+    diary SMT2_Data_mE_instrument_affectation_variables_globales.log 
+else
+    diary off    
+end
+
 disp(string(datetime) + " SMT2_Data_mE_instrument_affectation_variables_globales begin");
 
-affectation_variables_globales("Input\\SMT2_Data_mE.m");
+affectation_variables_globales("Input\\SMT2_Data_mE_2022_10_light.m");
 
 disp(string(datetime) + " SMT2_Data_mE_instrument_affectation_variables_globales end");
+
+diary off
 
 return
 end
