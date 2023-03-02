a774ea235e58c376c4bbced1b1c8d8a6c802e218
yanndanielou <yanndanielou@users.noreply.github.com>
2023-03-02 12:06:15 +0100

    Update Test_Fill_table_during_creation.m

--- Matlab/Test_Fill_table_during_creation/Test_Fill_table_during_creation.m
+++ Matlab/Test_Fill_table_during_creation/Test_Fill_table_during_creation.m
@@ -1,20 +1,23 @@
 function Test_Fill_table_during_creation(varargin)
 
+%clears all the text from the Command Window, resulting in a clear screen
+clc
+
 disp(string(datetime) + " Test_Fill_table_during_creation begin");
 
 
   SMT_mE_aig = struct( ...
 					  'no',{[10010],[10020],[10031,10032,10033]},...
 					  'first_table_created_empty',{[],[],[]},...
-					  'first_table_created_full',{[1001,10012],[],{[],[10021, 10022]}}...
+					  'first_table_created_full',{[[1001,10012];[0,0]],[[0,0];[0,0]],[[0,0];[30021, 30022]]}...
 					  );
 
 
-SMT_mE_aig(1).first_table_created_empty(1) = 1001;
+SMT_mE_aig(1).first_table_created_empty(1,1) = 1001;
 SMT_mE_aig(2).first_table_created_empty(1,2) = 10012;
 
-SMT_mE_aig(3).first_table_created_empty(2,1) = 10021;
-SMT_mE_aig(3).first_table_created_empty(2,2) = 10022;
+SMT_mE_aig(3).first_table_created_empty(2,1) = 30021;
+SMT_mE_aig(3).first_table_created_empty(2,2) = 30022;
 
 
 
@@ -45,22 +48,36 @@ end
 
 
 for structure_it = 1 : length(SMT_mE_aig)
-	%print_table_content(SMT_mE_aig(structure_it).first_table_created_empty, "SMT_mE_aig" + " at " +  structure_it + " field first_table_created_empty")
-	%disp("SMT_mE_aig" + " at " +  structure_it + " first_table_created_empty:");
-	%for field_it = 1 : length(SMT_mE_aig(structure_it).first_table_created_empty)
-	%	disp("SMT_mE_aig" + " at " +  structure_it + " field first_table_created_empty at " + field_it + " = "  + SMT_mE_aig(structure_it).first_table_created_empty(field_it));
-	%end
+	print_table_content(SMT_mE_aig(structure_it).first_table_created_empty, "SMT_mE_aig" + " at " +  structure_it + " field first_table_created_empty")
+    disp("SMT_mE_aig" + " at " +  structure_it + " first_table_created_empty:");
+
+    disp("SMT_mE_aig(" + structure_it + ").no");
+    disp(SMT_mE_aig(structure_it).no);   
+	for field_it = 1 : length(SMT_mE_aig(structure_it).no)
+		disp("SMT_mE_aig" + " at " +  structure_it + " field no at " + field_it + " = "  + SMT_mE_aig(structure_it).no(field_it));
+    end
+    
+    disp("SMT_mE_aig(" + structure_it + ").first_table_created_empty");
+    disp(SMT_mE_aig(structure_it).first_table_created_empty);   
+    for field_it = 1 : length(SMT_mE_aig(structure_it).first_table_created_empty)
+		disp("SMT_mE_aig" + " at " +  structure_it + " field first_table_created_empty at " + field_it + " = "  + SMT_mE_aig(structure_it).first_table_created_empty(field_it));
+    end
+    
+    first_table_created_full_length = length(SMT_mE_aig(structure_it).first_table_created_full);
+    first_table_created_empty_length = length(SMT_mE_aig(structure_it).first_table_created_empty);
+    
+    if first_table_created_full_length ~= first_table_created_empty_length
+        warning("SMT_mE_aig at " + structure_it + " table first_table_created_full has size" + first_table_created_full_length +  " and first_table_created_empty has size:" + first_table_created_empty_length)
+    end
+    
+    disp("SMT_mE_aig(" + structure_it + ").first_table_created_full");
+    disp(SMT_mE_aig(structure_it).first_table_created_full); 
+    for field_it = 1 : length(SMT_mE_aig(structure_it).first_table_created_full)
+		disp("SMT_mE_aig" + " at " +  structure_it + " field first_table_created_full at " + field_it + " = "  + SMT_mE_aig(structure_it).first_table_created_full(field_it));
+    end
 end
 
 
-for structure_it = 1 : length(SMT_mE_aig)
-	%print_table_content(SMT_mE_aig(structure_it).first_table_created_full, "SMT_mE_aig" + " at " +  structure_it + " field first_table_created_full")
-
-    %disp("SMT_mE_aig" + " at " +  structure_it + " first_table_created_empty:");
-	%for field_it = 1 : length(SMT_mE_aig(structure_it).first_table_created_full)
-	%	disp("SMT_mE_aig" + " at " +  structure_it + " field first_table_created_full at " + field_it + " = "  + SMT_mE_aig(structure_it).first_table_created_full(field_it));
-	%end
-end
 
 %print_structure_content(SMT_mE_aig, "SMT_mE_aig");
 
