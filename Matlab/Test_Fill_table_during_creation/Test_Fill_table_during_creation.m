ce91154930d6cc2398472af3009b5a987ce07254
yanndanielou <yanndanielou@users.noreply.github.com>
2023-03-02 16:51:44 +0100

    Update Test_Fill_table_during_creation.m

--- Matlab/Test_Fill_table_during_creation/Test_Fill_table_during_creation.m
+++ Matlab/Test_Fill_table_during_creation/Test_Fill_table_during_creation.m
@@ -9,33 +9,63 @@ disp(string(datetime) + " Test_Fill_table_during_creation begin");
   SMT_mE_aig = struct( ...
 					  'no',{[10010],[10020],[10031,10032,10033]},...
 					  'first_table_created_empty',{[],[],[]},...
-					  'first_table_created_full',{[[1001,10012];[0,0]],[[0,0];[0,0]],[[0,0];[30021, 30022]]}...
+					  'first_table_created_full',{[[1001]],[[0,0];[0,0]],[[0,0];[30021, 30022]]}...
 					  );
 
 
 SMT_mE_aig(1).first_table_created_empty(1,1) = 1001;
+
 SMT_mE_aig(2).first_table_created_empty(1,2) = 10012;
 
 SMT_mE_aig(3).first_table_created_empty(2,1) = 30021;
 SMT_mE_aig(3).first_table_created_empty(2,2) = 30022;
 
+disp("length(SMT_mE_aig(1).first_table_created_empty)" + length(SMT_mE_aig(1).first_table_created_empty));
+disp("length(SMT_mE_aig(1).first_table_created_full)" + length(SMT_mE_aig(1).first_table_created_full));
+
+
+disp("SMT_mE_aig(1).first_table_created_empty(1,1):" + SMT_mE_aig(1).first_table_created_empty(1,1));
+disp("SMT_mE_aig(1).first_table_created_full(1,1):" + SMT_mE_aig(1).first_table_created_full(1,1));
+
+
+
+disp("length(SMT_mE_aig(2).first_table_created_empty)" + length(SMT_mE_aig(2).first_table_created_empty));
+disp("length(SMT_mE_aig(2).first_table_created_full)" + length(SMT_mE_aig(2).first_table_created_full));
+
+disp("SMT_mE_aig(2).first_table_created_empty:" + SMT_mE_aig(1).first_table_created_empty);
+disp("SMT_mE_aig(2).first_table_created_full:" + SMT_mE_aig(1).first_table_created_full);
+
+
+
+disp("length(SMT_mE_aig(3).first_table_created_empty)" + length(SMT_mE_aig(3).first_table_created_empty));
+disp("length(SMT_mE_aig(3).first_table_created_full)" + length(SMT_mE_aig(3).first_table_created_full));
+
+
+disp("SMT_mE_aig(3).first_table_created_empty(2,1):" + SMT_mE_aig(3).first_table_created_empty(2,1));
+disp("SMT_mE_aig(3).first_table_created_full(2,1):" + SMT_mE_aig(3).first_table_created_full(2,1));
+
+disp("SMT_mE_aig(3).first_table_created_empty(2,2):" + SMT_mE_aig(3).first_table_created_empty(2,2));
+disp("SMT_mE_aig(3).first_table_created_full(2,2):" + SMT_mE_aig(3).first_table_created_full(2,2));
 
 
 
 
-outside_struct_table_created_empty = [];
-outside_struct_table_created_full = [[10011, 10012];[10021, 10022]];
 
 
-outside_struct_table_created_empty(1,1) = 10011;
-outside_struct_table_created_empty(1,2) = 10012;
+% 
+% outside_struct_table_created_empty = [];
+% outside_struct_table_created_full = [[10011, 10012];[10021, 10022]];
+% 
+% 
+% outside_struct_table_created_empty(1,1) = 10011;
+% outside_struct_table_created_empty(1,2) = 10012;
+% 
+% 
+% outside_struct_table_created_empty(2,1) = 10021;
+% outside_struct_table_created_empty(2,2) = 10022;
 
-
-outside_struct_table_created_empty(2,1) = 10021;
-outside_struct_table_created_empty(2,2) = 10022;
-
-print_table_content(outside_struct_table_created_empty, "outside_struct_table_created_empty")
-print_table_content(outside_struct_table_created_full, "outside_struct_table_created_full")
+%print_table_content(outside_struct_table_created_empty, "outside_struct_table_created_empty")
+%print_table_content(outside_struct_table_created_full, "outside_struct_table_created_full")
 
 for structure_it = 1 : length(SMT_mE_aig)
     %print_table_content(SMT_mE_aig(structure_it).no, "SMT_mE_aig" + " at " +  structure_it + " field no")
@@ -51,11 +81,11 @@ for structure_it = 1 : length(SMT_mE_aig)
 	print_table_content(SMT_mE_aig(structure_it).first_table_created_empty, "SMT_mE_aig" + " at " +  structure_it + " field first_table_created_empty")
     disp("SMT_mE_aig" + " at " +  structure_it + " first_table_created_empty:");
 
-    disp("SMT_mE_aig(" + structure_it + ").no");
-    disp(SMT_mE_aig(structure_it).no);   
-	for field_it = 1 : length(SMT_mE_aig(structure_it).no)
-		disp("SMT_mE_aig" + " at " +  structure_it + " field no at " + field_it + " = "  + SMT_mE_aig(structure_it).no(field_it));
-    end
+    %disp("SMT_mE_aig(" + structure_it + ").no");
+    %disp(SMT_mE_aig(structure_it).no);   
+	%for field_it = 1 : length(SMT_mE_aig(structure_it).no)
+	%	disp("SMT_mE_aig" + " at " +  structure_it + " field no at " + field_it + " = "  + SMT_mE_aig(structure_it).no(field_it));
+    %end
     
     disp("SMT_mE_aig(" + structure_it + ").first_table_created_empty");
     disp(SMT_mE_aig(structure_it).first_table_created_empty);   
@@ -67,7 +97,7 @@ for structure_it = 1 : length(SMT_mE_aig)
     first_table_created_empty_length = length(SMT_mE_aig(structure_it).first_table_created_empty);
     
     if first_table_created_full_length ~= first_table_created_empty_length
-        warning("SMT_mE_aig at " + structure_it + " table first_table_created_full has size" + first_table_created_full_length +  " and first_table_created_empty has size:" + first_table_created_empty_length)
+        warning("SMT_mE_aig at " + structure_it + " table first_table_created_full has size:" + first_table_created_full_length +  " and first_table_created_empty has size:" + first_table_created_empty_length)
     end
     
     disp("SMT_mE_aig(" + structure_it + ").first_table_created_full");
@@ -83,10 +113,6 @@ end
 
 
 disp(string(datetime) + " Test_Fill_table_during_creation end");
-disp(" ");
-disp(" ");
-disp(" ");
-disp(" ");
 
 return
 end
