8042fe68f915286ee50fcac60bbb695e6745b72e
yanndanielou <yanndanielou@users.noreply.github.com>
2023-03-03 21:28:54 +0100

    Update execution_fichier_m_ligne_par_ligne_avec_multilines.m

--- Matlab/SMT2_Data_high_performances/execution_fichier_m_ligne_par_ligne_avec_multilines.m
+++ Matlab/SMT2_Data_high_performances/execution_fichier_m_ligne_par_ligne_avec_multilines.m
@@ -9,7 +9,7 @@ list_var_global{:,1} = [];
 ligne_list_var_global = 1;
 tline = fgetl(fid);
 
-current_multiline_instruction = '';
+current_multiline_instruction_str = "";
 
 line_number=0;
 while ischar(tline)
@@ -17,7 +17,7 @@ while ischar(tline)
     
     %just to track progress
     if mod(line_number, 10000) == 0
-        disp("execution_fichier_m_ligne_par_ligne_avec_multilines, processing line:" + line_number);
+        disp(string(datetime) + " execution_fichier_m_ligne_par_ligne_avec_multilines, processing line:" + line_number);
     end
     
 	line_strimmed = strtrim(tline);
@@ -27,19 +27,27 @@ while ischar(tline)
 	if is_instruction_ending_in_next_line
         % remove the "..."
         line_strimmed_without_continuation_char_as_str = line_strimmed(1:end-3);
-        line_strimmed_without_continuation_char_as_table = convertStringsToChars(line_strimmed_without_continuation_char_as_str);
-		current_multiline_instruction = strcat(current_multiline_instruction, line_strimmed_without_continuation_char_as_table);		
+        current_multiline_instruction_str  = current_multiline_instruction_str + line_strimmed_without_continuation_char_as_str;
+        
+        %line_strimmed_without_continuation_char_as_table = convertStringsToChars(line_strimmed_without_continuation_char_as_str);
+        %current_multiline_instruction_str  = current_multiline_instruction_str + line_strimmed_without_continuation_char_as_str;
+		%current_multiline_instruction = strcat(current_multiline_instruction, line_strimmed_without_continuation_char_as_table);		
 	else
-		if length(current_multiline_instruction) > 0
+		if strlength(current_multiline_instruction_str) > 1
             
-            line_strimmed_without_continuation_char_as_table = convertStringsToChars(line_strimmed);
-            current_multiline_instruction = strcat(current_multiline_instruction, line_strimmed_without_continuation_char_as_table);	
+            current_multiline_instruction_str = current_multiline_instruction_str + line_strimmed;
+            
+            %line_strimmed_without_continuation_char_as_table = convertStringsToChars(line_strimmed);
+            current_multiline_instruction_table = convertStringsToChars(current_multiline_instruction_str);
+            
+            disp(string(datetime) + " execution_fichier_m_ligne_par_ligne_avec_multilines, executing multi line with size:" + strlength(current_multiline_instruction_table));
             
             %disp(current_multiline_instruction);
-			evalc(current_multiline_instruction);
-			current_multiline_instruction = '';
-		else
-			evalc(line_strimmed);
+			evalc(current_multiline_instruction_table);
+			current_multiline_instruction_str = "";
+        else
+            %disp(line_strimmed);
+			eval(tline);
 		end	
     end
     
