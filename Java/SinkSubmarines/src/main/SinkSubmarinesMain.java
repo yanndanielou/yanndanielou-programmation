e5f1459fe544a37c0428896cb652873ab0b9e98f
yanndanielou <yanndanielou@users.noreply.github.com>
2023-03-08 12:20:51 +0100

    SinkSubmarines

--- Java/SinkSubmarines/src/main/SinkSubmarinesMain.java
+++ Java/SinkSubmarines/src/main/SinkSubmarinesMain.java
@@ -1,3 +1,4 @@
+package main;
 
 import java.awt.*;
 import java.awt.event.*;
@@ -6,13 +7,14 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 
 import hmi.SinkSubmarinesMainView;
-
+import log.Log4JConfig;
 import time.TimeManager;
 
 public class SinkSubmarinesMain {
 
 	public static void main(String[] args) {
 
+		Log4JConfig config = new Log4JConfig();
 		TimeManager timeManager = new TimeManager();
 		SinkSubmarinesMainView sinkSubmarinesMainView = new SinkSubmarinesMainView("GridLayoutDemo");
 		// Schedule a job for the event dispatch thread:
