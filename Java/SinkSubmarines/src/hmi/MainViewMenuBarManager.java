ede260ffcf1521b3f0384865c545bd114bbf7d52
yanndanielou <yanndanielou@users.noreply.github.com>
2023-03-09 22:43:56 +0100

    sub

---
+++ Java/SinkSubmarines/src/hmi/MainViewMenuBarManager.java
@@ -0,0 +1,122 @@
+package hmi;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.KeyStroke;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MainViewMenuBarManager implements ActionListener {
+	private static final Logger LOGGER = LogManager.getLogger(SinkSubmarinesMainView.class);
+
+	SinkSubmarinesMainView parent_main_view = null;
+
+	public MainViewMenuBarManager(SinkSubmarinesMainView parent) {
+		parent_main_view = parent;
+	}
+
+	public void createMenu() {
+		// Where the GUI is created:
+		JMenuBar menuBar;
+		JMenu menu, submenu;
+		JMenuItem menuItem;
+		JRadioButtonMenuItem rbMenuItem;
+		JCheckBoxMenuItem cbMenuItem;
+
+		// Create the menu bar.
+		menuBar = new JMenuBar();
+
+		// Build the first menu.
+		menu = new JMenu("A Menu");
+		menu.setMnemonic(KeyEvent.VK_A);
+		menu.getAccessibleContext().setAccessibleDescription("The only menu in this program that has menu items");
+		menuBar.add(menu);
+		menu.addActionListener(this);
+
+		// a group of JMenuItems
+		menuItem = new JMenuItem("A text-only menu item", KeyEvent.VK_T);
+		menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1, ActionEvent.ALT_MASK));
+		menuItem.getAccessibleContext().setAccessibleDescription("This doesn't really do anything");
+		menuItem.addActionListener(this);
+		menu.add(menuItem);
+
+		menuItem = new JMenuItem("Both text and icon", new ImageIcon("images/middle.gif"));
+		menuItem.setMnemonic(KeyEvent.VK_B);
+		menuItem.addActionListener(this);
+
+		menu.add(menuItem);
+
+		menuItem = new JMenuItem(new ImageIcon("images/middle.gif"));
+		menuItem.setMnemonic(KeyEvent.VK_D);
+		menuItem.addActionListener(this);
+		menu.add(menuItem);
+
+		// a group of radio button menu items
+		menu.addSeparator();
+		ButtonGroup group = new ButtonGroup();
+		rbMenuItem = new JRadioButtonMenuItem("A radio button menu item");
+		rbMenuItem.setSelected(true);
+		rbMenuItem.setMnemonic(KeyEvent.VK_R);
+		rbMenuItem.addActionListener(this);
+		menuItem.addActionListener(this);
+
+		group.add(rbMenuItem);
+		menu.add(rbMenuItem);
+
+		rbMenuItem = new JRadioButtonMenuItem("Another one");
+		rbMenuItem.setMnemonic(KeyEvent.VK_O);
+		rbMenuItem.addActionListener(this);
+		group.add(rbMenuItem);
+		menu.add(rbMenuItem);
+
+		// a group of check box menu items
+		menu.addSeparator();
+		cbMenuItem = new JCheckBoxMenuItem("A check box menu item");
+		cbMenuItem.setMnemonic(KeyEvent.VK_C);
+		menu.add(cbMenuItem);
+
+		cbMenuItem = new JCheckBoxMenuItem("Another one");
+		cbMenuItem.setMnemonic(KeyEvent.VK_H);
+		menu.add(cbMenuItem);
+
+		// a submenu
+		menu.addSeparator();
+		submenu = new JMenu("A submenu");
+		submenu.setMnemonic(KeyEvent.VK_S);
+		submenu.addActionListener(this);
+
+		menuItem = new JMenuItem("An item in the submenu");
+		menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_2, ActionEvent.ALT_MASK));
+		menuItem.addActionListener(this);
+		submenu.add(menuItem);
+
+		menuItem = new JMenuItem("Another item");
+		menuItem.addActionListener(this);
+
+		submenu.add(menuItem);
+		menu.add(submenu);
+
+		// Build second menu in the menu bar.
+		menu = new JMenu("Another Menu");
+		menu.setMnemonic(KeyEvent.VK_N);
+		menu.getAccessibleContext().setAccessibleDescription("This menu does nothing");
+		menuBar.add(menu);
+
+		parent_main_view.setJMenuBar(menuBar);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		LOGGER.info("actionPerformed" + e);
+	}
+}
