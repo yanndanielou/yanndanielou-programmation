b4e725bd7eca0597cdf4e6bd64559cf1f57ff4e0
yanndanielou <yanndanielou@users.noreply.github.com>
2023-04-05 12:23:58 +0200

    Update Constants.java

--- Java/SinkSubmarines/src/constants/Constants.java
+++ Java/SinkSubmarines/src/constants/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 		// no way to instantiate this class
 	}
 	
-	public static final int MINIMUM_DELAY_BETWEEN_TWO_ALLY_BOMB_DROPPED_IN_MILLISECONDS = 100;
+	public static final int MINIMUM_DELAY_BETWEEN_TWO_ALLY_BOMB_DROPPED_IN_MILLISECONDS = 50;
 	public static final int SIMPLE_ALLY_BOMB_Y_SPEED = 1;
 
 }
