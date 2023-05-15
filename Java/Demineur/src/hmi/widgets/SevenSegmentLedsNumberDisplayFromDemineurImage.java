afb4f65cb7a0c04e118072a10c57430824897480
yanndanielou <yanndanielou@users.noreply.github.com>
2023-05-15 18:47:16 +0200

    Update SevenSegmentLedsNumberDisplayFromDemineurImage.java

--- Java/Demineur/src/hmi/widgets/SevenSegmentLedsNumberDisplayFromDemineurImage.java
+++ Java/Demineur/src/hmi/widgets/SevenSegmentLedsNumberDisplayFromDemineurImage.java
@@ -1,9 +1,67 @@
 package hmi.widgets;
 
-public class SevenSegmentLedsNumberDisplayFromDemineurImage {
+import java.awt.Dimension;
+import java.util.ArrayList;
 
-	public SevenSegmentLedsNumberDisplayFromDemineurImage() {
-		// TODO Auto-generated constructor stub
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class SevenSegmentLedsNumberDisplayFromDemineurImage extends JPanel {
+
+	private static final long serialVersionUID = -5796505066525142407L;
+
+	private ArrayList<ImageIcon> zero_to_nine_icons = new ArrayList<>();
+
+	private ImageIcon empty_digit_icon;
+	private ImageIcon horizontal_space_between_digits_icon;
+
+	private ArrayList<JLabel> digits_as_label = new ArrayList<>();
+
+	public SevenSegmentLedsNumberDisplayFromDemineurImage(int number_of_digits) {
+		setVisible(true);
+
+		for (int number = 0; number <= 9; number++) {
+			ImageIcon imageIcon = new ImageIcon("Images/all_digits_as_7_segments_leds_number_" + number + ".png");
+			zero_to_nine_icons.add(imageIcon);
+		}
+
+		empty_digit_icon = new ImageIcon("Images/all_digits_as_7_segments_leds_number_empty.png");
+		horizontal_space_between_digits_icon = new ImageIcon(
+				"Images/all_digits_as_7_segments_leds_horizontal_space_between_digits.png");
+
+		setLayout(null);
+
+		for (int digit = 1; digit <= number_of_digits; digit++) {
+			JLabel digit_as_label = new JLabel(empty_digit_icon);
+			digit_as_label.setSize(getDigitDimension());
+
+			// digit_as_label.setLocation((int) ((digit - 1) *
+			// (getDigitDimension().getWidth())), 0);
+
+			digit_as_label.setLocation(
+					(int) ((digit - 1)
+							* (getDigitDimension().getWidth() + horizontal_space_between_digits_icon.getIconWidth())),
+					0);
+			add(digit_as_label);
+			digits_as_label.add(digit_as_label);
+
+			if (digit < number_of_digits) {
+				JLabel horizontal_space_between_digits_as_label = new JLabel(horizontal_space_between_digits_icon);
+				horizontal_space_between_digits_as_label.setSize(horizontal_space_between_digits_icon.getIconHeight(),
+						horizontal_space_between_digits_icon.getIconHeight());
+				horizontal_space_between_digits_as_label.setLocation(digit_as_label.getX() + digit_as_label.getWidth(),
+						0);
+				add(horizontal_space_between_digits_as_label);
+			}
+
+		}
+	}
+
+	private Dimension getDigitDimension() {
+		ImageIcon imageIcon = zero_to_nine_icons.get(0);
+		Dimension dimension = new Dimension(imageIcon.getIconWidth(), imageIcon.getIconHeight());
+		return dimension;
 	}
 
 }
