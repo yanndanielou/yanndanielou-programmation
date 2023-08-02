e441624590ca3b62de74f9c6fe2abe857e363a46
yanndanielou <yanndanielou@users.noreply.github.com>
2023-08-02 13:21:52 +0200

    Create GameBoardDataModel.java

---
+++ Java/DesktopTowerDefense/src/builders/GameBoardDataModel.java
@@ -0,0 +1,24 @@
+package builders;
+
+import java.util.ArrayList;
+
+public class GameBoardDataModel {
+
+	private int game_board_attacker_entry_absolute_x;
+	private int game_board_attacker_entry_absolute_y;
+	private int game_board_total_width;
+	private int game_board_total_height;
+	public int getGame_board_attacker_entry_absolute_x() {
+		return game_board_attacker_entry_absolute_x;
+	}
+	public int getGame_board_attacker_entry_absolute_y() {
+		return game_board_attacker_entry_absolute_y;
+	}
+	public int getGame_board_total_width() {
+		return game_board_total_width;
+	}
+	public int getGame_board_total_height() {
+		return game_board_total_height;
+	}
+
+}
