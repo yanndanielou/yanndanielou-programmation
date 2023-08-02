b51949766a499001b2781c31eb265651b483a58d
yanndanielou <yanndanielou@users.noreply.github.com>
2023-08-02 13:20:25 +0200

    Create HMIConstants.java

---
+++ Java/DesktopTowerDefense/src/constants/HMIConstants.java
@@ -0,0 +1,19 @@
+package constants;
+
+public class HMIConstants {
+
+	private HMIConstants() {
+		// no way to instantiate this class
+	}
+
+	public static final int EXTERNAL_FRAME_WIDTH = 5;
+	public static final int TOP_PANEL_HEIGHT = 30;
+	public static final int TOP_PANEL_ELEMENTS_HEIGHT = (int) (0.9 * TOP_PANEL_HEIGHT);
+
+	public static final int MENU_BAR_HEIGHT = 10;
+
+	// FIXME
+	public static final int NOT_UNDERSTOOD_MISSING_FRAME_HEIGHT = 50;
+	public static final int NOT_UNDERSTOOD_MISSING_FRAME_WIDTH = 20;
+
+}
