5b7b2c8bc9da13e18df163d0bcf6c89ab15b33c1
yanndanielou <yanndanielou@users.noreply.github.com>
2023-08-05 19:23:37 +0200

    Update CheatCodeManager.java

--- Java/DesktopTowerDefense/src/cheat_codes/CheatCodeManager.java
+++ Java/DesktopTowerDefense/src/cheat_codes/CheatCodeManager.java
@@ -1,9 +1,14 @@
 package cheat_codes;
 
-
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import belligerents.Attacker;
+import core.GameManager;
+import game.Game;
 
 public class CheatCodeManager {
 
@@ -23,6 +28,14 @@ public class CheatCodeManager {
 	public boolean try_and_apply_text_cheat_code(String text_cheat_code) {
 		LOGGER.info("text_cheat_code_entered:" + text_cheat_code);
 		return false;
+	}
+
+	public void forbid_enemies_to_move() {
+		if (GameManager.hasGameInProgress()) {
+			Game game = GameManager.getInstance().getGame();
+			List<Attacker> all_attackers = game.getAttackers();
+			all_attackers.forEach((attacker) -> attacker.forbid_to_move());
+		}
 	}
 
 }
