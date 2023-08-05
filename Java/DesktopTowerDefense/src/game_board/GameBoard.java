19780df5e51d5cf2e2e770062cb251b85954d5fa
yanndanielou <yanndanielou@users.noreply.github.com>
2023-08-05 15:20:33 +0200

    DTD

--- Java/DesktopTowerDefense/src/game_board/GameBoard.java
+++ Java/DesktopTowerDefense/src/game_board/GameBoard.java
@@ -21,7 +21,7 @@ public class GameBoard {
 
 	private GameBoardDataModel gameBoardDataModel;
 
-	// private ArrayList<SquaresRow> squares_rows = new ArrayList<>();
+	// private ArrayList<GameBoardPointsRow> gameBoardPoints_rows = new ArrayList<>();
 
 	private ArrayList<GameBoardPoint> all_game_board_point_as_ordered_list = new ArrayList<GameBoardPoint>();
 
@@ -36,20 +36,20 @@ public class GameBoard {
 
 	@SuppressWarnings("unused")
 	@Deprecated
-	private void create_initial_squares() {
+	private void create_initial_gameBoardPoints() {
 
 		for (int column = 0; column < total_width; column++) {
-			GameBoardPointsSeries squaresColumn = new GameBoardPointsSeries(column);
-			game_board_points_columns.add(squaresColumn);
+			GameBoardPointsSeries gameBoardPointsColumn = new GameBoardPointsSeries(column);
+			game_board_points_columns.add(gameBoardPointsColumn);
 			for (int line = 0; line < total_height; line++) {
-				GameBoardPointsSeries squaresRow = new GameBoardPointsSeries(line);
+				GameBoardPointsSeries gameBoardPointsRow = new GameBoardPointsSeries(line);
 
-				// FIXME: squares_rows.add(squaresRow);
+				// FIXME: gameBoardPoints_rows.add(gameBoardPointsRow);
 
 				GameBoardPoint gameBoardPoint = new GameBoardPoint(game, line, column);
 
-				squaresColumn.addGameBoardPoint(gameBoardPoint);
-				squaresRow.addGameBoardPoint(gameBoardPoint);
+				gameBoardPointsColumn.addGameBoardPoint(gameBoardPoint);
+				gameBoardPointsRow.addGameBoardPoint(gameBoardPoint);
 
 				all_game_board_point_as_ordered_list.add(gameBoardPoint);
 			}
@@ -64,87 +64,87 @@ public class GameBoard {
 		return total_height;
 	}
 
-	public ArrayList<GameBoardPoint> getAll_squares_as_ordered_list() {
+	public ArrayList<GameBoardPoint> getAll_gameBoardPoints_as_ordered_list() {
 		return all_game_board_point_as_ordered_list;
 	}
 
-	public ArrayList<GameBoardPointsSeries> getSquaresColumns() {
+	public ArrayList<GameBoardPointsSeries> getGameBoardPointsColumns() {
 		return game_board_points_columns;
 	}
 
-	public GameBoardPoint getSquare(int row, int column) {
-		List<GameBoardPoint> square_matching_column_and_row = all_game_board_point_as_ordered_list.stream()
+	public GameBoardPoint getGameBoardPoint(int row, int column) {
+		List<GameBoardPoint> gameBoardPoint_matching_column_and_row = all_game_board_point_as_ordered_list.stream()
 				.filter(item -> item.getRow() == row && item.getColumn() == column).collect(Collectors.toList());
 
-		if (square_matching_column_and_row.size() > 1) {
-			LOGGER.fatal("Found more than one (" + square_matching_column_and_row.size() + " squares with row:" + row
-					+ " and column:" + column + ". FOund: " + square_matching_column_and_row);
+		if (gameBoardPoint_matching_column_and_row.size() > 1) {
+			LOGGER.fatal("Found more than one (" + gameBoardPoint_matching_column_and_row.size() + " gameBoardPoints with row:" + row
+					+ " and column:" + column + ". FOund: " + gameBoardPoint_matching_column_and_row);
 			return null;
 		}
-		if (square_matching_column_and_row.isEmpty()) {
-			LOGGER.error("Could not find square with row:" + row + " and column:" + column);
+		if (gameBoardPoint_matching_column_and_row.isEmpty()) {
+			LOGGER.error("Could not find gameBoardPoint with row:" + row + " and column:" + column);
 			return null;
 		}
 
-		return square_matching_column_and_row.get(0);
+		return gameBoardPoint_matching_column_and_row.get(0);
 	}
 
 	public void setGame(Game game) {
 		this.game = game;
 	}
 
-	public GameBoardPoint getNeighbourSquare(GameBoardPoint reference_square, NeighbourGameBoardPointDirection direction) {
+	public GameBoardPoint getNeighbourGameBoardPoint(GameBoardPoint reference_gameBoardPoint, NeighbourGameBoardPointDirection direction) {
 		GameBoardPoint neighbour = null;
 
-		int reference_square_column = reference_square.getColumn();
-		int reference_square_row = reference_square.getRow();
+		int reference_gameBoardPoint_column = reference_gameBoardPoint.getColumn();
+		int reference_gameBoardPoint_row = reference_gameBoardPoint.getRow();
 
-		boolean is_reference_square_first_of_column = reference_square_column == 0;
-		boolean is_reference_square_last_of_column = reference_square_column == total_height - 1;
+		boolean is_reference_gameBoardPoint_first_of_column = reference_gameBoardPoint_column == 0;
+		boolean is_reference_gameBoardPoint_last_of_column = reference_gameBoardPoint_column == total_height - 1;
 
-		boolean is_reference_square_first_of_row = reference_square_row == 0;
-		boolean is_reference_square_last_of_row = reference_square_row == total_width - 1;
+		boolean is_reference_gameBoardPoint_first_of_row = reference_gameBoardPoint_row == 0;
+		boolean is_reference_gameBoardPoint_last_of_row = reference_gameBoardPoint_row == total_width - 1;
 
-		// left square
+		// left gameBoardPoint
 		switch (direction) {
 		case NORTH:
-			if (!is_reference_square_first_of_column) {
-				neighbour = getSquare(reference_square_row, reference_square_column - 1);
+			if (!is_reference_gameBoardPoint_first_of_column) {
+				neighbour = getGameBoardPoint(reference_gameBoardPoint_row, reference_gameBoardPoint_column - 1);
 			}
 			break;
 		case NORTH_EAST:
-			if (!is_reference_square_last_of_row && !is_reference_square_first_of_column) {
-				neighbour = getSquare(reference_square_row + 1, reference_square_column - 1);
+			if (!is_reference_gameBoardPoint_last_of_row && !is_reference_gameBoardPoint_first_of_column) {
+				neighbour = getGameBoardPoint(reference_gameBoardPoint_row + 1, reference_gameBoardPoint_column - 1);
 			}
 			break;
 		case EAST:
-			if (!is_reference_square_last_of_row) {
-				neighbour = getSquare(reference_square_row + 1, reference_square_column);
+			if (!is_reference_gameBoardPoint_last_of_row) {
+				neighbour = getGameBoardPoint(reference_gameBoardPoint_row + 1, reference_gameBoardPoint_column);
 			}
 			break;
 		case SOUTH_EAST:
-			if (!is_reference_square_last_of_row && !is_reference_square_last_of_column) {
-				neighbour = getSquare(reference_square_row + 1, reference_square_column + 1);
+			if (!is_reference_gameBoardPoint_last_of_row && !is_reference_gameBoardPoint_last_of_column) {
+				neighbour = getGameBoardPoint(reference_gameBoardPoint_row + 1, reference_gameBoardPoint_column + 1);
 			}
 			break;
 		case SOUTH:
-			if (!is_reference_square_last_of_column) {
-				neighbour = getSquare(reference_square_row, reference_square_column + 1);
+			if (!is_reference_gameBoardPoint_last_of_column) {
+				neighbour = getGameBoardPoint(reference_gameBoardPoint_row, reference_gameBoardPoint_column + 1);
 			}
 			break;
 		case SOUTH_WEST:
-			if (!is_reference_square_first_of_row && !is_reference_square_last_of_column) {
-				neighbour = getSquare(reference_square_row - 1, reference_square_column + 1);
+			if (!is_reference_gameBoardPoint_first_of_row && !is_reference_gameBoardPoint_last_of_column) {
+				neighbour = getGameBoardPoint(reference_gameBoardPoint_row - 1, reference_gameBoardPoint_column + 1);
 			}
 			break;
 		case WEST:
-			if (!is_reference_square_first_of_row) {
-				neighbour = getSquare(reference_square_row - 1, reference_square_column);
+			if (!is_reference_gameBoardPoint_first_of_row) {
+				neighbour = getGameBoardPoint(reference_gameBoardPoint_row - 1, reference_gameBoardPoint_column);
 			}
 			break;
 		case NORTH_WEST:
-			if (!is_reference_square_first_of_row && !is_reference_square_first_of_column) {
-				neighbour = getSquare(reference_square_row - 1, reference_square_column - 1);
+			if (!is_reference_gameBoardPoint_first_of_row && !is_reference_gameBoardPoint_first_of_column) {
+				neighbour = getGameBoardPoint(reference_gameBoardPoint_row - 1, reference_gameBoardPoint_column - 1);
 			}
 			break;
 		}
