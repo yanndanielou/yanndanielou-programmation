7125dbbd0dced5426ebbf08d7cd9b9f652bfb6f8
yanndanielou <yanndanielou@users.noreply.github.com>
2023-08-11 16:08:39 +0200

    Update ConstructionLocationPanel.java

--- Java/DesktopTowerDefense/src/hmi/ConstructionLocationPanel.java
+++ Java/DesktopTowerDefense/src/hmi/ConstructionLocationPanel.java
@@ -3,7 +3,6 @@ package hmi;
 import java.awt.Color;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 import java.util.Random;
 
 import javax.swing.JLabel;
@@ -23,11 +22,12 @@ public class ConstructionLocationPanel extends JPanel {
 	private GameBoardPanel gameBoardPanel;
 
 	private JLabel representationAsLabel;
+	private JPanel mouseOverSelectionForConstructionWhenEmptyPanel;
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1504209832169214216L;
-
+	private Color backgroundColor;
 	Random random = new Random();
 
 	public ConstructionLocationPanel(GameBoardPanel gameBoardPanel,
@@ -35,39 +35,22 @@ public class ConstructionLocationPanel extends JPanel {
 		this.gameBoardPredefinedConstructionLocation = gameBoardPredefinedConstructionLocation;
 		this.gameBoardPanel = gameBoardPanel;
 		this.setLayout(null);
-		// this.setBackground(Color.BLACK);
-		this.setBackground(new Color(random.nextInt(0, 255), random.nextInt(0, 255), random.nextInt(0, 255)));
+		setOpaque(false);
+		backgroundColor = new Color(random.nextInt(0, 255), random.nextInt(0, 255), random.nextInt(0, 255));
 		this.setLocation(gameBoardPredefinedConstructionLocation.getRectangleDefinedArea().getX(),
 				gameBoardPredefinedConstructionLocation.getRectangleDefinedArea().getY());
 		this.setSize(gameBoardPredefinedConstructionLocation.getRectangleDefinedArea().getWidth(),
 				gameBoardPredefinedConstructionLocation.getRectangleDefinedArea().getHeight());
 
 		name = "" + random.nextInt();
-		/*
-		 * setVisible(true);
-		 * 
-		 * representationAsLabel = new JLabel("pl");
-		 * representationAsLabel.setLocation(getLocation());
-		 * representationAsLabel.setSize(getSize()); add(representationAsLabel);
-		 */ addMouseMotionListener(new MouseMotionListener() {
 
-			@Override
-			public void mouseMoved(MouseEvent mouseEvent) {
-				int mouseX = mouseEvent.getX();
-				int mouseY = mouseEvent.getY();
+		mouseOverSelectionForConstructionWhenEmptyPanel = new JPanel();
+		mouseOverSelectionForConstructionWhenEmptyPanel.setLayout(null);
+		mouseOverSelectionForConstructionWhenEmptyPanel.setSize(getWidth() / 2, getHeight() / 2);
+		mouseOverSelectionForConstructionWhenEmptyPanel.setOpaque(false);
 
-				// TODO Auto-generated method stub
-				LOGGER.info(name + " mouseMoved x:" + mouseX + ", y:" + mouseY + " : " + mouseEvent);
-			}
-
-			@Override
-			public void mouseDragged(MouseEvent mouseEvent) {
-				LOGGER.info("mouseDragged" + mouseEvent);
-
-			}
-		});
-
-		addMouseListener(new MouseListener() {
+		add(mouseOverSelectionForConstructionWhenEmptyPanel);
+		mouseOverSelectionForConstructionWhenEmptyPanel.addMouseListener(new MouseListener() {
 
 			@Override
 			public void mouseReleased(MouseEvent e) {
@@ -83,14 +66,18 @@ public class ConstructionLocationPanel extends JPanel {
 
 			@Override
 			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-
+				setBackground(null);
+				mouseOverSelectionForConstructionWhenEmptyPanel.setBackground(null);
+				setOpaque(false);
+				mouseOverSelectionForConstructionWhenEmptyPanel.setOpaque(false);
 			}
 
 			@Override
 			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-
+				setBackground(backgroundColor);
+				mouseOverSelectionForConstructionWhenEmptyPanel.setBackground(backgroundColor);
+				setOpaque(true);
+				mouseOverSelectionForConstructionWhenEmptyPanel.setOpaque(true);
 			}
 
 			@Override
@@ -99,7 +86,6 @@ public class ConstructionLocationPanel extends JPanel {
 
 			}
 		});
-
 	}
 
 }
