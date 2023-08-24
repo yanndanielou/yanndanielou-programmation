a18e1a7c03a69987790f22365efb03dd5e08bdaf
yanndanielou <yanndanielou@users.noreply.github.com>
2023-08-24 12:06:37 +0200

    Max reccursive

---
+++ Java/MaxReccursive/src/MaxReccursiveApplication.java
@@ -0,0 +1,19 @@
+
+public class MaxReccursiveApplication {
+
+	public static void main(String[] args) {
+		-Xss4m
+		reccursiveFunction(0);
+	}
+
+	private static void reccursiveFunction(int reccursiveNumber) {
+		System.out.println("reccursiveFunction:" + reccursiveNumber);
+		try {
+			reccursiveFunction(reccursiveNumber + 1);
+		} catch (StackOverflowError stackOverflowError) {
+			System.out.println(
+					"StackOverflowError:" + stackOverflowError + " raised at reccursive number:" + reccursiveNumber);
+		}
+	}
+
+}
