b03350ec3f5dbb70f0f295e93d82a89dd8e4331d
yanndanielou <yanndanielou@users.noreply.github.com>
2023-09-16 15:31:39 +0200

    Sandbox project

---
+++ Java/Sandbox/src/test/SandboxMainTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import main.SandboxMain;
+import static org.junit.Assert.assertEquals;
+
+public class SandboxMainTest {
+
+}
