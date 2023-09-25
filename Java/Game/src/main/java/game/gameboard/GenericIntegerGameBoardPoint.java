18e05e75b37b924a006fe6d5bf69ccc45d53bc81
yanndanielou <yanndanielou@users.noreply.github.com>
2023-09-25 22:34:03 +0200

    Create GenericIntegerGameBoardPoint.java

---
+++ Java/Game/src/main/java/game/gameboard/GenericIntegerGameBoardPoint.java
@@ -0,0 +1,40 @@
+package game.gameboard;
+
+import java.util.Collection;
+import java.util.EnumMap;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import geometry2d.integergeometry.IntegerPrecisionPoint;
+
+public class GenericIntegerGameBoardPoint extends IntegerPrecisionPoint {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7353270231660749618L;
+
+	@SuppressWarnings("unused")
+	private static final Logger LOGGER = LogManager.getLogger(GenericIntegerGameBoardPoint.class);
+
+	private EnumMap<NeighbourGameBoardPointDirection, GenericIntegerGameBoardPoint> neighbourPerDirection = new EnumMap<>(
+			NeighbourGameBoardPointDirection.class);
+
+	protected GenericIntegerGameBoardPoint(int line, int column) {
+		super(column, line);
+	}
+
+	public void setNeighbour(NeighbourGameBoardPointDirection direction, GenericIntegerGameBoardPoint neighbour) {
+		neighbourPerDirection.put(direction, neighbour);
+	}
+
+	public String getShortDescription() {
+		return "GameBoardPoint :" + "[" + getXAsInt() + "," + getYAsInt() + "]";
+	}
+
+	public Collection<GenericIntegerGameBoardPoint> getNeighbours() {
+		return neighbourPerDirection.values();
+	}
+
+}
