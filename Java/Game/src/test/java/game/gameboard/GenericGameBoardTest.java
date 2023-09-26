faefcf3f8c83343be9496946603a61540d25db12
yanndanielou <yanndanielou@users.noreply.github.com>
2023-09-26 10:07:40 +0200

    Update GenericGameBoardTest.java

--- Java/Game/src/test/java/game/gameboard/GenericGameBoardTest.java
+++ Java/Game/src/test/java/game/gameboard/GenericGameBoardTest.java
@@ -1,5 +1,6 @@
 package game.gameboard;
 
+import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.AfterAll;
@@ -36,35 +37,58 @@ class GenericGameBoardTest {
 		}
 
 		@Nested
-		public class MiddleCell {
+		public class Cell {
 			protected GenericIntegerGameBoardPoint gameBoardPoint;
 
-			@BeforeEach
-			void setUp() throws Exception {
-				gameBoardPoint = gameBoard.getGameBoardPointByXAndY(5, 5);
+			@Nested
+			public class TopLeftCornerCell {
+
+				@BeforeEach
+				void setUp() throws Exception {
+					gameBoardPoint = gameBoard.getGameBoardPointByXAndY(0, 0);
+				}
+
+				@ParameterizedTest
+				@CsvSource(value = { "NORTH,NIL,NIL", "NORTH_EAST,NIL,NIL", "EAST,1,0", "SOUTH_EAST,1,1", "SOUTH,0,1", "SOUTH_WEST,NIL,NIL",
+						"WEST,NIL,NIL", "NORTH_WEST,NIL,NIL" }, nullValues = "NIL")
+				void testNeighbour(NeighbourGameBoardPointDirection direction, Integer expectedX, Integer expectedY) {
+					GenericIntegerGameBoardPoint neighbour = gameBoard.getNeighbourGameBoardPoint(gameBoardPoint,
+							direction);
+					if (expectedX == null || expectedY == null) {
+						assertNull(neighbour);
+					} else {
+						assertEquals(expectedX.intValue(), neighbour.getX());
+						assertEquals(expectedY.intValue(), neighbour.getY());
+					}
+
+				}
+
 			}
 
-			@Test
-			public void testNorth() {
-				GenericIntegerGameBoardPoint neighbour = gameBoard.getNeighbourGameBoardPoint(gameBoardPoint, NeighbourGameBoardPointDirection.NORTH);
-				assertEquals(gameBoardPoint.getX(), neighbour.getX());
-			}
-			
-			@ParameterizedTest
-			@CsvSource({
-			    "NORTH,5,4",
-			    "NORTH_EAST,6,4",
-			    "EAST,6,5",
-			    "SOUTH_EAST,6,6",
-			    "SOUTH,5,6",
-			    "SOUTH_WEST,4,6",
-			    "WEST,4,5",
-			    "NORTH_WEST,4,4"
-			})
-			void testNeighbour(NeighbourGameBoardPointDirection direction, int expectedX, int expectedY) {
-				GenericIntegerGameBoardPoint neighbour = gameBoard.getNeighbourGameBoardPoint(gameBoardPoint, direction);
-				assertEquals(expectedX, neighbour.getX());
-				assertEquals(expectedY, neighbour.getY());
+			@Nested
+			public class MiddleCell {
+
+				@BeforeEach
+				void setUp() throws Exception {
+					gameBoardPoint = gameBoard.getGameBoardPointByXAndY(5, 5);
+				}
+
+				@Test
+				public void testNorth() {
+					GenericIntegerGameBoardPoint neighbour = gameBoard.getNeighbourGameBoardPoint(gameBoardPoint,
+							NeighbourGameBoardPointDirection.NORTH);
+					assertEquals(gameBoardPoint.getX(), neighbour.getX());
+				}
+
+				@ParameterizedTest
+				@CsvSource({ "NORTH,5,4", "NORTH_EAST,6,4", "EAST,6,5", "SOUTH_EAST,6,6", "SOUTH,5,6", "SOUTH_WEST,4,6",
+						"WEST,4,5", "NORTH_WEST,4,4" })
+				void testNeighbour(NeighbourGameBoardPointDirection direction, int expectedX, int expectedY) {
+					GenericIntegerGameBoardPoint neighbour = gameBoard.getNeighbourGameBoardPoint(gameBoardPoint,
+							direction);
+					assertEquals(expectedX, neighbour.getX());
+					assertEquals(expectedY, neighbour.getY());
+				}
 			}
 		}
 	}
