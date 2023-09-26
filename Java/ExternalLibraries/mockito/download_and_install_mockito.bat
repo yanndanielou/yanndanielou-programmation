a4ed6960d921132ee758bd62551f6c5c7b07fb0f
yanndanielou <yanndanielou@users.noreply.github.com>
2023-09-26 13:36:39 +0200

    Update download_and_install_mockito.bat

--- Java/ExternalLibraries/mockito/download_and_install_mockito.bat
+++ Java/ExternalLibraries/mockito/download_and_install_mockito.bat
@@ -2,8 +2,10 @@ MD mockito-5.5.0
 
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/mockito/mockito/archive/refs/tags/v5.5.0.zip', 'mockito-5.5.0/mockito-5.5.0.zip')
 
-powershell -command "Expand-Archive -Force 'mockito-5.5.0/mockito-5.5.0.zip' 'mockito-5.5.0'"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://repo1.maven.org/maven2/org/mockito/mockito-core/5.5.0/mockito-core-5.5.0.jar', 'mockito-5.5.0/mockito-core-5.5.0.jar')
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://repo1.maven.org/maven2/org/mockito/mockito-junit-jupiter/5.5.0/mockito-junit-jupiter-5.5.0.jar', 'mockito-5.5.0/mockito-junit-jupiter-5.5.0.jar')
 
+powershell -command "Expand-Archive -Force 'mockito-5.5.0/mockito-5.5.0.zip' 'mockito-5.5.0'"
 
 
 timeout /t 30
