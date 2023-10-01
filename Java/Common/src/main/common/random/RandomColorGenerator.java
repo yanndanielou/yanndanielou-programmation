f8f4bd5d01799782dedcb3af2d3344fae09a6633
yanndanielou <yanndanielou@users.noreply.github.com>
2023-10-01 15:07:01 +0200

    Create RandomColorGenerator.java

---
+++ Java/Common/src/main/common/random/RandomColorGenerator.java
@@ -0,0 +1,17 @@
+package main.common.random;
+
+import java.awt.Color;
+
+public class RandomColorGenerator {
+
+	private RandomColorGenerator() {
+	}
+
+	public static Color getRandomColor() {
+		int r = RandomIntegerGenerator.getRandomNumberUsingNextInt(0, 255);
+		int g = RandomIntegerGenerator.getRandomNumberUsingNextInt(0, 255);
+		int b = RandomIntegerGenerator.getRandomNumberUsingNextInt(0, 255);
+
+		return new Color(r, g, b);
+	}
+}
