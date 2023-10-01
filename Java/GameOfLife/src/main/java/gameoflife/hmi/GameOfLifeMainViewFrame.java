4cf5eac27df00222867e80e2af65f08c0ba3bd1f
yanndanielou <yanndanielou@users.noreply.github.com>
2023-10-01 22:12:47 +0200

    Update GameOfLifeMainViewFrame.java

--- Java/GameOfLife/src/main/java/gameoflife/hmi/GameOfLifeMainViewFrame.java
+++ Java/GameOfLife/src/main/java/gameoflife/hmi/GameOfLifeMainViewFrame.java
@@ -1,7 +1,10 @@
 package gameoflife.hmi;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -32,14 +35,16 @@ public class GameOfLifeMainViewFrame extends JFrame implements GameOfLifeMainVie
 	private GameBoardPanel gameFieldPanel;
 	private TopPanel topPanel;
 
-	//private MainViewPanel mainViewPanel;
+	// private MainViewPanel mainViewPanel;
 
 	private JScrollPane gameFieldScrollPane;
 
 	JPanel topLevelPanelForJFrame;
 	JLayeredPane level1LayeredPane;
-	
+
 	private JButton panButton;
+
+	protected Dimension frameExtraDimensionComparedToInputTopLevelPanel;
 
 	public GameOfLifeMainViewFrame() {
 		super("DesktopGameOfLife");
@@ -89,47 +94,68 @@ public class GameOfLifeMainViewFrame extends JFrame implements GameOfLifeMainVie
 		gameFieldPanel.initializeGamefield(game.getGameBoard());
 
 		gameFieldPanel.setPreferredSize(gameFieldPanel.getSize());
-		gameFieldPanel.setBounds(0, 0, (int) gameFieldPanel.getSize().getWidth(), (int) gameFieldPanel.getSize().getHeight());
+		gameFieldPanel.setBounds(0, 0, (int) gameFieldPanel.getSize().getWidth(),
+				(int) gameFieldPanel.getSize().getHeight());
 
-	//	mainViewPanel = new MainViewPanel(this, gameFieldPanel);
+		// mainViewPanel = new MainViewPanel(this, gameFieldPanel);
 
 		topLevelPanelForJFrame = new JPanel();
-		
 
 		gameFieldScrollPane = new JScrollPane(gameFieldPanel, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
 				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-		//gameFieldScrollPane.setPreferredSize(HMIConstants.MINIMUM_WINDOW_DIMENSION);
-		//gameFieldScrollPane.setLocation(0, 0);
-		gameFieldScrollPane.setBounds(0, 0, (int) HMIConstants.MINIMUM_WINDOW_DIMENSION.getWidth(), (int) HMIConstants.MINIMUM_WINDOW_DIMENSION.getHeight());
-
-
+		// gameFieldScrollPane.setPreferredSize(HMIConstants.MINIMUM_WINDOW_DIMENSION);
+		// gameFieldScrollPane.setLocation(0, 0);
+		gameFieldScrollPane.setBounds(0, 0, (int) HMIConstants.MINIMUM_WINDOW_DIMENSION.getWidth(),
+				(int) HMIConstants.MINIMUM_WINDOW_DIMENSION.getHeight());
 
 		panButton = HMIUtils.createJButtonFromImage("src/main/resources/images/PanButtonIcon.png");
 		panButton.setLocation((int) HMIConstants.SPACE_BETWEEN_COMMANDS_DIMENSION.getWidth(),
 				(int) HMIConstants.SPACE_BETWEEN_COMMANDS_DIMENSION.getHeight());
-		
-		
+
 		// mainViewContentPane.add(panButton, 0);
 
-
-
-		level1LayeredPane = new JLayeredPane(); 
+		level1LayeredPane = new JLayeredPane();
 		level1LayeredPane.setPreferredSize(HMIConstants.MINIMUM_WINDOW_DIMENSION);
 
 		level1LayeredPane.add(gameFieldScrollPane, 50, 0);
-		level1LayeredPane.add(panButton, 51, 0);	
+		level1LayeredPane.add(panButton, 51, 0);
 
-
-		/*topLevelPanelForJFrame.setLayout(null);
-		topLevelPanelForJFrame.setSize(getSize());
-			*/
+		/*
+		 * topLevelPanelForJFrame.setLayout(null);
+		 * topLevelPanelForJFrame.setSize(getSize());
+		 */
 		topLevelPanelForJFrame.add(level1LayeredPane, BorderLayout.CENTER);
-		topLevelPanelForJFrame.add(level1LayeredPane);
-		
+		// topLevelPanelForJFrame.add(level1LayeredPane);
+
 		setContentPane(topLevelPanelForJFrame);
 
 		setMinimumSize(HMIConstants.MINIMUM_WINDOW_DIMENSION);
 		pack();
+
+		Dimension initialFrameSize = this.getSize();
+		frameExtraDimensionComparedToInputTopLevelPanel = new Dimension(
+				(int) (initialFrameSize.getWidth() - HMIConstants.MINIMUM_WINDOW_DIMENSION.getWidth()),
+				(int) (initialFrameSize.getHeight() - HMIConstants.MINIMUM_WINDOW_DIMENSION.getHeight()));
+
+		this.addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentResized(ComponentEvent evt) {
+				Dimension newFrameSize = evt.getComponent().getSize();
+				
+				Dimension newFrameContentSize = new Dimension((int) (newFrameSize.getWidth() - frameExtraDimensionComparedToInputTopLevelPanel.getWidth()),
+						(int) (newFrameSize.getHeight()- frameExtraDimensionComparedToInputTopLevelPanel.getHeight()));
+
+				gameFieldScrollPane.setBounds(0, 0,
+						(int) newFrameContentSize.getWidth(),
+						(int) newFrameContentSize.getHeight());
+
+				level1LayeredPane.setPreferredSize(newFrameContentSize);
+
+				// level1LayeredPane.setBounds(0, 0, (int) size.getWidth(), (int)
+				// size.getHeight());
+				// repaint();
+			}
+		});
 
 	}
 
