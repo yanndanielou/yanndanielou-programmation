c35cdf98e001fa02058ab23ff76a03433611eb27
yanndanielou <yanndanielou@users.noreply.github.com>
2023-10-02 22:29:21 +0200

    Create SandboxMain.java

---
+++ Java/Sandbox/src/main/java/SandboxMain.java
@@ -0,0 +1,12 @@
+public class SandboxMain {
+
+	public SandboxMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
