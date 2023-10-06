f240092b15422570fb1e4f9f65798aae2764f16f
yanndanielou <yanndanielou@users.noreply.github.com>
2023-10-06 15:25:22 +0200

    Update GradleSandboxApplication.java

--- Java/GradleSandBox/src/main/java/gradlesandbox/application/GradleSandboxApplication.java
+++ Java/GradleSandBox/src/main/java/gradlesandbox/application/GradleSandboxApplication.java
@@ -4,15 +4,13 @@ import org.joda.time.LocalTime;
 
 import greeter.Greeter;
 
- 
-
 public class GradleSandboxApplication {
-	 public static void main(String[] args) {
-		    LocalTime currentTime = new LocalTime();
-		    System.out.println("The current local time is: " + currentTime);
+	public static void main(String[] args) {
+		LocalTime currentTime = new LocalTime();
+		System.out.println("The current local time is: " + currentTime);
 
-		    Greeter greeter = new Greeter();
-		    System.out.println(greeter.sayHello());
-		  }
+		Greeter greeter = new Greeter();
+		System.out.println(greeter.sayHello());
+	}
 
 }
