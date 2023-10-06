8438c600dcb53ba501f804d228d90859a359ead8
yanndanielou <yanndanielou@users.noreply.github.com>
2023-10-06 15:27:38 +0200

    Update build_with_gradle.bat

--- Java/GradleSandBox/build_with_gradle.bat
+++ Java/GradleSandBox/build_with_gradle.bat
@@ -1,8 +1,10 @@
 SET GRADLE_PATH="C:\Program Files\java\Gradle\gradle-8.4-all\gradle-8.4\bin"
 SET GRADLE_VERSION=8.4
+SET JDK_HOME="C:\Program Files\java\jdk-21"
 
-call %GRADLE_PATH%\gradle.bat" wrapper --gradle-version %GRADLE_VERSION%
+SET JAVA_HOME=%JDK_HOME%
+call %GRADLE_PATH%\gradle.bat wrapper --gradle-version %GRADLE_VERSION%
 call gradlew build
 
 
-
+timeout /t 15
