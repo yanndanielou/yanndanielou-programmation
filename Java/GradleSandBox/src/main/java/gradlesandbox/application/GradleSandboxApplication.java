0c00ef6b9287739103494f3089080459106e73a4
yanndanielou <yanndanielou@users.noreply.github.com>
2023-10-06 15:16:36 +0200

    Create GradleSandboxApplication.java

---
+++ Java/GradleSandBox/src/main/java/gradlesandbox/application/GradleSandboxApplication.java
@@ -0,0 +1,18 @@
+package gradlesandbox.application;
+
+import org.joda.time.LocalTime;
+
+import greeter.Greeter;
+
+ 
+
+public class GradleSandboxApplication {
+	 public static void main(String[] args) {
+		    LocalTime currentTime = new LocalTime();
+		    System.out.println("The current local time is: " + currentTime);
+
+		    Greeter greeter = new Greeter();
+		    System.out.println(greeter.sayHello());
+		  }
+
+}
