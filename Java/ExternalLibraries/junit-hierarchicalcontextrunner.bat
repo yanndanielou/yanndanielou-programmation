5b0c273e9a84334a214618ca3a90a8870f6894f7
yanndanielou <yanndanielou@users.noreply.github.com>
2023-10-08 21:44:36 +0200

    Create junit-hierarchicalcontextrunner.bat

---
+++ Java/ExternalLibraries/junit-hierarchicalcontextrunner.bat
@@ -0,0 +1,7 @@
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://repo1.maven.org/maven2/de/bechte/junit/junit-hierarchicalcontextrunner/4.12.1/junit-hierarchicalcontextrunner-4.12.1.jar', 'junit-hierarchicalcontextrunner-4.12.1.jar')
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://repo1.maven.org/maven2/de/bechte/junit/junit-hierarchicalcontextrunner/4.11.1/junit-hierarchicalcontextrunner-4.11.1.jar', 'junit-hierarchicalcontextrunner-4.11.1.jar')
+
+
+timeout /t 30
+
+
