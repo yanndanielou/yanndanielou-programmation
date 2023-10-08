38784f65610e6c05a80f44af989d90d0ab96cda1
yanndanielou <yanndanielou@users.noreply.github.com>
2023-10-08 22:48:15 +0200

    Create hamcrest.bat

---
+++ Java/ExternalLibraries/hamcrest.bat
@@ -0,0 +1,8 @@
+
+@call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/org/hamcrest/hamcrest-junit/2.0.0.0/hamcrest-junit-2.0.0.0.jar"
+@call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/com/btmatthews/hamcrest/hamcrest-matchers/1.0.1/hamcrest-matchers-1.0.1.jar"
+@call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/org/hamcrest/hamcrest-all/1.3/hamcrest-all-1.3.jar"
+@call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar"
+
+@timeout /t 10
+
