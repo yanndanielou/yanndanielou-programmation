da56a3460531bf20d6666466547c9d094723e713
yanndanielou <yanndanielou@users.noreply.github.com>
2023-10-08 22:32:21 +0200

    Update appacheCommons.bat

--- Java/ExternalLibraries/appacheCommons.bat
+++ Java/ExternalLibraries/appacheCommons.bat
@@ -1,20 +1,13 @@
-call DOWNLOAD_FILE_FROM_LINK https://repo1.maven.org/maven2/org/apache/commons/commons-collections4/4.4/commons-collections4-4.4-javadoc.jar
-Pause
 
-echo https://repo1.maven.org/maven2/org/apache/commons/commons-collections4/4.4/commons-collections4-4.4-javadoc.jar
-echo https://repo1.maven.org/maven2/org/apache/commons/commons-collections4/4.4/commons-collections4-4.4-sources.jar
-echo https://repo1.maven.org/maven2/org/apache/commons/commons-collections4/4.4/commons-collections4-4.4.jar
-echo https://repo1.maven.org/maven2/org/apache/commons/commons-lang3/3.13.0/commons-lang3-3.13.0-sources.jar
-echo https://repo1.maven.org/maven2/org/apache/commons/commons-lang3/3.13.0/commons-lang3-3.13.0.jarv
-echo https://repo1.maven.org/maven2/org/apache/commons/commons-lang3/3.13.0/commons-lang3-3.13.0-javadoc.jar
-echo https://repo1.maven.org/maven2/org/apache/commons/commons-lang3/3.13.0/commons-lang3-3.13.0-test-sources.jarv
-echo https://repo1.maven.org/maven2/org/apache/commons/commons-lang3/3.13.0/commons-lang3-3.13.0-tests.jar
-
-
-
-
-
-echo powershell -Command "(New-Object Net.WebClient).DownloadFile('https://repo1.maven.org/maven2/org/apache/commons/commons-collections4/4.4/commons-collections4-4.4-javadoc.jar', 'commons-collections4-4.4-javadoc.jar')
+call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/org/apache/commons/commons-collections4/4.4/commons-collections4-4.4-javadoc.jar"
+call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/org/apache/commons/commons-collections4/4.4/commons-collections4-4.4-javadoc.jar"
+call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/org/apache/commons/commons-collections4/4.4/commons-collections4-4.4-sources.jar"
+call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/org/apache/commons/commons-collections4/4.4/commons-collections4-4.4.jar"
+call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/org/apache/commons/commons-lang3/3.13.0/commons-lang3-3.13.0-sources.jar"
+call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/org/apache/commons/commons-lang3/3.13.0/commons-lang3-3.13.0.jar"
+call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/org/apache/commons/commons-lang3/3.13.0/commons-lang3-3.13.0-javadoc.jar"
+call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/org/apache/commons/commons-lang3/3.13.0/commons-lang3-3.13.0-test-sources.jar"
+call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/org/apache/commons/commons-lang3/3.13.0/commons-lang3-3.13.0-tests.jar"
 
 timeout /t 10
 
