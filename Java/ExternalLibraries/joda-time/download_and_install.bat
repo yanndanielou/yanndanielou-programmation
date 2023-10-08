67a662e01fe53331c8d78c3aff18f8661d116aa4
yanndanielou <yanndanielou@users.noreply.github.com>
2023-10-08 22:37:25 +0200

    Clean jars

--- Java/ExternalLibraries/joda-time/download_and_install.bat
+++
@@ -1,6 +0,0 @@
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://repo1.maven.org/maven2/joda-time/joda-time/2.12.5/joda-time-2.12.5.jar', 'joda-time-2.12.5.jar')
-
-
-timeout /t 30
-
-
--- Java/ExternalLibraries/objenesis/download_and_install.bat
+++
@@ -1,6 +0,0 @@
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://repo1.maven.org/maven2/org/objenesis/objenesis/3.3/objenesis-3.3.jar', 'objenesis-3.3.jar')
-
-
-timeout /t 30
-
-
