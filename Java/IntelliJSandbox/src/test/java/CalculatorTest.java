a31fa78746f49588278a53eb1ab1f09b5171d1d1
yanndanielou <yanndanielou@users.noreply.github.com>
2023-10-09 11:39:00 +0200

    IntelliJ

--- Java/IntelliJSandbox/src/test/java/CalculatorTest.java
+++ Java/IntelliJSandbox/src/test/java/CalculatorTest.java
@@ -1,9 +1,11 @@
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
 
     @org.junit.jupiter.api.Test
     void plus() {
+        assertEquals(5, Calculator.plus(3,2));
     }
 
     @org.junit.jupiter.api.Test
