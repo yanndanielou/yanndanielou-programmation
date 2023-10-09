66497e057b7daad81e5113f1b2a1b7eda7c2540c
yanndanielou <yanndanielou@users.noreply.github.com>
2023-10-09 11:27:23 +0200

    Create CalculatorTest.java

---
+++ Java/IntelliJSandbox/src/test/java/CalculatorTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void plus() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void minus() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiply() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void divide() {
+    }
+}
