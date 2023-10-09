3406574fb4b861022f46f055b2049caa7c2ca668
yanndanielou <yanndanielou@users.noreply.github.com>
2023-10-09 11:52:06 +0200

    IntelliJ

--- Java/IntelliJSandbox/src/test/java/CalculatorTest.java
+++ Java/IntelliJSandbox/src/test/java/CalculatorTest.java
@@ -1,22 +1,47 @@
+import org.junit.jupiter.api.Nested;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
 
-    @org.junit.jupiter.api.Test
-    void plus() {
-        assertEquals(5, Calculator.plus(3,2));
-    }
 
     @org.junit.jupiter.api.Test
     void minus() {
+        assertEquals(3, Calculator.minus(5, 2));
     }
 
     @org.junit.jupiter.api.Test
     void multiply() {
+        assertEquals(15, Calculator.multiply(5, 3));
+
     }
 
-    @org.junit.jupiter.api.Test
-    void divide() {
+
+    @Nested
+    class Divide {
+        @org.junit.jupiter.api.Test
+        void noRoundedNeeded() {
+            assertEquals(5, Calculator.divide(10, 2));
+        }
+
+        @org.junit.jupiter.api.Test
+        void rounded() {
+            assertEquals(5, Calculator.divide(11, 2));
+        }
+
+        @org.junit.jupiter.api.Test
+        void divideBy0() {
+            assertThrows(ArithmeticException.class,
+                    () -> Calculator.divide(10, 0));
+
+        }
+    }
+
+    @Nested
+    class Plus {
+        @org.junit.jupiter.api.Test
+        void plus() {
+            assertEquals(5, Calculator.plus(3, 2));
+        }
     }
 }
