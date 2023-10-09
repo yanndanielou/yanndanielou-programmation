c5f4ace1e3c4669ca919df45f86b4e4b552c75ee
yanndanielou <yanndanielou@users.noreply.github.com>
2023-10-09 11:27:44 +0200

    IntelliJ

---
+++ Java/IntelliJSandbox/src/main/java/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    public static int plus(int a, int b){
+        return a+b;
+    }
+    public static int minus(int a, int b){
+        return a-b;
+    }
+    public static int multiply(int a, int b){
+        return a*b;
+    }
+    public static int divide(int a, int b){
+        return a/b;
+    }
+}
