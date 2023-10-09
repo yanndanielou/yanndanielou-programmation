35d8416c60c498f2c7cd2d956a63a46ccebbb72d
yanndanielou <yanndanielou@users.noreply.github.com>
2023-10-09 12:32:57 +0200

    Update Calculator.java

--- Java/IntelliJSandbox/src/main/java/Calculator.java
+++ Java/IntelliJSandbox/src/main/java/Calculator.java
@@ -1,9 +1,19 @@
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Calculator {
+    private static final Logger LOGGER = LogManager.getLogger(Calculator.class);
+
     public static int plus(int a, int b){
-        return a+b;
+        int res = a + b;
+        LOGGER.info(()-> a + " plus " + b + " = " + res);
+        return res;
     }
     public static int minus(int a, int b){
-        return a-b;
+        int res = a - b;
+        LOGGER.info(()-> a + " minus " + b + " = " + res);
+        return res;
     }
     public static int multiply(int a, int b){
         return a*b;
