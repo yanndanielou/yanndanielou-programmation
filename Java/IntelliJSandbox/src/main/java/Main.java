7b0a12155e510bdd3217569a313a9b072c2ec48a
yanndanielou <yanndanielou@users.noreply.github.com>
2023-10-10 17:03:17 +0200

    Update Main.java

--- Java/IntelliJSandbox/src/main/java/Main.java
+++ Java/IntelliJSandbox/src/main/java/Main.java
@@ -1,10 +1,18 @@
+import java.util.List;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Entrée with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.print("Hello and welcome!");
+
+        for (int i = 10; i < Integer.MAX_VALUE; i *= 2) {
+
+            List<Integer> primeNumbers = PrimeNumbersCalculator.printAllPrimeNumberIntoTextFileUntil(i, "allPrimeNumbers_from_1_to_" + i + ".csv");
+
+        }
 
         // Press Maj+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
