bece35f867db4fee4200900250b6dd83d648727c
yanndanielou <yanndanielou@users.noreply.github.com>
2023-10-17 11:47:08 +0200

    Common: add gradle support

---
+++ Java/Common/build_with_gradle.bat
@@ -0,0 +1,14 @@
+SET GRADLE_PATH="C:\Program Files\java\Gradle\gradle-8.4-all\gradle-8.4\bin"
+SET GRADLE_VERSION=8.4
+SET JDK_HOME="C:\Program Files\java\jdk-21"
+
+CALL clean_with_gradle.bat
+
+ECHO %DATE% %TIME% >> gradle_logs.log
+
+SET JAVA_HOME=%JDK_HOME%
+call %GRADLE_PATH%\gradle.bat wrapper --gradle-version %GRADLE_VERSION%  >> gradle_logs.log  2>&1
+call gradlew build >> gradle_logs.log  2>&1
+
+
+timeout /t 15
