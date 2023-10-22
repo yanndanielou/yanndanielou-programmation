1be573153dc1757984a313618218564a51cceee0
yanndanielou <yanndanielou@users.noreply.github.com>
2023-10-22 14:56:58 +0200

    Handle Gradle

--- Java/Game/lib/src/test/java/game/gameboard/GenericGameBoardTest.java
+++ Java/Game/lib/src/test/java/game/gameboard/GenericGameBoardTest.java
@@ -1,7 +1,7 @@
 package game.gameboard;
 
-import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
