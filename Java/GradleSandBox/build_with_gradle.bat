80c8d7dba955b42be2f6cd23699290c659c88c0d
yanndanielou <yanndanielou@users.noreply.github.com>
2023-10-22 14:23:46 +0200

    Gradle Sandbox

--- Java/GradleSandBox/build_with_gradle.bat
+++ Java/GradleSandBox/build_with_gradle.bat
@@ -4,11 +4,19 @@ SET JDK_HOME="C:\Program Files\java\jdk-21"
 
 CALL clean_with_gradle.bat
 
-ECHO %DATE% %TIME% >> gradle_logs.log
+ECHO %DATE% %TIME% > gradle_logs.log
 
 SET JAVA_HOME=%JDK_HOME%
 call %GRADLE_PATH%\gradle.bat wrapper --gradle-version %GRADLE_VERSION%  >> gradle_logs.log  2>&1
 call gradlew build >> gradle_logs.log  2>&1
+rem call gradlew shadowJar >> gradle_logs.log  2>&1
+
+
+rem call gradlew jar >> gradle_logs.log  2>&1
+
+ECHO %DATE% %TIME% >> gradle_logs.log
+
+
 
 
 timeout /t 15
