941884f75775c1ae1872bf9c2854e07a9158fede
yanndanielou <yanndanielou@users.noreply.github.com>
2023-11-02 15:57:24 +0100

    Update Appache Jena.bat

--- Java/ExternalLibraries/Appache Jena.bat
+++ Java/ExternalLibraries/Appache Jena.bat
@@ -1,9 +1,28 @@
+call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/commons-io/commons-io/2.15.0/commons-io-2.15.0.jar"
+call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/commons-codec/commons-codec/1.16.0/commons-codec-1.16.0.jar"
 
-call DOWNLOAD_FILE_FROM_LINK.bat "https://dlcdn.apache.org/jena/binaries/apache-jena-fuseki-4.10.0.zip"
-call DOWNLOAD_FILE_FROM_LINK.bat "https://dlcdn.apache.org/jena/source/jena-4.10.0-source-release.zip"
-call DOWNLOAD_FILE_FROM_LINK.bat "https://dlcdn.apache.org/jena/binaries/apache-jena-4.10.0.zip
+call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/org/slf4j/slf4j-api/2.0.9/slf4j-api-2.0.9.jar"
+call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/org/slf4j/slf4j-simple/2.0.9/slf4j-simple-2.0.9.jar"
 
-powershell -command "Expand-Archive -Force 'vmockito-5.5.0.zip' 'mockito-5.5.0'"
+call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/org/apache/thrift/libthrift/0.19.0/libthrift-0.19.0.jar"
+
+call DOWNLOAD_FILE_FROM_LINK.bat "https://maven.clazzes.org/org/apache/xerces/org.apache.xerces/2.12.2/org.apache.xerces-2.12.2.jar"
+
+call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/commons-logging/commons-logging/1.2/commons-logging-1.2.jar"
+
+call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/com/github/ben-manes/caffeine/caffeine/3.1.8/caffeine-3.1.8.jar"
+
+rem com.hp.hpl.jena.rdf.model
+call DOWNLOAD_FILE_FROM_LINK.bat https://repo1.maven.org/maven2/com/hp/hpl/jena/jena/2.6.4/jena-2.6.4.jar
+
+call DOWNLOAD_FILE_FROM_LINK.bat https://repo1.maven.org/maven2/com/github/jsonld-java/jsonld-java/0.13.4/jsonld-java-0.13.4.jar
+
+call DOWNLOAD_FILE_FROM_LINK.bat https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-core/2.15.3/jackson-core-2.15.3.jar
+call DOWNLOAD_FILE_FROM_LINK.bat https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-annotations/2.15.3/jackson-annotations-2.15.3.jar
+
+rem start DOWNLOAD_AND_UNZIP_FILE_FROM_LINK.bat "https://dlcdn.apache.org/jena/binaries/apache-jena-fuseki-4.10.0.zip"
+rem start DOWNLOAD_AND_UNZIP_FILE_FROM_LINK.bat "https://dlcdn.apache.org/jena/source/jena-4.10.0-source-release.zip"
+rem call DOWNLOAD_AND_UNZIP_FILE_FROM_LINK.bat "https://dlcdn.apache.org/jena/binaries/apache-jena-4.10.0.zip"
 
 
 timeout /t 15
