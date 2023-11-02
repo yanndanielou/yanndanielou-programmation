c3c20a5016e3f0944d40db844a95151823011994
yanndanielou <yanndanielou@users.noreply.github.com>
2023-11-02 15:57:30 +0100

    Update DOWNLOAD_AND_UNZIP_FILE_FROM_LINK.bat

--- Java/ExternalLibraries/DOWNLOAD_AND_UNZIP_FILE_FROM_LINK.bat
+++ Java/ExternalLibraries/DOWNLOAD_AND_UNZIP_FILE_FROM_LINK.bat
@@ -1,14 +1,10 @@
 @SET FULL_LINK=%1
 @for /f "delims=" %%i in ("%FULL_LINK%") do @set "fileName=%%~nxi"
-set fileNameWithoutExtension = %fileName:~0,-4%
+@set fileNameWithoutExtension=%fileName:~0,-4%
 
-echo fileName %fileName% 
-echo FULL_LINK %FULL_LINK% 
-echo fileNameWithoutExtension %fileNameWithoutExtension% 
+@echo fileName %fileName% 
+@echo FULL_LINK %FULL_LINK% 
+@echo fileNameWithoutExtension %fileNameWithoutExtension% 
 
-pause
-
-CALL DOWNLOAD_FILE_FROM_LINK %1
+@CALL DOWNLOAD_FILE_FROM_LINK %1
 powershell -command "Expand-Archive -Force '%fileName%' '%fileNameWithoutExtension%'"
-
-pause
