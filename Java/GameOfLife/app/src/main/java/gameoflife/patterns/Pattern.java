dba45def390f81ba66c9ddb3264e98d23dcab3de
yanndanielou <yanndanielou@users.noreply.github.com>
2023-11-03 22:06:08 +0100

    Create Pattern.java

---
+++ Java/GameOfLife/app/src/main/java/gameoflife/patterns/Pattern.java
@@ -0,0 +1,9 @@
+package gameoflife.patterns;
+
+import geometry2d.integergeometry.IntegerPrecisionRectangle;
+
+public class Pattern {
+
+	private IntegerPrecisionRectangle rectangleBoundingBox;
+
+}
