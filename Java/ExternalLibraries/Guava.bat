84ff028e721a9e255fe96a4c55d071a9e9f3d0a1
yanndanielou <yanndanielou@users.noreply.github.com>
2023-11-14 16:52:44 +0100

    Create Guava.bat

---
+++ Java/ExternalLibraries/Guava.bat
@@ -0,0 +1,8 @@
+rem <!-- https://mvnrepository.com/artifact/com.google.guava/guava -->
+
+call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/com/google/guava/guava/32.1.3-jre/guava-32.1.3-jre.jar"
+call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/com/google/guava/guava/32.1.3-jre/guava-32.1.3-jre-javadoc.jar"
+call DOWNLOAD_FILE_FROM_LINK.bat "https://repo1.maven.org/maven2/com/google/guava/guava/32.1.3-jre/guava-32.1.3-jre-sources.jar"
+
+
+timeout /t 15
