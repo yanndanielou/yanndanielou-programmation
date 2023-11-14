2e8f2daa4e168384bb314d39b1b41c37983d3970
yanndanielou <yanndanielou@users.noreply.github.com>
2023-11-14 22:24:06 +0100

    Create JavaFX.bat

---
+++ Java/ExternalLibraries/JavaFX.bat
@@ -0,0 +1,7 @@
+
+start DOWNLOAD_AND_UNZIP_FILE_FROM_LINK.bat "https://download.java.net/java/GA/jdk21.0.1/415e3f918a1f4062a0074a2794853d0d/12/GPL/openjdk-21.0.1_windows-x64_bin.zip"
+call DOWNLOAD_AND_UNZIP_FILE_FROM_LINK.bat "https://download2.gluonhq.com/openjfx/21.0.1/openjfx-21.0.1_windows-x64_bin-sdk.zip"
+
+
+
+timeout /t 15
