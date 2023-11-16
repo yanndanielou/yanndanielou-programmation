1c65a63c8814cdba73af60e8c9b9acd7a97c68a0
yanndanielou <yanndanielou@users.noreply.github.com>
2023-11-16 09:16:06 +0100

    Update download.bat

--- Java/JavaFXSandbox/JavaFXSudoku/download.bat
+++ Java/JavaFXSandbox/JavaFXSudoku/download.bat
@@ -1,4 +1,13 @@
-..\..\ExternalLibraries\DOWNLOAD_FILE_FROM_LINK.bat https://github.com/BenJeau/JavaFX-Sudoku/archive/refs/heads/master.zip
+call ..\..\ExternalLibraries\DOWNLOAD_FILE_FROM_LINK.bat https://github.com/BenJeau/JavaFX-Sudoku/archive/refs/heads/master.zip
+
+@set fileNameWithoutExtension=%fileName:~0,-4%
+
+@echo fileName %fileName% 
+@echo FULL_LINK %FULL_LINK% 
+@echo fileNameWithoutExtension %fileNameWithoutExtension% 
+
+powershell -command "Expand-Archive -Force '%fileName%' '%fileNameWithoutExtension%'"
+
 
 timeout /t 30
 
