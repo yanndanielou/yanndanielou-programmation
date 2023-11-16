45c9a32ba3c61af5485ee6c4458b3af62cf36612
yanndanielou <yanndanielou@users.noreply.github.com>
2023-11-16 11:23:16 +0100

    Sudoku

---
+++ Java/JavaFXSandbox/JavaFXSudoku/master/JavaFX-Sudoku-master/Main.java
@@ -0,0 +1,443 @@
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class Main extends Application {
+
+	private int value = 0;
+	private long countUp = 0;
+
+	private BorderPane root;
+	private Scene scene;
+	private GridPane table;
+	private Sudoku sudoku;
+
+	private ArrayList<Integer> board, untouched;
+	private Map<Integer, Button> boardText, numButtons;
+	private Map<Integer, GridPane> grid;
+
+	private Image applicationIcon;
+	private HBox hbox;
+	private Button clear, newGame;
+	private GridPane num;
+
+	private Date start;
+	private Timeline timeline;
+
+	private Stage stage;
+
+	/**
+	 * Changes the CSS ids of the horizontal line
+	 * 
+	 * @param array an Array of CSS ids
+	 * @param start the horizontal line number
+	 */
+	private void changeHorizontalIds(String[] array, int start) {
+		for (int i = start * 9; i < start * 9 + 9; i++) {
+			changeIdsHelper(array, i);
+		}
+	}
+
+	/**
+	 * Changes the CSS ids of the vertical line
+	 * 
+	 * @param array an Array of CSS ids
+	 * @param start the vertical line number
+	 */
+	private void changeVerticalIds(String[] array, int start) {
+		for (int i = start; i < start + 9 * 9; i += 9) {
+			changeIdsHelper(array, i);
+		}
+	}
+
+	/**
+	 * Changes the CSS ids of a specific Sudoku board element according to its
+	 * original state
+	 * 
+	 * @param array an Array of CSS ids
+	 * @param i     the location of the button in the Sudoku board
+	 */
+	private void changeIdsHelper(String[] array, int i) {
+		if (!(boardText.get(i).getText()).equals(String.valueOf(value)) || value == 0) {
+			if (untouched.get(i) != 0) {
+				boardText.get(i).setId(array[0]);
+			} else if (board.get(i) != 0) {
+				boardText.get(i).setId(array[1]);
+			} else {
+				boardText.get(i).setId(array[2]);
+			}
+		} else {
+			boardText.get(i).setId(array[3]);
+		}
+	}
+
+	/**
+	 * Resets the game
+	 */
+	private void reset() {
+		// Removes every buttons (GridPane) inside the main GridPane
+		for (int i = 0; i < 9; i++) {
+			table.getChildren().remove(grid.get(i));
+		}
+
+		// Creates a new Sudoku board for the player
+		sudoku.clear();
+		sudoku.generateBoard();
+		sudoku.generatePlayer();
+
+		// Print out the solution
+		System.out.println(sudoku.toString());
+
+		// Get player's board
+		board = sudoku.getPlayer();
+
+		// List and maps of Buttons, GridPanes and value of the board
+		untouched = new ArrayList<Integer>(board);
+		boardText = new HashMap<Integer, Button>();
+		grid = new HashMap<Integer, GridPane>();
+	}
+
+	/**
+	 * Returns the number of elements of the specified number
+	 * 
+	 * @param num the number researched
+	 * @return a number of elements equal to the parameter
+	 */
+	private int getNum(int num) {
+		int count = 0;
+		for (int p = 0; p < 81; p++) {
+			if (Integer.valueOf(boardText.get(p).getText()) == num) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	/**
+	 * Generates the board, in terms of GUI
+	 */
+	private void generateBoard() {
+		// Each block
+		for (int i = 0; i < 9; i++) {
+
+			grid.put(i, new GridPane());
+
+			int t = i % 3 * 3 + (i / 3) * 27;
+			int temp = 0;
+
+			// Each element in that block
+			for (int j = t; j < t + 20; j += 9, temp++) {
+
+				// Each row of the block
+				for (int k = 0; k < 3; k++) {
+
+					// Index of current element
+					final int pos = j + k;
+
+					// New Button
+					boardText.put(pos, new Button());
+
+					if (board.get(pos) == 0) {
+						boardText.get(pos).setId("zero");
+
+						boardText.get(pos).setOnAction(e -> {
+							if (value != 0) {
+								if (boardText.get(pos).getText().equals(String.valueOf(value))) {
+									boardText.get(pos).setText("0");
+									board.set(pos, 0);
+									boardText.get(pos).setId("helperZero");
+								} else {
+									boardText.get(pos).setText(String.valueOf(value));
+									boardText.get(pos).setId("");
+									board.set(pos, value);
+								}
+
+								for (int l = 0; l < 81; l++) {
+									if (!boardText.get(l).getId().equals("number")
+											&& boardText.get(l).getText().equals(String.valueOf(value))) {
+										boardText.get(l).setId("number");
+									}
+								}
+
+								setLegend();
+							}
+
+							// Checks if the game is done
+							if (sudoku.checkBoard(board)) {
+								timeline.stop();
+
+								Alert alert = new Alert(AlertType.NONE,
+										"You just completed the sudoku board in " + countUp / 1000
+												+ " seconds. Do you want to play again?",
+										ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+								alert.showAndWait();
+
+								if (alert.getResult() == ButtonType.YES) {
+									newGame.fire();
+								} else if (alert.getResult() == ButtonType.NO) {
+									stage.close();
+								} else if (alert.getResult() == ButtonType.CANCEL) {
+									clear.fire();
+								}
+							}
+						});
+
+					} else {
+						boardText.get(pos).setId("preset");
+					}
+
+					boardText.get(pos).setOnMouseEntered(e -> {
+						if (!sudoku.checkBoard(board) && value != 0) {
+							changeHorizontalIds(new String[] { "helper", "helper", "helperZero", "numberHelper" },
+									pos / 9);
+							changeVerticalIds(new String[] { "helper", "helper", "helperZero", "numberHelper" },
+									pos % 9);
+							if (board.get(pos) == 0) {
+								scene.setCursor(Cursor.HAND);
+							}
+						}
+					});
+
+					boardText.get(pos).setOnMouseExited(e -> {
+						if (!sudoku.checkBoard(board) && value != 0) {
+							changeHorizontalIds(new String[] { "preset", "", "zero", "number" }, pos / 9);
+							changeVerticalIds(new String[] { "preset", "", "zero", "number" }, pos % 9);
+							scene.setCursor(Cursor.DEFAULT);
+						}
+					});
+
+					boardText.get(pos).setText(String.valueOf(board.get(pos)));
+					grid.get(i).add(boardText.get(pos), k, temp);
+				}
+			}
+			table.add(grid.get(i), i % 3, i / 3);
+		}
+	}
+
+	/**
+	 * Sets up the legend state by checking if any of the number has nine or more
+	 * appearance in the player's Sudoku board
+	 */
+	private void setLegend() {
+		for (int i = 1; i < 10; i++) {
+			if (getNum(i) >= 9) {
+				if (!numButtons.get(i - 1).getId().equals("legendFull")) {
+					numButtons.get(i - 1).setId("legendFull");
+				}
+			} else if (i != value) {
+				numButtons.get(i - 1).setId("");
+			} else {
+				numButtons.get(i - 1).setId("legend");
+			}
+		}
+	}
+
+	/**
+	 * Counts the time elapsed in seconds from the start of the game, from 0 to
+	 * infinite and display that number in the title bard
+	 */
+	private void startTimer() {
+		start = Calendar.getInstance().getTime();
+		timeline = new Timeline(new KeyFrame(Duration.seconds(1), e -> {
+			countUp = Calendar.getInstance().getTime().getTime() - start.getTime();
+			stage.setTitle(
+					"Sudoku - Time: " + String.valueOf(TimeUnit.SECONDS.convert(countUp, TimeUnit.MILLISECONDS)));
+		}));
+
+		timeline.setCycleCount(Animation.INDEFINITE);
+		timeline.play();
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		// Creates a reference to the primaryStage to be
+		// able to manipulate it in other methods
+		stage = primaryStage;
+
+		// Clear button
+		clear = new Button("Clear");
+		clear.setOnAction(e -> {
+			board = new ArrayList<Integer>(untouched);
+			for (int i = 0; i < 81; i++) {
+				if (board.get(i) != Integer.valueOf(boardText.get(i).getText())) {
+					boardText.get(i).setText(String.valueOf(board.get(i)));
+					boardText.get(i).setId("zero");
+				}
+			}
+
+			setLegend();
+		});
+
+		// New game button
+		newGame = new Button("New Game");
+		newGame.setOnAction(e -> {
+			if (value != 0) {
+				numButtons.get(value - 1).setId("");
+				value = 0;
+			}
+			timeline.stop();
+			stage.setTitle("Sudoku - Time: 0");
+			reset();
+			generateBoard();
+			startTimer();
+			setLegend();
+		});
+
+		// Starts the timer
+		startTimer();
+
+		// Layout of the board
+		table = new GridPane();
+		table.setVgap(8);
+		table.setHgap(8);
+		table.setAlignment(Pos.CENTER);
+
+		// Layout of the nine numbers at the bottom (legend)
+		num = new GridPane();
+		num.setHgap(2);
+		num.setPadding(new Insets(0, 0, 16, 0));
+		num.setAlignment(Pos.CENTER);
+
+		// Layout of the top two buttons
+		hbox = new HBox();
+		hbox.setSpacing(10);
+		hbox.setPadding(new Insets(16, 0, 0, 0));
+		hbox.setAlignment(Pos.CENTER);
+		hbox.getChildren().addAll(newGame, clear);
+
+		// Main layout of the Game
+		root = new BorderPane();
+		root.setTop(hbox);
+		root.setCenter(table);
+		root.setBottom(num);
+
+		// Generates the Sudoku board
+		sudoku = new Sudoku();
+		sudoku.generateBoard();
+		sudoku.generatePlayer();
+
+		// Prints out the solution
+		System.out.println(sudoku.toString());
+
+		// Application icon
+		applicationIcon = new Image("file:resources/applicationIcon.png");
+		primaryStage.getIcons().add(applicationIcon);
+
+		// Get player's board
+		board = sudoku.getPlayer();
+
+		// List and maps of buttons, GridPanes and value of the board
+		untouched = new ArrayList<Integer>(board);
+		boardText = new HashMap<Integer, Button>();
+		grid = new HashMap<Integer, GridPane>();
+		numButtons = new HashMap<Integer, Button>();
+
+		// Generates the GUI for the board
+		generateBoard();
+
+		// Sets up the legend (nine numbers at the bottom)
+		for (int i = 0; i < 9; i++) {
+			numButtons.put(i, new Button());
+			numButtons.get(i).setText(String.valueOf(i + 1));
+			num.add(numButtons.get(i), i, 0);
+
+			final int lo = i + 1;
+
+			numButtons.get(i).setOnAction(e -> {
+
+				if (value == Integer.valueOf(numButtons.get(lo - 1).getText())) {
+					if (getNum(value) < 9) {
+						numButtons.get(value - 1).setId("");
+					}
+
+					for (int k = 0; k < 81; k++) {
+						if ((boardText.get(k).getText()).equals(String.valueOf(value))) {
+							if (untouched.get(k) != 0) {
+								boardText.get(k).setId("preset");
+							} else if (board.get(k) != 0) {
+								boardText.get(k).setId("");
+							}
+						}
+					}
+
+					value = 0;
+				} else {
+					if (value != 0 && getNum(value) < 9) {
+						numButtons.get(value - 1).setId("");
+					}
+
+					value = lo;
+					numButtons.get(value - 1).setId("legend");
+
+					for (int k = 0; k < 81; k++) {
+						if ((boardText.get(k).getText()).equals(String.valueOf(value))) {
+							boardText.get(k).setId("number");
+						} else {
+							if (untouched.get(k) != 0) {
+								boardText.get(k).setId("preset");
+							} else if (board.get(k) != 0) {
+								boardText.get(k).setId("");
+							}
+						}
+					}
+				}
+
+				if (getNum(value) >= 9 && value != 0) {
+					numButtons.get(value - 1).setId("legendFull");
+				}
+			});
+
+			numButtons.get(i).setOnMouseEntered(e -> {
+				scene.setCursor(Cursor.HAND);
+			});
+
+			numButtons.get(i).setOnMouseExited(e -> {
+				scene.setCursor(Cursor.DEFAULT);
+			});
+		}
+
+		// Sets up the state of the legend (nine numbers at the bottom) according to the
+		// player's Sudoku board
+		setLegend();
+
+		// Sets the scene to the BorderPane layout and links the CSS file
+		scene = new Scene(root, 350, 450);
+		scene.getStylesheets().add("file:resources/application.css");
+
+		// Sets the stage, sets its title, displays it, and restricts its minimal size
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Sudoku - Time: 0");
+		primaryStage.show();
+		primaryStage.setMinHeight(primaryStage.getHeight());
+		primaryStage.setMinWidth(primaryStage.getWidth());
+	}
+
+	/**
+	 * Main method for the Sudoku game
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ Java/JavaFXSandbox/JavaFXSudoku/master/JavaFX-Sudoku-master/Sudoku.java
@@ -0,0 +1,470 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Sudoku {
+
+	/**
+	 * Keeps track of the values already tried at the specified location in the
+	 * board when creating the board, useful for the backtracking algorithm
+	 */
+	private ArrayList<ArrayList<Integer>> previousGenerate, previousVerify;
+
+	/**
+	 * Uncovered Sudoku board
+	 */
+	private ArrayList<Integer> board;
+
+	/**
+	 * Semi-uncovered Sudoku board
+	 */
+	private ArrayList<Integer> player;
+
+	/**
+	 * Constructor for Sudoku
+	 */
+	public Sudoku() {
+		clear();
+	}
+
+	/**
+	 * Creates a board with only 20 to 40 elements uncovered
+	 */
+	public void generatePlayer() {
+		generatePlayer(generateRandom(40, 20));
+	}
+
+	/**
+	 * Getter method for the player's Sudoku board
+	 * 
+	 * @return an ArrayList containing the player's Sudoku board
+	 */
+	public ArrayList<Integer> getPlayer() {
+		return player;
+	}
+
+	/**
+	 * Generates the player's board by putting the number of elements specified
+	 * 
+	 * @param num
+	 */
+	public void generatePlayer(int num) {
+		for (int i = 0; i < num; i++) {
+			ArrayList<Integer> zerosIndex = getIndexes(player, 0);
+			int rand = generateRandom(zerosIndex.size() - 1, 0);
+
+			player.set(zerosIndex.get(rand), board.get(zerosIndex.get(rand)));
+		}
+
+		/*
+		 * Test the solve method System.out.println(printBoard(board));
+		 * System.out.println(printBoard(player));
+		 * 
+		 * System.out.println(sudokuString(player));
+		 * 
+		 * try { System.out.println(solve(getIndexes(player, 0).get(0),
+		 * getIndexes(player, 0))); } catch (StackOverflowError e) {
+		 * System.out.println("------"); }
+		 * 
+		 * System.out.println(printBoard(player));
+		 */
+	}
+
+	/*
+	 * Test the solve method public static void main(String[] args) { Sudoku s = new
+	 * Sudoku(); s.generateBoard(); s.generatePlayer(); }
+	 */
+
+	/**
+	 * Used to verify with online sudoku solver
+	 */
+	public String sudokuString(ArrayList<Integer> l) {
+		String sudoku = "";
+		for (int i : l) {
+			if (i == 0) {
+				sudoku += ".";
+			} else {
+				sudoku += i;
+			}
+		}
+		return sudoku;
+	}
+
+	/**
+	 * Generates the board by calling the recursive method generateBoard(int num)
+	 */
+	public void generateBoard() {
+		generateBoard(0);
+	}
+
+	/**
+	 * Recursive method generating the Sudoku using a backtracking algorithm
+	 * 
+	 * @param num the index of the current element in board
+	 */
+	private void generateBoard(int num) {
+		if (!fullBoard() && !checkBoard(board)) {
+			ArrayList<Integer> available = complement(
+					combineArrayList(Arrays.asList(getNeighbours(num, board), previousGenerate.get(num))));
+
+			if (available.size() == 0) {
+				board.set(num, 0);
+				previousGenerate.get(num).clear();
+
+				generateBoard(num - 1);
+			} else {
+				board.set(num, available.get(generateRandom(available.size(), 0)));
+				previousGenerate.get(num).add(board.get(num));
+
+				generateBoard(num + 1);
+			}
+		}
+	}
+
+	/**
+	 * Checks if there are more than two possible solutions
+	 * 
+	 * TODO: Fix StackOverflow error
+	 */
+	private boolean solve(int num, ArrayList<Integer> zeros) {
+		ArrayList<Integer> available = complement(
+				combineArrayList(Arrays.asList(getNeighbours(num, player), previousVerify.get(num))));
+
+		if (available.size() == 0) {
+			if (num == zeros.get(0)) {
+				return true;
+			} else {
+				player.set(num, 0);
+				previousVerify.get(num).clear();
+
+				return solve(zeros.get(zeros.indexOf(num) - 1), zeros);
+			}
+		} else {
+			player.set(num, available.get(0));
+			previousVerify.get(num).add(player.get(num));
+
+			if (num == zeros.get(zeros.size() - 1)) {
+				if (available.size() == 1 && player.equals(board)) {
+					return solve(num, zeros);
+				} else {
+					return false;
+				}
+			} else {
+				return solve(zeros.get(zeros.indexOf(num) + 1), zeros);
+			}
+		}
+	}
+
+	/**
+	 * Returns an ArrayList containing the every number in the specified block in
+	 * the board
+	 * 
+	 * Visual representation of the Sudoku board (numbers represents the block
+	 * number)
+	 * 
+	 * <pre>
+	 * 0 | 1 | 2
+	 * ---------
+	 * 3 | 4 | 5
+	 * ---------
+	 * 6 | 7 | 8
+	 * </pre>
+	 * 
+	 * @param num  the block number
+	 * @param list an ArrayList containing the Sudoku board
+	 * @return an ArrayList with the elements of the block
+	 */
+	private ArrayList<Integer> getBlock(int num, ArrayList<Integer> list) {
+		ArrayList<Integer> temp = new ArrayList<Integer>();
+
+		int t = num % 3 * 3 + (num / 3) * 27;
+
+		for (int i = t; i < t + 20; i += 9) {
+			temp.addAll(list.subList(i, i + 3));
+		}
+
+		return removeZeros(temp);
+	}
+
+	/**
+	 * Returns an ArrayList containing the every number in the specified vertical
+	 * line in the board (the most left one is 0 and the most right one is 8)
+	 * 
+	 * @param num  the vertical line number
+	 * @param list an ArrayList containing the Sudoku board
+	 * @return an ArrayList with the elements of the vertical line
+	 */
+	private ArrayList<Integer> getVerticalLine(int num, ArrayList<Integer> list) {
+		ArrayList<Integer> temp = new ArrayList<Integer>();
+
+		for (int i = num; i < num + 81; i += 9) {
+			temp.add(list.get(i));
+		}
+
+		return removeZeros(temp);
+	}
+
+	/**
+	 * Returns an ArrayList containing the every number in the specified horizontal
+	 * line in the board (the top one is 0 and the bottom is 8)
+	 * 
+	 * @param num  the horizontal line number
+	 * @param list an ArrayList containing the Sudoku board
+	 * @return an ArrayList with the elements of the horizontal line
+	 */
+	private ArrayList<Integer> getHorizontalLine(int num, ArrayList<Integer> list) {
+		return removeZeros(new ArrayList<Integer>(list.subList(num * 9, num * 9 + 9)));
+	}
+
+	/**
+	 * Checks if the block if full and valid (no duplicate numbers)
+	 * 
+	 * Visual representation of the Sudoku board (numbers represents the block
+	 * number)
+	 * 
+	 * <pre>
+	 * 0 | 1 | 2
+	 * ---------
+	 * 3 | 4 | 5
+	 * ---------
+	 * 6 | 7 | 8
+	 * </pre>
+	 * 
+	 * @param num  the block number
+	 * @param list an ArrayList containing the Sudoku board
+	 * @return true if the block is valid, false otherwise
+	 */
+	private boolean checkBlock(int num, ArrayList<Integer> list) {
+		return 9 == removeDuplicates(getBlock(num, list)).size();
+	}
+
+	/**
+	 * Checks if the vertical line if full and valid (no duplicate numbers)
+	 * 
+	 * @param num  the vertical line number
+	 * @param list an ArrayList containing the Sudoku board
+	 * @return true if the vertical line is valid, false otherwise
+	 */
+	private boolean checkVerticalLine(int num, ArrayList<Integer> list) {
+		return 9 == removeDuplicates(getVerticalLine(num, list)).size();
+	}
+
+	/**
+	 * Checks if the horizontal line if full and valid (no duplicate numbers)
+	 * 
+	 * @param num  the horizontal line number
+	 * @param list an ArrayList containing the Sudoku board
+	 * @return true if the horizontal line is valid, false otherwise
+	 */
+	private boolean checkHorizontalLine(int num, ArrayList<Integer> list) {
+		return 9 == removeDuplicates(getHorizontalLine(num, list)).size();
+	}
+
+	/**
+	 * Checks if the board is full, does not have any zero
+	 * 
+	 * @return true if the board is full, false otherwise
+	 */
+	private boolean fullBoard() {
+		return !board.contains(0);
+	}
+
+	/**
+	 * Checks if the board conforms to the Sudoku rules
+	 * 
+	 * @param list an ArrayList representing the Sudoku board
+	 * @return true if the board is valid, false otherwise
+	 */
+	public boolean checkBoard(ArrayList<Integer> list) {
+		for (int row = 0; row < 9; row++) {
+			for (int col = 0; col < 9; col++) {
+				if (!(checkVerticalLine(col, list) || checkHorizontalLine(row, list)
+						|| checkBlock(row / 3 * 3 + col / 3, list))) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * Returns an ArrayList containing every location of the value inside the
+	 * ArrayList
+	 * 
+	 * @param num   an ArrayList representing the Sudoku board
+	 * @param value the searched number in the ArrayList
+	 * @return an ArrayList containing the index of the elements equal to value
+	 */
+	private ArrayList<Integer> getIndexes(ArrayList<Integer> num, Integer value) {
+		ArrayList<Integer> temp = new ArrayList<Integer>();
+
+		for (int i = 0; i < num.size(); i++) {
+			if (value.equals(num.get(i))) {
+				temp.add(i);
+			}
+		}
+
+		return temp;
+	}
+
+	/**
+	 * Returns an ArrayList with the elements (from 1 to 9) that are not part of
+	 * parameter ArrayList
+	 * 
+	 * @param num an ArrayList containing elements from 1 to 9 inclusively
+	 * @return an ArrayList with those numbers
+	 */
+	private ArrayList<Integer> complement(ArrayList<Integer> num) {
+		ArrayList<Integer> temp = new ArrayList<Integer>();
+
+		for (int i = 1; i < 10; i++) {
+			if (!num.contains(i)) {
+				temp.add(i);
+			}
+		}
+
+		return temp;
+	}
+
+	/**
+	 * Gets an ArrayList of the neighbours of the specified location (num) inside
+	 * the ArrayList
+	 * 
+	 * @param num  the location of the element
+	 * @param list an ArrayList of the Sudoku board
+	 * @return an ArrayList with the neighbours of the location (without any
+	 *         duplicates)
+	 */
+	private ArrayList<Integer> getNeighbours(int num, ArrayList<Integer> list) {
+		return removeZeros(removeDuplicates(combineArrayList(Arrays.asList(getVerticalLine(num % 9, list),
+				getHorizontalLine(num / 9, list), getBlock((num / 9) / 3 * 3 + (num % 9) / 3, list)))));
+	}
+
+	/**
+	 * Combines every ArrayList in the List to a single ArrayList
+	 * 
+	 * @param list a List of ArrayList
+	 * @return an ArrayList containing every elements of the ArrayList in the List
+	 */
+	private ArrayList<Integer> combineArrayList(List<ArrayList<Integer>> list) {
+		ArrayList<Integer> temp = new ArrayList<Integer>();
+
+		for (ArrayList<Integer> i : list) {
+			temp.addAll(i);
+		}
+
+		return temp;
+	}
+
+	/**
+	 * Removes every zeros in the ArrayList
+	 * 
+	 * @param num an ArrayList containing zeros
+	 * @return an ArrayList without any zeros
+	 */
+	private ArrayList<Integer> removeZeros(ArrayList<Integer> num) {
+		num.removeAll(Collections.singleton(0));
+		return num;
+	}
+
+	/**
+	 * Removes the duplicate elements from an ArrayList
+	 * 
+	 * @param num an ArrayList containing duplicates elements
+	 * @return an ArrayList without duplicates elements
+	 */
+	private ArrayList<Integer> removeDuplicates(ArrayList<Integer> num) {
+		return new ArrayList<Integer>(new HashSet<Integer>(num));
+	}
+
+	/**
+	 * Clears and reinitializes the ArrayLists
+	 */
+	public void clear() {
+		board = new ArrayList<Integer>(Collections.nCopies(81, 0));
+		player = new ArrayList<Integer>(Collections.nCopies(81, 0));
+		previousGenerate = new ArrayList<ArrayList<Integer>>();
+		previousVerify = new ArrayList<ArrayList<Integer>>();
+
+		for (int i = 0; i < 81; i++) {
+			previousGenerate.add(new ArrayList<Integer>());
+			previousVerify.add(new ArrayList<Integer>());
+		}
+	}
+
+	/**
+	 * Method used for initial testing purposes, it fill up the ArrayList with
+	 * random numbers from 9 to 1 inclusively
+	 */
+	@SuppressWarnings("unused")
+	private void generateRandomBoard() {
+		for (int i = 0; i < board.size(); i++) {
+			board.set(i, generateRandom(9, 1));
+		}
+	}
+
+	/**
+	 * Generates a random integer number between the max and min parameter
+	 * inclusively
+	 * 
+	 * @param max an integer representing the maximum number of the range
+	 * @param min and integer representing the minimum number of the range
+	 * @return a random integer number
+	 */
+	private int generateRandom(int max, int min) {
+		return new Random().nextInt(max) + min;
+	}
+
+	/**
+	 * Creates a Sudoku board with the provided ArrayList
+	 * 
+	 * @param num ArrayList representing a Sudoku board
+	 * @return String representation of the Sudoku board
+	 */
+	public String printBoard(ArrayList<Integer> num) {
+		StringBuffer sb = new StringBuffer();
+
+		for (int i = 0; i < 81; i++) {
+
+			if (i != 0 && i % 27 == 0) {
+				sb.append("\n  ");
+				for (int j = 0; j < 13; j++) {
+					sb.append("_ ");
+				}
+				sb.append("\n");
+			}
+
+			if (i % 9 == 0) {
+				sb.append("\n");
+				sb.append("  ");
+			}
+
+			if (i % 3 == 0 && i % 9 != 0) {
+				sb.append(" |  ");
+			}
+
+			if (num.get(i) == 0) {
+				sb.append(" ");
+			} else {
+				sb.append(num.get(i));
+			}
+			sb.append(" ");
+		}
+
+		return sb.toString();
+	}
+
+	/**
+	 * Builds a String representation of the Sudoku board
+	 *
+	 * @return String representation of the Sudoku board
+	 */
+	public String toString() {
+		return printBoard(board);
+	}
+}
