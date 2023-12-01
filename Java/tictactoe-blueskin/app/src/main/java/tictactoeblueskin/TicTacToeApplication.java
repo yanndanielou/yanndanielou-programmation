ab842b7b781673e52842fc802b88516ca554cc38
yanndanielou <yanndanielou@users.noreply.github.com>
2023-12-01 13:16:56 +0100

    Morpion

--- Java/tictactoe-blueskin/app/src/main/java/tictactoeblueskin/TicTacToeApplication.java
+++ Java/tictactoe-blueskin/app/src/main/java/tictactoeblueskin/TicTacToeApplication.java
@@ -19,7 +19,7 @@ public class TicTacToeApplication extends Application {
 		stage.setScene(scene);
 		stage.show();
 	}
-
+ 
 
 	public static void main(String[] args) {
 		Application.launch(TicTacToeApplication.class);
