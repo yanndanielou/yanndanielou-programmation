24df4f755214feb422a21ccb7f8e36356017f17c
yanndanielou <yanndanielou@users.noreply.github.com>
2023-12-01 21:40:13 +0100

    Update SMT3Server.py

--- Python/Lancer_simulations_sur_smt3_ATSPlus/SMT3Server.py
+++ Python/Lancer_simulations_sur_smt3_ATSPlus/SMT3Server.py
@@ -1,8 +1,10 @@
 class SMT3Server:
-    def __init__(self, port, description, smt3Version):
+    def __init__(self, port, smt3Version):
         self.port = port
-        self.description = description
         self.smt3Version = smt3Version
         self.url = None
         self.full_url = None 
+
+    def description(self):
+        return self.smt3Version + "_" + str(self.port)
     
