9cda863a1638f96ce5a6fdfdbd8341bc0190a273
yanndanielou <yanndanielou@users.noreply.github.com>
2023-12-01 21:15:51 +0100

    Update param.py

--- Python/Lancer_simulations_sur_smt3_ATSPlus/param.py
+++ Python/Lancer_simulations_sur_smt3_ATSPlus/param.py
@@ -1,6 +1,12 @@
 # -*-coding:Utf-8 -*
 
+import SMT3Server
 
 import logging
 logger_level = logging.INFO
+
+sMT3Servers = list()
+sMT3Servers.append(SMT3Server.SMT3Server(8081, "", "D_5_3_0"))
+sMT3Servers.append(SMT3Server.SMT3Server(8082, "", "D_5_3_1"))
+sMT3Servers.append(SMT3Server.SMT3Server(8083, "", "D_5_3_2"))
 
