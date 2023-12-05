76e0f43793721b13c851e4769965dfc41f21201d
yanndanielou <yanndanielou@users.noreply.github.com>
2023-12-05 14:10:44 +0100

    Update Apache PDFBox.bat

--- Java/ExternalLibraries/Apache PDFBox.bat
+++ Java/ExternalLibraries/Apache PDFBox.bat
@@ -1,5 +1,5 @@
 
-CALL DOWNLOAD_FILE_FROM_LINK "https://www.apache.org/dyn/closer.lua/pdfbox/3.0.1/pdfbox-app-3.0.1.jar"
+CALL DOWNLOAD_FILE_FROM_LINK "https://dlcdn.apache.org/pdfbox/3.0.1/pdfbox-app-3.0.1.jar"
 CALL DOWNLOAD_FILE_FROM_LINK "https://www.apache.org/dyn/closer.lua/pdfbox/3.0.1/debugger-app-3.0.1.jar"
 CALL DOWNLOAD_FILE_FROM_LINK "https://www.apache.org/dyn/closer.lua/pdfbox/3.0.1/preflight-app-3.0.1.jar"
 
