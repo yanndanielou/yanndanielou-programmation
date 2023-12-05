9df3e5caabfa025f3c06522135068d64510a9c04
yanndanielou <yanndanielou@users.noreply.github.com>
2023-12-05 11:54:26 +0100

    Create Apache PDFBox.bat

---
+++ Java/ExternalLibraries/Apache PDFBox.bat
@@ -0,0 +1,7 @@
+
+CALL DOWNLOAD_FILE_FROM_LINK "https://www.apache.org/dyn/closer.lua/pdfbox/3.0.1/pdfbox-app-3.0.1.jar"
+CALL DOWNLOAD_FILE_FROM_LINK "https://www.apache.org/dyn/closer.lua/pdfbox/3.0.1/debugger-app-3.0.1.jar"
+CALL DOWNLOAD_FILE_FROM_LINK "https://www.apache.org/dyn/closer.lua/pdfbox/3.0.1/preflight-app-3.0.1.jar"
+
+
+timeout /t 15
