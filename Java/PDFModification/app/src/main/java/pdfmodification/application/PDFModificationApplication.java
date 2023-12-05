9a1a4afcf8db43424da23aa30ea3b7ab7959bf81
yanndanielou <yanndanielou@users.noreply.github.com>
2023-12-05 20:53:07 +0100

    PDF

--- Java/PDFModification/app/src/main/java/pdfmodification/application/PDFModificationApplication.java
+++ Java/PDFModification/app/src/main/java/pdfmodification/application/PDFModificationApplication.java
@@ -1,25 +1,51 @@
-import java.io.File;
-import java.util.HashMap;
-import org.apache.pdfbox.multipdf.Overlay;
+package pdfmodification.application;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.PDPageContentStream.AppendMode;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDSimpleFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
+import org.apache.pdfbox.util.Matrix;
 
 public class PDFModificationApplication {
+	protected static final Logger LOGGER = LogManager.getLogger(PDFModificationApplication.class);
 
-    public static void main(String[] args) throws Exception{  
-        PDDocument realDoc = PDDocument.load(new File("originaldocument.pdf"));
-        //the above is the document you want to watermark
-        //for all the pages, you can add overlay guide, indicating watermark the original pages with the watermark document.
+	public static void main(String[] args) throws Exception {
+		
+		LOGGER.info(() -> "Application started");
+		PDDocument document = new PDDocument();
 
-        HashMap<Integer, String> overlayGuide = new HashMap<Integer, String>();
-        for(int i=0; i<realDoc.getNumberOfPages(); i++){
-            overlayGuide.put(i+1, "watermark.pdf");
-            //watermark.pdf is the document which is a one page PDF with your watermark image in it. 
-            //Notice here, you can skip pages from being watermarked.
-        }
-        Overlay overlay = new Overlay();
-        overlay.setInputPDF(realDoc);
-        overlay.setOutputFile("final.pdf");
-        overlay.setOverlayPosition(Overlay.Position.BACKGROUND);
-        overlay.overlay(overlayGuide);      
-    }
+		PDRectangle rectangle = PDRectangle.A4;
+		PDPage page = new PDPage(rectangle);
+
+		document.addPage(page);
+
+		PDPageContentStream content = new PDPageContentStream(document, page, AppendMode.APPEND, true);
+
+		content.beginText();
+		content.newLineAtOffset(rectangle.getWidth()/3, rectangle.getHeight()/2);
+
+		content.setFont(new PDType1Font(Standard14Fonts.FontName.HELVETICA), 20);
+		content.showText("My watermark");
+		content.setTextMatrix(new Matrix(1, 0, 0, 1.5f, 7, 30));
+		content.showText("Stretched text (size 12, factor 1.5)");
+		content.setTextMatrix(new Matrix(1, 0, 0, 2f, 7, 5));
+		content.showText("Stretched text (size 12, factor 2)");
+		content.endText();
+
+		content.close();
+
+		LOGGER.info(() -> "Save output pdf");
+		document.save("SimplePdfStretchedText.pdf");
+
+		LOGGER.info(() -> "Application end");
+
+	}
 }
--- Windows/PDFBox/add_watermark_to_pdf.bat
+++ Windows/PDFBox/add_watermark_to_pdf.bat
@@ -1,6 +1,6 @@
 del output_pdf_with_watermark.pdf
 
-rem java -jar ..\..\java\ExternalLibraries\pdfbox-app-3.0.1.jar fromtext -pageSize=A4 -standardFont=Times-Roman --input=watermark_text_to_add.txt --output=watermark_only.pdf
+java -jar ..\..\java\ExternalLibraries\pdfbox-app-3.0.1.jar fromtext -pageSize=A4 -standardFont=Times-Roman --input=watermark_text_to_add.txt --output=watermark_only.pdf
 rem java -jar ..\..\java\ExternalLibraries\pdfbox-app-3.0.1.jar overlay --input=original_pdf.pdf -default=watermark_only.pdf --output=output_pdf_with_watermark.pdf
 
 rem java -jar ..\..\java\ExternalLibraries\pdfbox-app-2.0.30.jar overlay --input=original_pdf.pdf -default=watermark_only.pdf --output=output_pdf_with_watermark.pdf
