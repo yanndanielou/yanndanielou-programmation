ca5a624c843c4da8aa34f4c64636939c281fd53a
yanndanielou <yanndanielou@users.noreply.github.com>
2023-12-08 21:59:15 +0100

    Update param.py

--- Python/Lancer_simulations_sur_smt3_ATSPlus/param.py
+++ Python/Lancer_simulations_sur_smt3_ATSPlus/param.py
@@ -9,16 +9,16 @@ pas_sauvegarde = 1
 
 
 sMT3Servers = list()
-sMT3Servers.append(SMT3Server.SMT3Server(8081, "D_5_3_0", "A"))
-sMT3Servers.append(SMT3Server.SMT3Server(8082, "D5_3_2", ""))
-#sMT3Servers.append(SMT3Server.SMT3Server(8083, "D5_3_2_Beta1"))
-sMT3Servers.append(SMT3Server.SMT3Server(8084, "D5_3_2_Beta2", "B"))
+#sMT3Servers.append(SMT3Server.SMT3Server(8081, "D_5_3_0", "A"))
+#sMT3Servers.append(SMT3Server.SMT3Server(8082, "D5_3_2", ""))
+sMT3Servers.append(SMT3Server.SMT3Server(8083, "D5_3_2_1",""))
+#sMT3Servers.append(SMT3Server.SMT3Server(8084, "D5_3_2_Beta2", "B"))
 #sMT3Servers.append(SMT3Server.SMT3Server(8081, "D_5_3_0", "C"))
 #sMT3Servers.append(SMT3Server.SMT3Server(8084, "D5_3_2_Beta3", "D"))
 
 listNumerosSimulationsAEffectuer = None
-listNumerosSimulationsAEffectuer = list()
-listNumerosSimulationsAEffectuer.append(750)
-listNumerosSimulationsAEffectuer.append(751)
-listNumerosSimulationsAEffectuer.append(752)
+#listNumerosSimulationsAEffectuer = list()
+#listNumerosSimulationsAEffectuer.append(750)
+#listNumerosSimulationsAEffectuer.append(751)
+#listNumerosSimulationsAEffectuer.append(752)
                             
