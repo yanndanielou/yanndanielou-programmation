3646de511d1279a3656d5d641d9bff9a623e20e7
yanndanielou <yanndanielou@users.noreply.github.com>
2023-12-20 22:39:26 +0100

    Create OpenCSV.bat

---
+++ Java/ExternalLibraries/OpenCSV.bat
@@ -0,0 +1,9 @@
+
+CALL DOWNLOAD_FILE_FROM_LINK "https://repo1.maven.org/maven2/com/opencsv/opencsv/5.9/opencsv-5.9.jar"
+CALL DOWNLOAD_FILE_FROM_LINK "https://repo1.maven.org/maven2/com/opencsv/opencsv/5.9/opencsv-5.9-javadoc.jar"
+CALL DOWNLOAD_FILE_FROM_LINK "https://repo1.maven.org/maven2/com/opencsv/opencsv/5.9/opencsv-5.9-sources.jar"
+
+
+
+
+timeout /t 15
