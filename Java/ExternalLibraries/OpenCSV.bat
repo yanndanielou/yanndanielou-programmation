797fa2cf11f265e729591c66c5e5fc300a6f7213
yanndanielou <yanndanielou@users.noreply.github.com>
2023-12-22 19:10:53 +0100

    Update OpenCSV.bat

--- Java/ExternalLibraries/OpenCSV.bat
+++ Java/ExternalLibraries/OpenCSV.bat
@@ -2,7 +2,7 @@
 CALL DOWNLOAD_FILE_FROM_LINK "https://repo1.maven.org/maven2/com/opencsv/opencsv/5.9/opencsv-5.9.jar"
 CALL DOWNLOAD_FILE_FROM_LINK "https://repo1.maven.org/maven2/com/opencsv/opencsv/5.9/opencsv-5.9-javadoc.jar"
 CALL DOWNLOAD_FILE_FROM_LINK "https://repo1.maven.org/maven2/com/opencsv/opencsv/5.9/opencsv-5.9-sources.jar"
-
+CALL DOWNLOAD_FILE_FROM_LINK "https://repo1.maven.org/maven2/commons-beanutils/commons-beanutils/1.9.4/commons-beanutils-1.9.4.jar"
 
 
 
