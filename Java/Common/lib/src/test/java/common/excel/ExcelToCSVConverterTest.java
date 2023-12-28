8d0ad3f79a9b40fdc007bd02772bfd5b10cf2eaf
yanndanielou <yanndanielou@users.noreply.github.com>
2023-12-28 10:31:43 +0100

    Create ExcelToCSVConverterTest.java

---
+++ Java/Common/lib/src/test/java/common/excel/ExcelToCSVConverterTest.java
@@ -0,0 +1,36 @@
+package common.excel;
+
+import static matcher.BasicMatchers.empty;
+import static matcher.BasicMatchers.greaterThanOrEqualTo;
+import static matcher.BasicMatchers.is;
+import static matcher.BasicMatchers.lessThanOrEqualTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import common.random.RandomIntegerGenerator;
+
+public class ExcelToCSVConverterTest {
+
+	@Nested
+	public class ConvertAllWorksheetFromExcelFileToCSV {
+		@Nested
+		public class BadUsages {
+
+		}
+
+		@Nested
+		public class CorrectUsages {
+
+			@Test
+			void basicExampleExcelFile() {
+				ExcelToCSVConverter.convertAllWorksheetFromExcelFileToCSV("ExcelWorksheetToTestCsvConversion.xlsx");
+			}
+		}
+	}
+
+}
