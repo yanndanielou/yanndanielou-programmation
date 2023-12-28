5d23cb93e00500d44821812f71bca8cd2590ace2
yanndanielou <yanndanielou@users.noreply.github.com>
2023-12-28 10:31:55 +0100

    Create CaesarCipher.java

---
+++ Java/Common/lib/src/main/java/common/crypt/caesarcipher/CaesarCipher.java
@@ -0,0 +1,19 @@
+package common.crypt.caesarcipher;
+
+public class CaesarCipher {
+	/**
+	 * 
+	 * @param toEncode
+	 * @param rotation: if >0, right rotation. if <0, left rotation
+	 * @return
+	 */
+	public static String encodeAnyASCIIStringWithCaesarCipher(String toEncode, int rotation) {
+		String result = "";
+		for (byte initialChar : toEncode.getBytes()) {
+		}
+		String string = new String("");
+		return "";
+
+	}
+
+}
