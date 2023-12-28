dc2ff51dd0577c7e35606d74f589772c080e7e37
yanndanielou <yanndanielou@users.noreply.github.com>
2023-12-28 14:41:02 +0100

    Update PDFModificationApplication.java

--- Java/PDFModification/app/src/main/java/pdfmodification/application/PDFModificationApplication.java
+++ Java/PDFModification/app/src/main/java/pdfmodification/application/PDFModificationApplication.java
@@ -65,70 +65,73 @@ public class PDFModificationApplication {
 	}
 
 	/*
-	@Deprecated
-	public static void allStepsInOneMethodWithIntermediateFiles() throws IOException {
-
-		PDDocument newWatermarkOnlyDocument = new PDDocument();
-
-		PDRectangle rectangle = PDRectangle.A4;
-		PDPage watermarkOnlyPage = new PDPage(rectangle);
-
-		newWatermarkOnlyDocument.addPage(watermarkOnlyPage);
-
-		PDPageContentStream watermarkOnlyDocumentPageContentStream = new PDPageContentStream(newWatermarkOnlyDocument,
-				watermarkOnlyPage, AppendMode.APPEND, true);
-
-		watermarkOnlyDocumentPageContentStream.beginText();
-		watermarkOnlyDocumentPageContentStream.newLineAtOffset(rectangle.getWidth() / 3, rectangle.getHeight() / 2);
-
-		watermarkOnlyDocumentPageContentStream.setStrokingColor(Color.blue);
-		watermarkOnlyDocumentPageContentStream.setNonStrokingColor(Color.gray);
-		watermarkOnlyDocumentPageContentStream.setFont(new PDType1Font(Standard14Fonts.FontName.HELVETICA), 20);
-		watermarkOnlyDocumentPageContentStream.showText("My watermark");
-		watermarkOnlyDocumentPageContentStream.setTextMatrix(new Matrix(1, 0, 0, 1.5f, 7, 30));
-		watermarkOnlyDocumentPageContentStream.showText("Stretched text (size 12, factor 1.5)");
-		watermarkOnlyDocumentPageContentStream.setTextMatrix(new Matrix(1, 0, 0, 2f, 7, 5));
-		watermarkOnlyDocumentPageContentStream.showText("Stretched text (size 12, factor 2)");
-		watermarkOnlyDocumentPageContentStream.endText();
-
-		watermarkOnlyDocumentPageContentStream.close();
-
-		File myObj = new File(PDFModificationHelpers.watermarkOnlyPDFFileName);
-		if (myObj.delete()) {
-			System.out.println("Delete previous version of file file: " + myObj.getName());
-		} else {
-			System.out.println("Failed to delete the file.");
-		}
-
-		LOGGER.info(() -> "Save output pdf");
-		newWatermarkOnlyDocument.save(PDFModificationHelpers.watermarkOnlyPDFFileName);
-
-		// OverlayPDF
-		UUID randomUUID = java.util.UUID.randomUUID();
-		String fileName = PDFModificationHelpers.outputDirectoryName + randomUUID.toString() + ".pdf";
-		FileUtils.copyFile(new File(PDFModificationHelpers.originalPDFDocumentBeforeAnyModificationFullPath),
-				new File(fileName));
-
-		File file = new File(fileName);
-		PDDocument originalDoc = Loader.loadPDF(file);
-
-		Overlay overlayer = new Overlay();
-		overlayer.setInputPDF(originalDoc);
-		overlayer.setAllPagesOverlayFile(PDFModificationHelpers.watermarkOnlyPDFFileName);
-		overlayer.setOverlayPosition(Position.BACKGROUND);
-
-		overlayer.close();
-
-		PDDocument watermarkDocument = new PDDocument();
-		overlayer.setAllPagesOverlayPDF(watermarkDocument);
-
-		try (PDDocument result = overlayer.overlay(new HashMap<>())) {
-			result.save(PDFModificationHelpers.documentWithWatermark);
-		}
-		// watermarkDocument.save(documentWithWatermark);
-
-	}
-	*/
+	 * @Deprecated public static void allStepsInOneMethodWithIntermediateFiles()
+	 * throws IOException {
+	 * 
+	 * PDDocument newWatermarkOnlyDocument = new PDDocument();
+	 * 
+	 * PDRectangle rectangle = PDRectangle.A4; PDPage watermarkOnlyPage = new
+	 * PDPage(rectangle);
+	 * 
+	 * newWatermarkOnlyDocument.addPage(watermarkOnlyPage);
+	 * 
+	 * PDPageContentStream watermarkOnlyDocumentPageContentStream = new
+	 * PDPageContentStream(newWatermarkOnlyDocument, watermarkOnlyPage,
+	 * AppendMode.APPEND, true);
+	 * 
+	 * watermarkOnlyDocumentPageContentStream.beginText();
+	 * watermarkOnlyDocumentPageContentStream.newLineAtOffset(rectangle.getWidth() /
+	 * 3, rectangle.getHeight() / 2);
+	 * 
+	 * watermarkOnlyDocumentPageContentStream.setStrokingColor(Color.blue);
+	 * watermarkOnlyDocumentPageContentStream.setNonStrokingColor(Color.gray);
+	 * watermarkOnlyDocumentPageContentStream.setFont(new
+	 * PDType1Font(Standard14Fonts.FontName.HELVETICA), 20);
+	 * watermarkOnlyDocumentPageContentStream.showText("My watermark");
+	 * watermarkOnlyDocumentPageContentStream.setTextMatrix(new Matrix(1, 0, 0,
+	 * 1.5f, 7, 30)); watermarkOnlyDocumentPageContentStream.
+	 * showText("Stretched text (size 12, factor 1.5)");
+	 * watermarkOnlyDocumentPageContentStream.setTextMatrix(new Matrix(1, 0, 0, 2f,
+	 * 7, 5)); watermarkOnlyDocumentPageContentStream.
+	 * showText("Stretched text (size 12, factor 2)");
+	 * watermarkOnlyDocumentPageContentStream.endText();
+	 * 
+	 * watermarkOnlyDocumentPageContentStream.close();
+	 * 
+	 * File myObj = new File(PDFModificationHelpers.watermarkOnlyPDFFileName); if
+	 * (myObj.delete()) {
+	 * System.out.println("Delete previous version of file file: " +
+	 * myObj.getName()); } else { System.out.println("Failed to delete the file.");
+	 * }
+	 * 
+	 * LOGGER.info(() -> "Save output pdf");
+	 * newWatermarkOnlyDocument.save(PDFModificationHelpers.watermarkOnlyPDFFileName
+	 * );
+	 * 
+	 * // OverlayPDF UUID randomUUID = java.util.UUID.randomUUID(); String fileName
+	 * = PDFModificationHelpers.outputDirectoryName + randomUUID.toString() +
+	 * ".pdf"; FileUtils.copyFile(new
+	 * File(PDFModificationHelpers.originalPDFDocumentBeforeAnyModificationFullPath)
+	 * , new File(fileName));
+	 * 
+	 * File file = new File(fileName); PDDocument originalDoc =
+	 * Loader.loadPDF(file);
+	 * 
+	 * Overlay overlayer = new Overlay(); overlayer.setInputPDF(originalDoc);
+	 * overlayer.setAllPagesOverlayFile(PDFModificationHelpers.
+	 * watermarkOnlyPDFFileName); overlayer.setOverlayPosition(Position.BACKGROUND);
+	 * 
+	 * overlayer.close();
+	 * 
+	 * PDDocument watermarkDocument = new PDDocument();
+	 * overlayer.setAllPagesOverlayPDF(watermarkDocument);
+	 * 
+	 * try (PDDocument result = overlayer.overlay(new HashMap<>())) {
+	 * result.save(PDFModificationHelpers.documentWithWatermark); } //
+	 * watermarkDocument.save(documentWithWatermark);
+	 * 
+	 * }
+	 */
 
 	/**
 	 * https://stackoverflow.com/questions/32844926/using-overlay-in-pdfbox-2-0
@@ -164,25 +167,21 @@ public class PDFModificationApplication {
 			 */
 			pageOfOriginalDocumentWithMatermarkAsContentStream
 					.setFont(new PDType1Font(Standard14Fonts.FontName.HELVETICA), 50);
-			
-			pageOfOriginalDocumentWithMatermarkAsContentStream.newLineAtOffset(rectangle.getWidth() / 3,
-					rectangle.getHeight() / 6 * 1);
+
+			pageOfOriginalDocumentWithMatermarkAsContentStream.newLineAtOffset(rectangle.getWidth() / 10,
+					rectangle.getHeight() / 10 * 2);
 			pageOfOriginalDocumentWithMatermarkAsContentStream.showText("Confidentiel - Propriété Siemens");
-			
-			pageOfOriginalDocumentWithMatermarkAsContentStream.newLineAtOffset(rectangle.getWidth() / 3,
-					rectangle.getHeight() / 6 * 2);
-			pageOfOriginalDocumentWithMatermarkAsContentStream.showText("Version Draft");
-			
-			pageOfOriginalDocumentWithMatermarkAsContentStream.newLineAtOffset(rectangle.getWidth() / 3,
-					rectangle.getHeight() / 6 * 3);
+
+			pageOfOriginalDocumentWithMatermarkAsContentStream.newLineAtOffset(0, -40);
+			pageOfOriginalDocumentWithMatermarkAsContentStream.showText("Draft");
+
+			pageOfOriginalDocumentWithMatermarkAsContentStream.newLineAtOffset(0, -40);
 			pageOfOriginalDocumentWithMatermarkAsContentStream.showText("Copie réservée à");
-			
-			pageOfOriginalDocumentWithMatermarkAsContentStream.newLineAtOffset(rectangle.getWidth() / 3,
-					rectangle.getHeight() / 6 * 4);
-			pageOfOriginalDocumentWithMatermarkAsContentStream.showText(pdfAllowedUser.getPrenom() + " "
-					+ pdfAllowedUser.getNom());
-			
-			
+
+			pageOfOriginalDocumentWithMatermarkAsContentStream.newLineAtOffset(0, -40);
+			pageOfOriginalDocumentWithMatermarkAsContentStream
+					.showText(pdfAllowedUser.getPrenom() + " " + pdfAllowedUser.getNom());
+
 			pageOfOriginalDocumentWithMatermarkAsContentStream.endText();
 			pageOfOriginalDocumentWithMatermarkAsContentStream.close();
 		}
@@ -217,7 +216,7 @@ public class PDFModificationApplication {
 				+ PDFModificationHelpers.PDF_EXTENSION_WITH_POINT;
 		String generatedPersonnalizedProtectedPDFFullPath = PDFModificationHelpers.outputDirectoryName + "/"
 				+ generatedPersonnalizedProtectedPDFFileNameWithExtension;
-		
+
 		FileHelper.removeFileIfExists(generatedPersonnalizedProtectedPDFFullPath);
 
 		LOGGER.info(() -> "Save generated protected PDF:" + generatedPersonnalizedProtectedPDFFullPath + " for "
