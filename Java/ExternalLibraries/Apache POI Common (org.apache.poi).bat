dad0ae6e79546460d2afa46e78cdeca76defe9de
yanndanielou <yanndanielou@users.noreply.github.com>
2023-12-28 12:35:16 +0100

    Update Apache POI Common (org.apache.poi).bat

--- Java/ExternalLibraries/Apache POI Common (org.apache.poi).bat
+++ Java/ExternalLibraries/Apache POI Common (org.apache.poi).bat
@@ -1,4 +1,8 @@
 
 CALL DOWNLOAD_FILE_FROM_LINK "https://repo1.maven.org/maven2/org/apache/poi/poi/5.2.5/poi-5.2.5.jar"
+CALL DOWNLOAD_FILE_FROM_LINK "https://repo1.maven.org/maven2/org/apache/poi/poi-ooxml/5.2.5/poi-ooxml-5.2.5.jar"
+CALL DOWNLOAD_FILE_FROM_LINK "https://repo1.maven.org/maven2/org/apache/commons/commons-compress/1.25.0/commons-compress-1.25.0.jar"
+CALL DOWNLOAD_FILE_FROM_LINK "https://repo1.maven.org/maven2/org/apache/xmlbeans/xmlbeans/5.2.0/xmlbeans-5.2.0.jar"
+CALL DOWNLOAD_FILE_FROM_LINK "https://repo1.maven.org/maven2/org/apache/poi/ooxml-schemas/1.0/ooxml-schemas-1.0.jar"
 
 timeout /t 15
