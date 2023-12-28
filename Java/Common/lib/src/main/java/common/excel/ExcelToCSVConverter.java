90bb14ada0ab9b204c2003f99580a82538d70228
yanndanielou <yanndanielou@users.noreply.github.com>
2023-12-28 12:34:13 +0100

    Update ExcelToCSVConverter.java

--- Java/Common/lib/src/main/java/common/excel/ExcelToCSVConverter.java
+++ Java/Common/lib/src/main/java/common/excel/ExcelToCSVConverter.java
@@ -1,11 +1,13 @@
 package common.excel;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import com.spire.xls.Workbook;
-import com.spire.xls.Worksheet;
-import com.spire.xls.collections.WorksheetsCollection;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 import common.filesanddirectories.FileNameExtensionAndPathHelper;
 
@@ -18,24 +20,40 @@ public class ExcelToCSVConverter {
 				.getExtensionWithoutPointOfFile(excelFilePathWithExtension);
 		String excelFileNameWithoutExtension = FileNameExtensionAndPathHelper
 				.getFileNameWithoutExtension(excelFilePathWithExtension);
+		InputStream inp = null;
+		try {
+			inp = new FileInputStream(excelFilePathWithExtension);
+			Workbook wb = WorkbookFactory.create(inp);
 
-		// Create an instance of Workbook class
-		Workbook workbook = new Workbook();
-		// Load an Excel file
-		workbook.loadFromFile(excelFilePathWithExtension);
+			LOGGER.info(() -> "Excel document " + excelFilePathWithExtension + " has " + wb.getNumberOfSheets()
+					+ " work sheets");
 
-		// Get the first worksheet
-		WorksheetsCollection worksheets = workbook.getWorksheets();
-		LOGGER.info(
-				() -> "Excel document " + excelFilePathWithExtension + " has " + worksheets.size() + " work sheets");
-		for (int workSheetNumber = 0; workSheetNumber < worksheets.size(); workSheetNumber++) {
-			Worksheet sheet = worksheets.get(workSheetNumber);
-			String sheetName = sheet.getName();
-			LOGGER.info(() -> "Treat sheet: " + sheetName);
-			// Save the worksheet as CSV
-			sheet.saveToFile(excelFileNameWithoutExtension + "-" + sheetName, ",");
+			for (int i = 0; i < wb.getNumberOfSheets(); i++) {
+				String sheetName = wb.getSheetAt(i).getSheetName();
+
+				System.out.println(sheetName);
+			}
+		} catch (java.io.FileNotFoundException ex) {
+			LOGGER.error(ex);
+		} catch (Exception ex) {
+			LOGGER.error(ex);
+		} finally {
+			try {
+				inp.close();
+			} catch (IOException ex) {
+			}
 		}
-
+		/*
+		 * // Create an instance of Workbook class Workbook workbook = new Workbook();
+		 * // Load an Excel file workbook.loadFromFile(excelFilePathWithExtension);
+		 * 
+		 * // Get the first worksheet WorksheetsCollection worksheets =
+		 * workbook.getWorksheets(); for (int workSheetNumber = 0; workSheetNumber <
+		 * worksheets.size(); workSheetNumber++) { Worksheet sheet =
+		 * worksheets.get(workSheetNumber); String sheetName = sheet.getName();
+		 * LOGGER.info(() -> "Treat sheet: " + sheetName); // Save the worksheet as CSV
+		 * sheet.saveToFile(excelFileNameWithoutExtension + "-" + sheetName, ","); }
+		 */
 	}
 
 }
