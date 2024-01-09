fc97a5ca367d528fc77d942020fcd35475d3b04b
yanndanielou <yanndanielou@users.noreply.github.com>
2024-01-09 21:33:03 +0100

    3dcore

--- Java/ExternalLibraries/DOWNLOAD_AND_UNZIP_FILE_FROM_LINK.bat
+++ Java/ExternalLibraries/DOWNLOAD_AND_UNZIP_FILE_FROM_LINK.bat
@@ -1,10 +1,22 @@
 @SET FULL_LINK=%1
 @for /f "delims=" %%i in ("%FULL_LINK%") do @set "fileName=%%~nxi"
-@set fileNameWithoutExtension=%fileName:~0,-4%
+
+
+@set fileNameLast3Digits=%fileName:~-3%
+@set fileNameLast4Digits=%fileName:~-4%
+
+Echo fileNameLast3Digits : %fileNameLast3Digits%
+Echo fileNameLast4Digits : %fileNameLast4Digits%
+
+@set fileNameWithoutExtension=%fileName:~0,-10%
+IF .7z == %fileNameLast3Digits% set fileNameWithoutExtension=%fileName:~0,-3% & echo Extension found: .7z
+IF .zip == %fileNameLast4Digits% set fileNameWithoutExtension=%fileName:~0,-4% & echo .zip
 
 @echo fileName %fileName% 
 @echo FULL_LINK %FULL_LINK% 
 @echo fileNameWithoutExtension %fileNameWithoutExtension% 
+
+pause
 
 @CALL DOWNLOAD_FILE_FROM_LINK %1
 powershell -command "Expand-Archive -Force '%fileName%' '%fileNameWithoutExtension%'"
--- Java/ExternalLibraries/Java 3D Core.bat
+++ Java/ExternalLibraries/Java 3D Core.bat
@@ -1,5 +1,11 @@
 
 CALL DOWNLOAD_FILE_FROM_LINK "https://repo1.maven.org/maven2/com/massisframework/j3d/java3d-core/1.6.0.1/java3d-core-1.6.0.1.jar"
+CALL DOWNLOAD_FILE_FROM_LINK "https://repo1.maven.org/maven2/javax/vecmath/vecmath/1.5.2/vecmath-1.5.2.jar"
+CALL DOWNLOAD_FILE_FROM_LINK https://repo1.maven.org/maven2/java3d/j3d-core-utils/1.3.1/j3d-core-utils-1.3.1.jar
+
+
+echo read https://jogamp.org/wiki/index.php/Downloading_and_installing_JOGL
+CALL DOWNLOAD_AND_UNZIP_FILE_FROM_LINK "https://jogamp.org/deployment/jogamp-current/archive/jogamp-all-platforms.7z"
 
 
 timeout /t 15
