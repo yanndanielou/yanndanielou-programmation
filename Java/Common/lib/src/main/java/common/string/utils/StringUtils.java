68b13d0261e86f41ed87cd29725d9391efa33791
yanndanielou <yanndanielou@users.noreply.github.com>
2024-04-24 21:35:17 +0200

    Update StringUtils.java

--- Java/Common/lib/src/main/java/common/string/utils/StringUtils.java
+++ Java/Common/lib/src/main/java/common/string/utils/StringUtils.java
@@ -1,6 +1,14 @@
 package common.string.utils;
 
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class StringUtils {
 
 
+	public static String getISO8601CurrentLocalTime() {
+		String formattedTime= ZonedDateTime.now( ZoneOffset.UTC ).format( DateTimeFormatter.ISO_INSTANT);
+	return formattedTime;
+	}
 }
