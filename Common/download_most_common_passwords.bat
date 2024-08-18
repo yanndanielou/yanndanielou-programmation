456d39dbdc96661c3c1e92cb42d3b580ca31c9a7
yanndanielou <yanndanielou@users.noreply.github.com>
2024-08-18 15:38:32 +0200

    Update download_most_common_passwords.bat

--- Common/download_most_common_passwords.bat
+++ Common/download_most_common_passwords.bat
@@ -1,12 +1,17 @@
 
 
-@CALL ..\Windows\DownloadFromWeb\DOWNLOAD_FILE_FROM_LINK.bat  "https://github.com/danielmiessler/SecLists/blob/master/Passwords/Common-Credentials/10-million-password-list-top-1.txt"
-@CALL ..\Windows\DownloadFromWeb\DOWNLOAD_FILE_FROM_LINK.bat  "https://github.com/danielmiessler/SecLists/blob/master/Passwords/Common-Credentials/10-million-password-list-top-10.txt"
-@CALL ..\Windows\DownloadFromWeb\DOWNLOAD_FILE_FROM_LINK.bat  "https://github.com/danielmiessler/SecLists/blob/master/Passwords/Common-Credentials/10-million-password-list-top-100.txt"
-@CALL ..\Windows\DownloadFromWeb\DOWNLOAD_FILE_FROM_LINK.bat  "https://github.com/danielmiessler/SecLists/blob/master/Passwords/Common-Credentials/10-million-password-list-top-1000.txt"
-@CALL ..\Windows\DownloadFromWeb\DOWNLOAD_FILE_FROM_LINK.bat  "https://github.com/danielmiessler/SecLists/blob/master/Passwords/Common-Credentials/10-million-password-list-top-10000.txt"
-@CALL ..\Windows\DownloadFromWeb\DOWNLOAD_FILE_FROM_LINK.bat  "https://github.com/danielmiessler/SecLists/blob/master/Passwords/Common-Credentials/10-million-password-list-top-100000.txt"
-@CALL ..\Windows\DownloadFromWeb\DOWNLOAD_FILE_FROM_LINK.bat  "https://github.com/danielmiessler/SecLists/blob/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt"
-@CALL ..\Windows\DownloadFromWeb\DOWNLOAD_FILE_FROM_LINK.bat  "https://github.com/danielmiessler/SecLists/blob/master/Passwords/Common-Credentials/10-million-password-list-top-10000000.txt"
+@CALL ..\Windows\DownloadFromWeb\DOWNLOAD_FILE_FROM_LINK.bat  "https://github.com/danielmiessler/SecLists/raw/master/Passwords/Common-Credentials/10-million-password-list-top-1.txt"
+@CALL ..\Windows\DownloadFromWeb\DOWNLOAD_FILE_FROM_LINK.bat  "https://github.com/danielmiessler/SecLists/raw/master/Passwords/Common-Credentials/10-million-password-list-top-10.txt"
+@CALL ..\Windows\DownloadFromWeb\DOWNLOAD_FILE_FROM_LINK.bat  "https://github.com/danielmiessler/SecLists/raw/master/Passwords/Common-Credentials/10-million-password-list-top-100.txt"
+@CALL ..\Windows\DownloadFromWeb\DOWNLOAD_FILE_FROM_LINK.bat  "https://github.com/danielmiessler/SecLists/raw/master/Passwords/Common-Credentials/10-million-password-list-top-1000.txt"
+@CALL ..\Windows\DownloadFromWeb\DOWNLOAD_FILE_FROM_LINK.bat  "https://github.com/danielmiessler/SecLists/raw/master/Passwords/Common-Credentials/10-million-password-list-top-10000.txt"
+@CALL ..\Windows\DownloadFromWeb\DOWNLOAD_FILE_FROM_LINK.bat  "https://github.com/danielmiessler/SecLists/raw/master/Passwords/Common-Credentials/10-million-password-list-top-100000.txt"
+@CALL ..\Windows\DownloadFromWeb\DOWNLOAD_FILE_FROM_LINK.bat  "https://github.com/danielmiessler/SecLists/raw/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt"
+@CALL ..\Windows\DownloadFromWeb\DOWNLOAD_FILE_FROM_LINK.bat  "https://github.com/danielmiessler/SecLists/raw/master/Passwords/Common-Credentials/10-million-password-list-top-10000000.txt"
+
+
+
+https://github.com/danielmiessler/SecLists/blob/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt
+https://github.com/danielmiessler/SecLists/raw/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt
 
 @timeout /t 30
