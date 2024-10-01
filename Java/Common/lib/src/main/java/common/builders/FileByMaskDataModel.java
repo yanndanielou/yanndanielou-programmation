d7f82e5437ad25b0205bd71e750735500acad215
yanndanielou <yanndanielou@users.noreply.github.com>
2024-10-01 23:33:26 +0200

    second regard

--- Java/Common/lib/src/main/java/common/builders/FileByMaskDataModel.java
+++ Java/Common/lib/src/main/java/common/builders/FileByMaskDataModel.java
@@ -4,17 +4,24 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
+import common.collection.CollectionUtils;
 import common.filesanddirectories.FileNameExtensionAndPathHelper;
 
 public class FileByMaskDataModel extends NamedDataModel {
 
 	private String folderPath;
 	private String fileNameMask;
+	private String fileName;
 
 	public List<File> getResolvedFiles() {
-		File[] allFilesNamesWithExtension = FileNameExtensionAndPathHelper.getAllFilesNamesMatchingMask(folderPath,
-				fileNameMask);
-		return Arrays.asList(allFilesNamesWithExtension);
+		if (fileName != null) {
+			return CollectionUtils.asList(new File(folderPath + "\\" + fileName));
+		} else {
+
+			File[] allFilesNamesWithExtension = FileNameExtensionAndPathHelper.getAllFilesNamesMatchingMask(folderPath,
+					fileNameMask);
+			return Arrays.asList(allFilesNamesWithExtension);
+		}
 	}
 
 	@Override
