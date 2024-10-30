f58134a3f5d3030c6b79d033ae1e57a73efbd88a
yanndanielou <yanndanielou@users.noreply.github.com>
2024-10-30 16:51:25 +0100

    Update install_common_libraries.bat

--- Python/install_common_libraries.bat
+++ Python/install_common_libraries.bat
@@ -2,5 +2,6 @@ CALL SET_PYTHON_HOME.bat
 
 call %PYTHON_HOME%\python.exe -m pip install mypy
 call %PYTHON_HOME%\python.exe -m pip install m3uspiff
+call %PYTHON_HOME%\python.exe -m pip install pysimplegui
 
 timeout /t 100
