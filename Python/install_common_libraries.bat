9cf21e6afd1eed5bc5e8abb8f4309b86a8e38004
yanndanielou <yanndanielou@users.noreply.github.com>
2024-11-13 16:31:06 +0100

    Update install_common_libraries.bat

--- Python/install_common_libraries.bat
+++ Python/install_common_libraries.bat
@@ -4,5 +4,7 @@ call %PYTHON_HOME%\python.exe -m pip install mypy
 call %PYTHON_HOME%\python.exe -m pip install m3uspiff
 call %PYTHON_HOME%\python.exe -m pip install ffmpeg
 call %PYTHON_HOME%\python.exe -m pip install ffprobe 
+call %PYTHON_HOME%\python.exe -m pip install customtkinter
+
 
 timeout /t 100
