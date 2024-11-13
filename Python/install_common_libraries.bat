ff442a969554e6bb40cd5464632777c3c586828f
yanndanielou <yanndanielou@users.noreply.github.com>
2024-11-13 15:12:26 +0100

    Update install_common_libraries.bat

--- Python/install_common_libraries.bat
+++ Python/install_common_libraries.bat
@@ -2,5 +2,7 @@ CALL SET_PYTHON_HOME.bat
 
 call %PYTHON_HOME%\python.exe -m pip install mypy
 call %PYTHON_HOME%\python.exe -m pip install m3uspiff
+call %PYTHON_HOME%\python.exe -m pip install ffmpeg
+call %PYTHON_HOME%\python.exe -m pip install ffprobe 
 
 timeout /t 100
