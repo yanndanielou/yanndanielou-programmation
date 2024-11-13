52eee7f25263c76b3adb852a3cd6be9265648a7b
yanndanielou <yanndanielou@users.noreply.github.com>
2024-11-13 15:18:19 +0100

    python

--- Python/xinstall_common_libraries.bat
+++ Python/xinstall_common_libraries.bat
@@ -3,5 +3,7 @@ CALL SET_PYTHON_HOME.bat
 call %PYTHON_HOME%\python.exe -m pip install mypy
 call %PYTHON_HOME%\python.exe -m pip install m3uspiff
 call %PYTHON_HOME%\python.exe -m pip install pysimplegui
+call %PYTHON_HOME%\python.exe -m pip install googlesearch-python
+
 
 timeout /t 100
