856067dbca459af867bfbe30f39cfa50d3ee6e5a
yanndanielou <yanndanielou@users.noreply.github.com>
2024-11-26 23:16:08 +0100

    Update TransformPerformanceCountersLogsToSplunkMetrics.py

--- Python/TransformPerformanceCountersLogsToSplunkMetrics/TransformPerformanceCountersLogsToSplunkMetrics.py
+++ Python/TransformPerformanceCountersLogsToSplunkMetrics/TransformPerformanceCountersLogsToSplunkMetrics.py
@@ -84,11 +84,11 @@ def process_input_zip_file(input_zip_file):
     subprocess.call(r'"C:\Program Files\7-Zip\7z.exe" e ' + "\"" + input_zip_file + "\"" + ' -o' + "\"" + temp_folder_full_path_name + "\"")
     LoggerConfig.printAndLogInfo("Extraction done")
     
-    output_zip_file_name = input_zip_file_file_without_extension
+    output_zip_file_name = input_zip_file_file_without_extension.replace("_actual","")
     if len(param.metric_name_fixed_prefix_for_all_mesures) > 0:
         output_zip_file_name = output_zip_file_name + "_" + param.metric_name_fixed_prefix_for_all_mesures
     
-    output_zip_file_name = output_zip_file_name + param.output_files_suffix + "_ready_for_splunk." +  param.output_files_extension
+    output_zip_file_name = output_zip_file_name + param.output_files_suffix + "_splunk." +  param.output_files_extension
     output_zip_file = zipfile.ZipFile(param.output_metrics_ready_for_splunk_directory + "\\" + output_zip_file_name,'w')
     
     LoggerConfig.printAndLogInfo("Convert all performance_counter_log to csv")
@@ -103,32 +103,6 @@ def process_input_zip_file(input_zip_file):
     LoggerConfig.printAndLogInfo("Remove temporary directory:" + temp_folder_full_path_name)
     shutil.rmtree(temp_folder_full_path_name)
 
-
-def remove_faulty_characters_in_performance_counter_log_converted_to_csv( original_line, line_number):   
-    
-    return original_line
-        
-     
-def compute_splunk_ready_file_name(performance_counter_log_file_without_extension, all_used_machine_names, all_ignore_machine_names, all_used_application_names, all_ignore_application_names,  all_used_mesure_types, all_ignore_mesure_types):
-    splunk_ready_file_name_without_extension = performance_counter_log_file_without_extension 
-    
-    if len(param.metric_name_fixed_prefix_for_all_mesures) > 0:
-        splunk_ready_file_name_without_extension = splunk_ready_file_name_without_extension + "_" + param.metric_name_fixed_prefix_for_all_mesures
-    
-    if len(all_ignore_machine_names) > 0 or len(all_ignore_application_names) > 0 or len(all_ignore_mesure_types) > 0:
-        splunk_ready_file_name_without_extension = splunk_ready_file_name_without_extension + "_partial"
-        
-        if len(all_ignore_machine_names) > 0:
-            splunk_ready_file_name_without_extension = splunk_ready_file_name_without_extension + "_".join(all_used_machine_names)
-            
-        if len(all_ignore_application_names) > 0:
-            splunk_ready_file_name_without_extension = splunk_ready_file_name_without_extension + "_".join(all_used_application_names).replace("Process_","")
-            
-        #if len(all_ignore_mesure_types) > 0:
-        #    splunk_ready_file_name = splunk_ready_file_name + "_".join(all_used_mesure_types)
-    
-    splunk_ready_file_name_with_extension = splunk_ready_file_name_without_extension  + param.output_files_suffix
-    return splunk_ready_file_name_with_extension
 
 def convert_performance_counter_log_file_to_metric_file( performance_counter_log_file_file_name, performance_counter_log_file_full_path):
     LoggerConfig.printAndLogInfo( "Convert performance_counter_log file:" + performance_counter_log_file_file_name)  
@@ -155,7 +129,7 @@ def convert_performance_counter_log_file_to_metric_file( performance_counter_log
 
     return metrics
     
-def handle_performance_counter_log_file( performance_counter_log_file_full_path, temp_folder_full_path_name, output_zip_file):
+def handle_performance_counter_log_file( performance_counter_log_file_full_path, temp_folder_full_path_name, output_zip_file: zipfile.ZipFile):
     performance_counter_log_file_file_name = basename(performance_counter_log_file_full_path)        
     performance_counter_log_file_without_extension = os.path.splitext(performance_counter_log_file_file_name)[0]
     
@@ -191,55 +165,9 @@ def create_splunk_ready_metric_file( splunk_ready_file_name, splunk_ready_file_c
     return splunk_ready_file_name
  
 
-def add_splunk_ready_metric_file_to_zip(output_zip_file, splunk_ready_file_full_path, splunk_ready_file_name):
+def add_splunk_ready_metric_file_to_zip(output_zip_file: zipfile.ZipFile, splunk_ready_file_full_path, splunk_ready_file_name):
     output_zip_file.write(splunk_ready_file_full_path, arcname=splunk_ready_file_name, compress_type=zipfile.ZIP_DEFLATED)
             
-def process_input_zip_file( input_zip_file):
-    LoggerConfig.printAndLogInfo( "processing " + basename(input_zip_file))
-
-    input_zip_file_file_name = basename(input_zip_file)
-    LoggerConfig.printAndLogInfo( "File name:" + input_zip_file_file_name)
-    
-    input_zip_file_directory_name = os.path.dirname(input_zip_file)    
-    LoggerConfig.printAndLogInfo( "Directory name:" + input_zip_file_directory_name)
-    
-    input_zip_file_file_without_extension = os.path.splitext(input_zip_file_file_name)[0]
-    LoggerConfig.printAndLogInfo( "File name without extension:" + input_zip_file_file_without_extension)
-    
-    temp_folder_name = "temp_" + input_zip_file_file_without_extension
-    
-    temp_folder_full_path_name = param.output_metrics_ready_for_splunk_directory + "\\" + temp_folder_name
-    
-    if os.path.exists(temp_folder_full_path_name):
-        LoggerConfig.printAndLogInfo( "\t" + "Removing:" + temp_folder_full_path_name + ". Previous execution of the tool was probably interrupted")
-        shutil.rmtree(temp_folder_full_path_name, ignore_errors=False, onerror=None)
-        
-    os.makedirs(temp_folder_full_path_name)
-    
-    LoggerConfig.printAndLogInfo( "extract to temporary folder")
-    subprocess.call(r'"C:\Program Files\7-Zip\7z.exe" e ' + "\"" + input_zip_file + "\"" + ' -o' + "\"" + temp_folder_full_path_name + "\"")
-    LoggerConfig.printAndLogInfo( "Extraction done")
-    
-    output_zip_file_name = input_zip_file_file_without_extension
-    if len(param.metric_name_fixed_prefix_for_all_mesures) > 0:
-        output_zip_file_name = output_zip_file_name + "_" + param.metric_name_fixed_prefix_for_all_mesures
-    
-    output_zip_file_name = output_zip_file_name + param.output_files_suffix + "_ready_for_splunk." +  param.output_files_extension
-    output_zip_file = zipfile.ZipFile(param.output_metrics_ready_for_splunk_directory + "\\" + output_zip_file_name,'w')
-    
-    LoggerConfig.printAndLogInfo( "Convert all performance counters logs to csv")
-    
-    handle_performance_counter_log_files = glob.glob(temp_folder_full_path_name + "\\*" + param.input_performance_counter_file_extension)
-    for performance_counter_log_file in handle_performance_counter_log_files:
-        handle_performance_counter_log_file( performance_counter_log_file, temp_folder_full_path_name, output_zip_file)
-
-    #Close the zip
-    LoggerConfig.printAndLogInfo( "Zip DONE")
-    output_zip_file.close()            
-        
-    LoggerConfig.printAndLogInfo( "Remove temporary directory:" + temp_folder_full_path_name)
-    shutil.rmtree(temp_folder_full_path_name)
-
 def main(argv):
 
     log_file_name = os.path.basename(__file__) + str(random.randrange(100000)) + ".log"
