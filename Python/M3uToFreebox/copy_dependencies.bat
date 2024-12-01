c29b7c8c69278269eb95447149e17575e45f76c9
yanndanielou <yanndanielou@users.noreply.github.com>
2024-12-01 22:01:35 +0100

    Update copy_dependencies.bat

--- Python/M3uToFreebox/copy_dependencies.bat
+++ Python/M3uToFreebox/copy_dependencies.bat
@@ -1,5 +1,20 @@
 @RD /S /Q Dependencies
-MD Dependencies
-echo "" > Dependencies\__init__.py
-COPY ..\Common Dependencies\
-COPY ..\Logger Dependencies\
+@MD Dependencies
+@echo "" > Dependencies\__init__.py
+
+CALL :COPY_DEPENDENCY Common
+CALL :COPY_DEPENDENCY Logger
+
+@GOTO :END_OF_FILE
+
+
+:COPY_DEPENDENCY
+@Echo install %1
+@MD Dependencies\%1
+ROBOCOPY ..\%1 Dependencies\%1 *.py
+@EXIT /B 0
+
+
+:END_OF_FILE
+
+@timeout /t 100
