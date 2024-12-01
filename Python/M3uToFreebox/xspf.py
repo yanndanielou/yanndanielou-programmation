cb35e617991c46fb5bb84887dcb023a89767908f
yanndanielou <yanndanielou@users.noreply.github.com>
2024-12-01 22:21:37 +0100

    m3u

--- Python/M3uToFreebox/xspf.py
+++ Python/M3uToFreebox/xspf.py
@@ -40,5 +40,19 @@ class XspfFileCreator:
         print(pretty_xml)
         
         with open(output_file_name, 'w') as f:
-              f.write(pretty_xml)
+            f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+            f.write('<playlist xmlns="http://xspf.org/ns/0/" xmlns:vlc="http://www.videolan.org/vlc/playlist/ns/0/" version="1">\n')
+            f.write('\t<title>Liste de lecture</title>\n')
+            f.write('\t<trackList>\n')
+            f.write('\t\t<track>\n')
+            f.write('\t\t<location>file:///C:/Users/fr232487/Downloads/WhatsApp%20Video%202024-11-01%20at%2018.06.53.mp4</location>\n')
+            f.write('\t\t<duration>8981</duration>\n')
+            f.write('\t\t<extension application="http://www.videolan.org/vlc/playlist/0">\n')
+            f.write('\t\t\t<vlc:id>0</vlc:id>\n')
+            f.write('\t\t</extension>\n')
+            f.write('\t</track>\n')
+            f.write('</trackList>\n')
+            f.write('</playlist>\n')
+
+                  
     
