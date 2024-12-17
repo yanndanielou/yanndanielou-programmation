54480f89aee45aba506e287be309435df3951747
yanndanielou <yanndanielou@users.noreply.github.com>
2024-12-17 20:55:58 +0100

    Create download_file_progress_bar_tqdm.py

---
+++ Python/Sandbox/download_file_progress_bar_tqdm.py
@@ -0,0 +1,26 @@
+# -*-coding:Utf-8 -*
+
+import requests
+import tqdm
+
+def download(url: str, filename: str):
+    with open(filename, 'wb') as f:
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            total = int(r.headers.get('content-length', 0))
+
+            # tqdm has many interesting parameters. Feel free to experiment!
+            tqdm_params = {
+                'desc': url,
+                'total': total,
+                'miniters': 1,
+                'unit': 'B',
+                'unit_scale': True,
+                'unit_divisor': 1024,
+            }
+            with tqdm.tqdm(**tqdm_params) as pb:
+                for chunk in r.iter_content(chunk_size=8192):
+                    pb.update(len(chunk))
+                    f.write(chunk)
+
+download('https://sample-videos.com/zip/10mb.zip', '100MB.bin')
